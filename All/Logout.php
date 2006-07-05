b96b04f06f44183a2762a7123df005db723a98c8
Eddie Kohler <ekohler@gmail.com>
2006-07-05 19:30:29 +0000

    x

--- All/Logout.php
+++ All/Logout.php
@@ -1,6 +1,7 @@
 <?php 
 require_once('../Code/confHeader.inc');
 $_SESSION["Me"]->invalidate();
+unset($_SESSION["AskedYouToUpdateContactInfo"]);
 $LoginType = "Logout";
 $Conf->confirmMsg("You have been logged out, but you can log in again if you'd like.");
 include('login.php');
