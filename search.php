e7860df6d6f83aee692c9c7fa64ce19ee06819ed
Eddie Kohler <ekohler@gmail.com>
2006-08-18 16:11:06 +0000

    search

--- search.php
+++ search.php
@@ -10,33 +10,73 @@ $Me->goIfNotPC('index.php');
 $Conf->header("Search");
 
 
+$totals = 0;
+foreach (array('ti', 'ab', 'au', 'co') as $f)
+    $totals += ($search[$f] = (defval($_REQUEST[$f], 0) != 0));
+if ($totals == 0)
+    $search['ti'] = $search['ab'] = $search['au'] = $search['co'] = 1;
+if (!$Me->amAssistant() && $Conf->blindSubmission() > 1)
+    $search['au'] = $search['co'] = 0;
+$searchall = ($Me->amAssistant() && defval($_REQUEST["all"], 0) != 0);
+
 echo "<form method='get' action='search.php'>
-<input class='textlite' type='text' size='40' name='search' value=\"";
+<table class='simple'>
+  <tr>
+    <td>
+      <input class='textlite' type='text' size='40' name='search' value=\"";
 if (isset($_REQUEST["search"]))
     echo htmlspecialchars($_REQUEST["search"]);
 echo "\" /> <input class='button' type='submit' name='go' value='Search' />
-</form>\n";
+    </td>\n\n    <td>\n";
+
+echo "	<input type='checkbox' name='ti' value='1'", ($search['ti'] ? " checked='checked'" : ""), " />&nbsp;Titles\n",
+    "	<span class='sep'></span> <input type='checkbox' name='ab' value='1'", ($search['ab'] ? " checked='checked'" : ""), " />&nbsp;Abstracts\n    </td>\n";
+if ($Me->amAssistant() || $Conf->blindSubmission() <= 1) {
+    echo "    <td>\n	<input type='checkbox' name='au' value='1'", ($search['au'] ? " checked='checked'" : ""), " />&nbsp;Authors\n",
+	"	<span class='sep'></span> <input type='checkbox' name='co' value='1'", ($search['co'] ? " checked='checked'" : ""), " />&nbsp;Collaborators\n    </td>\n";
+    if (!$Me->amAssistant() && $Conf->blindSubmission() == 1)
+	echo "  <tr>\n    <td colspan='2'></td><td><small>Non-blind submissions only</small></td>\n";
+}
+
+echo "  </tr>\n";
+if ($Me->amAssistant()) {
+    echo "  <tr>
+    <td></td>
+    <td colspan='2'>Papers: <input type='radio' name='all' value='0'",
+	($searchall ? "" : " checked='checked'"),
+	" />&nbsp;Submitted <span class='sep'></span> <input type='radio' name='all' value='1'",
+	($searchall ? " checked='checked'" : ""),
+	" />&nbsp;All</td>
+  </tr>\n";
+}
+
+echo "</table>\n</form>\n";
+
+// XXXXXXXX all vs. submitted
 
 
 if (isset($_REQUEST["search"]) && trim($_REQUEST["search"]) != "") {
-    // create a temporary table
-    $q = "create temporary table Matches select paperId from Paper\n";
-
-    $sep = "where";
-    $re = "";
+    // develop query
+    $re = $q = "";
+    $auextra = ($Me->amAssistant() ? "" : "blind=0 and ");
     foreach (preg_split("/\\s+/", $_REQUEST["search"]) as $word)
 	if ($word != "") {
 	    $re .= ($re == "" ? "" : "|") . preg_quote($word);
 	    $word = preg_replace("/([%_\\\\])/", "\\\$1", $word);
 	    $word = sqlq($word);
-	    $q .= $sep . " title like '%$word%' or abstract like '%$word%'";
-	    if ($Me->amAssistant())
-		$q .= " or authorInformation like '%$word%' or collaborators like '%$word%'";
-	    else
-		$q .= " or (blind=0 and (authorInformation like '%$word%' or collaborators like '%$word%'))";
-	    $sep = " or";
+	    if ($search['ti'])
+		$q .= " or (title like '%$word%')";
+	    if ($search['ab'])
+		$q .= " or (abstract like '%$word%')";
+	    if ($search['au'])
+		$q .= " or (${auextra}authorInformation like '%$word%')";
+	    if ($search['co'])
+		$q .= " or (${auextra}collaborators like '%$word%')";
 	}
     
+    // create a temporary table
+    $q = "create temporary table Matches select paperId from Paper where" . substr($q, 3);
+
     /* $find = sqlqtrim($_REQUEST["search"]);
     if ($Me->amAssistant())
 	$q .= "	where match(title, abstract, authorInformation, collaborators) against ('" . sqlqtrim($_REQUEST["search"]) . "' in boolean mode)\n";
