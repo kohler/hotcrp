5a2aee1d0ba9726c96a1841ab85e165c8a64fa80
Eddie Kohler <ekohler@gmail.com>
2006-08-26 01:10:54 +0000

    nit

--- contactauthors.php
+++ contactauthors.php
@@ -1,5 +1,6 @@
 <?php 
 require_once('Code/confHeader.inc');
+require_once('Code/papertable.inc');
 $Conf->connect();
 $Me = $_SESSION["Me"];
 $Me->goIfInvalid();
@@ -9,7 +10,7 @@ $Me->goIfInvalid();
 function confHeader() {
     global $paperId, $prow, $Conf;
     $title = ($paperId > 0 ? "Paper #$paperId Contact Authors" : "Paper Contact Authors");
-    $Conf->header($title, "contactauthors", actionBar($prow, false, "Contact Authors", "contactauthors.php?paperId=$paperId"));
+    $Conf->header($title, "contactauthors", actionBar($prow, false, "Contact Authors", "contactauthors.php?paperId=$paperId"), false);
 }
 
 function errorMsgExit($msg) {
@@ -102,63 +103,57 @@ else if (isset($_REQUEST["update"])) {
 
 
 if ($OK) {    
+    $paperTable = new PaperTable(false, false, true, false);
+    
     echo "<form method='post' action=\"contactauthors.php?paperId=$paperId&amp;post=1\" enctype='multipart/form-data'>
-<table class='paperauthors'>
-<tr class='id'>
-  <td class='caption'><h2>#$paperId</h2></td>
-  <td class='entry' colspan='2'><h2>", htmlspecialchars($prow->title), "</h2></td>\n</tr>\n";
+<table class='paper'>\n";
 
-?>
+    // title
+    echo "<tr class='id'>\n  <td class='caption'><h2>#$paperId</h2></td>\n";
+    echo "  <td class='entry' colspan='2'><h2>";
+    $paperTable->echoTitle($prow);
+    echo "</h2></td>\n</tr>\n\n";
 
-<tr>
-  <td class='caption'>Status:</td>
-  <td class='entry'><?php echo $Me->paperStatus($paperId, $prow, 1) ?></td>
-</tr>
+    // Paper contents
+    $paperTable->echoStatusRow($prow, PaperTable::STATUS_DOWNLOAD);
+    $paperTable->echoAbstractRow($prow);
+    $paperTable->echoAuthorInformation($prow);
+    $paperTable->echoCollaborators($prow);
+    $paperTable->echoTopics($prow);
 
-
-<tr>
-  <td class='caption'>Contact&nbsp;authors:</td>
-  <td class='entry plholder'><table class='pltable'>
-    <tr class='pl_headrow'><th>Name</th> <th>Email</th> <th></th></tr>
-    <?php {
-      $q = "select firstName, lastName, email, contactId
+    // Contact authors
+    echo "<tr>\n  <td class='caption'>Contact&nbsp;authors</td>\n";
+    echo "  <td class='entry plholder'><table class='pltable'>
+    <tr class='pl_headrow'><th>Name</th> <th>Email</th> <th></th></tr>\n";
+    $q = "select firstName, lastName, email, contactId
 	from ContactInfo
 	join PaperConflict using (contactId)
 	where paperId=$paperId and author=1
 	order by lastName, firstName, email";
-      $result = $Conf->qe($q, "while finding contact authors");
-      if (!DB::isError($result)) {
-	  while ($row = $result->fetchRow()) {
-	      echo "<tr><td>", contactHtml($row[0], $row[1]), "</td> <td>", htmlspecialchars($row[2]), "</td>";
-	      if ($Me->amAssistant())
-		  echo " <td><button class='button_small' type='submit' name='remove' value='$row[3]'>Remove contact author</button></td>";
-	      echo "</tr>\n    ";
-	  }
-      }
-    } ?>
-    <tr><td><input class='textlite' type='text' name='name' size='20' onchange='highlightUpdate()' /></td>
+    $result = $Conf->qe($q, "while finding contact authors");
+    if (!DB::isError($result)) {
+	while ($row = $result->fetchRow()) {
+	    echo "<tr><td>", contactHtml($row[0], $row[1]), "</td> <td>", htmlspecialchars($row[2]), "</td>";
+	    if ($Me->amAssistant())
+		echo " <td><button class='button_small' type='submit' name='remove' value='$row[3]'>Remove contact author</button></td>";
+	    echo "</tr>\n    ";
+	}
+    }
+
+    echo "    <tr><td><input class='textlite' type='text' name='name' size='20' onchange='highlightUpdate()' /></td>
 	<td><input class='textlite' type='text' name='email' size='20' onchange='highlightUpdate()' /></td>
 	<td><input class='button_default' type='submit' name='update' value='Add contact author' /></td>
     </tr>
   </table></td>
 </tr>
 
-<tr>
-  <td class='caption'>Authors:</td>
-  <td class='entry'><?php
-    echo authorTable($prow->authorInformation);
-?></td>
-</tr>
-
 </table>
-</form>
-
-
-<?php
+</form>\n";
     
 } else {
     $Conf->errorMsg("The paper disappeared!");
     printPaperLinks();
 }
 
-$Conf->footer(); ?>
+$Conf->footer();
+?>
