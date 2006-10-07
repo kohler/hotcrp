fe6865c2ce6bd3cc24b6e44de6e335463e9f1b43
Eddie Kohler <ekohler@gmail.com>
2006-10-07 03:21:29 +0000

    search improvement

--- search.php
+++ search.php
@@ -11,45 +11,56 @@ $Conf->header("Search", 'search');
 
 
 $totals = 0;
-foreach (array('ti', 'ab', 'au', 'co') as $f)
+foreach (array('ti', 'ab', 'au', 'co', 're') as $f)
     $totals += ($search[$f] = (defval($_REQUEST[$f], 0) != 0));
 if ($totals == 0)
-    $search['ti'] = $search['ab'] = $search['au'] = $search['co'] = 1;
+    $search['ti'] = $search['ab'] = $search['au'] = $search['co'] = $search['re'] = 1;
 if (!$Me->amAssistant() && $Conf->blindSubmission() > 1)
-    $search['au'] = $search['co'] = 0;
+    $search['au'] = $search['co'] = -1;
+if (!$Me->amAssistant())
+    $search['re'] = -1; // XXX fixme
 $searchall = ($Me->amAssistant() && defval($_REQUEST["all"], 0) != 0);
 
 echo "<form method='get' action='search.php'>
 <table class='simple'>
   <tr>
     <td>
-      <input class='textlite' type='text' size='40' name='search' value=\"";
+      <span class='nowrap'><input class='textlite' type='text' size='40' name='search' value=\"";
 if (isset($_REQUEST["search"]))
     echo htmlspecialchars($_REQUEST["search"]);
-echo "\" /> <input class='button' type='submit' name='go' value='Search' />
+echo "\" /> <input class='button' type='submit' name='go' value='Search' /></span>
+      <br />
+      <small>Finds <b>any</b> of the words</small>
     </td>
     <td>\n";
 
-echo "	<input type='checkbox' name='ti' value='1'", ($search['ti'] ? " checked='checked'" : ""), " />&nbsp;Titles\n",
-    "	<span class='sep'></span> <input type='checkbox' name='ab' value='1'", ($search['ab'] ? " checked='checked'" : ""), " />&nbsp;Abstracts\n    </td>\n";
-if ($Me->amAssistant() || $Conf->blindSubmission() <= 1)
-    echo "    <td>\n	<input type='checkbox' name='au' value='1'", ($search['au'] ? " checked='checked'" : ""), " />&nbsp;Authors\n",
-	"	<span class='sep'></span> <input type='checkbox' name='co' value='1'", ($search['co'] ? " checked='checked'" : ""), " />&nbsp;Collaborators\n    </td>\n";
+$aufoot = ($Conf->blindSubmission() == 1 && !$Me->amAssistant() ? "*" : "");
+foreach (array('ti' => 'Titles', 'ab' => 'Abstracts',
+	       'au' => "Authors$aufoot", 'co' => "Collaborators$aufoot",
+	       're' => 'Reviewers') as $tag => $name) {
+    if ($search[$tag] >= 0) {
+	if ($tag != 'ti')
+	    echo "  <span class='sep'></span>";
+	echo "  <span class='nowrap'><input type='checkbox' name='$tag' value='1'";
+	if ($search[$tag] > 0)
+	    echo " checked='checked'";
+	echo " />&nbsp;", $name, "</span>\n";
+    }
+}
+   
 
-echo "  </tr><tr>\n    <td><small>Finds <b>any</b> of the words</small></td>\n";
+echo "  <br />\n";
 if (!$Me->amAssistant() && $Conf->blindSubmission() == 1)
-    echo "    <td></td><td><small>Non-blind submissions only</small></td>
-  </tr><tr>\n    <td></td>\n";
+    echo "  <small>Non-blind submissions only</small><br />\n";
 
-if ($Me->amAssistant()) {
-    echo "    <td colspan='2'>Papers: <input type='radio' name='all' value='0'",
+if ($Me->amAssistant())
+    echo "  Papers: <input type='radio' name='all' value='0'",
 	($searchall ? "" : " checked='checked'"),
 	" />&nbsp;Submitted <span class='sep'></span> <input type='radio' name='all' value='1'",
 	($searchall ? " checked='checked'" : ""),
-	" />&nbsp;All</td>
-  </tr>\n";
-}
+	" />&nbsp;All\n";
 
+echo "</td>\n</tr>\n";
 echo "</table>\n</form>\n\n";
 
 
@@ -62,18 +73,23 @@ if (isset($_REQUEST["search"]) && trim($_REQUEST["search"]) != "") {
 	    $re .= ($re == "" ? "" : "|") . preg_quote($word);
 	    $word = preg_replace("/([%_\\\\])/", "\\\$1", $word);
 	    $word = sqlq($word);
-	    if ($search['ti'])
+	    if ($search['ti'] > 0)
 		$q .= " or (title like '%$word%')";
-	    if ($search['ab'])
+	    if ($search['ab'] > 0)
 		$q .= " or (abstract like '%$word%')";
-	    if ($search['au'])
+	    if ($search['au'] > 0)
 		$q .= " or (${auextra}authorInformation like '%$word%')";
-	    if ($search['co'])
-		$q .= " or (${auextra}collaborators like '%$word%')";
+	    if ($search['co'] > 0)
+		$q .= " or (${auextra}Paper.collaborators like '%$word%')";
+	    if ($search['re'] > 0)
+		$q .= " or (firstName like '%$word%') or (lastName like '%$word%') or (email like '%$word%')";
 	}
     
     // create a temporary table
-    $q = "create temporary table Matches select paperId from Paper where" . substr($q, 3);
+    if ($search['re'] > 0)
+	$q = "create temporary table Matches select Paper.paperId from Paper left join PaperReview using (paperId) left join ContactInfo on (PaperReview.contactId=ContactInfo.contactId) where" . substr($q, 3) . " group by Paper.paperId";
+    else
+	$q = "create temporary table Matches select paperId from Paper where" . substr($q, 3);
 
     /* $find = sqlqtrim($_REQUEST["search"]);
     if ($Me->amAssistant())
