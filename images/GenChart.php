da058e6b1f43ed98231b37e0906c741f9eac8d63
Eddie Kohler <ekohler@gmail.com>
2006-10-13 20:21:54 +0000

    image awesome

--- images/GenChart.php
+++ images/GenChart.php
@@ -9,24 +9,23 @@ if (!isset($_REQUEST["v"]))
 
 $s = (isset($_REQUEST["s"]) ? $_REQUEST["s"] : 0);
 
-$blockHeight = ($s == 1 ? 4 : 8);
-$blockWidth = ($s == 1 ? 5 : 8); // Was 16
+$blockHeight = ($s == 1 ? 3 : 8);
+$blockWidth = ($s == 1 ? 3 : 8); // Was 16
 $blockSkip = 2; // along vertical
 $blockPad = ($s == 1 ? 2 : 4); // pad from left/right side
+$textWidth = ($s == 1 ? 0 : 12);
 
 if (isset($_REQUEST["first"]) && is_numeric($_REQUEST["first"]))
     $valMin = $valMax = intval($_REQUEST["first"]);
 else
     $valMin = $valMax = 1;
 $values = array();
-$maxY = 5;
+$maxY = 3;
 foreach (explode(",", $_REQUEST["v"]) as $value) {
     $value = (is_numeric($value) && $value > 0 ? intval($value) : 0);
     $values[$valMax++] = $value;
     $maxY = max($value, $maxY);
 }
-
-$textWidth = 12;
 
 $picWidth = ($blockWidth + $blockPad) * ($valMax - $valMin)
     + $blockPad
@@ -39,23 +38,31 @@ $pic=ImageCreate($picWidth+1,$picHeight+1);
 //
 // White background, black outline
 //
-$cWhite=ImageColorAllocate($pic,255,255,255);
-$cBlack=ImageColorAllocate($pic,0,0,0);
+$cWhite = imagecolorallocate($pic,255,255,255);
+$cBlack = imagecolorallocate($pic,0,0,0);
+$cgrey = imagecolorallocate($pic, 190, 190, 255);
 
 if ($s == 0) {
     ImageFilledRectangle($pic,0,0,$picWidth+1,$picHeight+1,$cBlack);
     ImageFilledRectangle($pic,1,1,$picWidth-1,$picHeight-1,$cWhite);
+} else {
+    imagecolortransparent($pic, $cWhite);
+    imagefilledrectangle($pic, 0, $picHeight, $picWidth + 1, $picHeight + 1, $cgrey);
+    imagefilledrectangle($pic, 0, $picHeight - $blockHeight - $blockPad, 0, $picHeight + 1, $cgrey);
+    imagefilledrectangle($pic, $picWidth, $picHeight - $blockHeight - $blockPad, $picWidth + 1, $picHeight + 1, $cgrey);
 }
 
-for ($value = $valMin; $value < $valMax; $value++) {
-  //
-  // Set fill color for rectangles...
-  //
-  $frac = (255.0 * ($value-$valMin));
-  $frac = $frac / ($valMax - $valMin);
+$cbad = array(200, 128, 128);
+$cgood = array(0, 240, 0);
 
-  //  print "frac is $frac\n";
-  $cFill=ImageColorAllocate($pic,255-$frac, 0, $frac);
+for ($value = $valMin; $value < $valMax; $value++) {
+    // Set fill color for rectangles...
+    $frac = ($value - $valMin) / ($valMax - $valMin);
+
+    //  print "frac is $frac\n";
+    $cFill=ImageColorAllocate($pic, $cgood[0] * $frac + $cbad[0] * (1 - $frac),
+	$cgood[1] * $frac + $cbad[1] * (1 - $frac),
+	$cgood[2] * $frac + $cbad[2] * (1 - $frac));
 
   $height=$values[$value];
 
@@ -77,8 +84,13 @@ for ($value = $valMin; $value < $valMax; $value++) {
 
 }
 
-ImageStringUp($pic, 2, 0, 40, ($s == 1 ? "-" : "Bad"), $cBlack);
-ImageStringUp($pic, 2, $picWidth-$textWidth, 40, ($s == 1 ? "+" : "Good"), $cBlack);
+if ($s == 0) {
+    ImageStringUp($pic, 2, 0, 40, "Bad", $cBlack);
+    ImageStringUp($pic, 2, $picWidth-$textWidth, 40, "Good", $cBlack);
+} else {
+    imagestring($pic, 1, $textWidth + $blockPad, $picHeight - $blockHeight - $blockSkip - 2, "-", $cgrey);
+    imagestring($pic, 1, $picWidth - $blockWidth - $textWidth - $blockPad - 1, $picHeight - $blockHeight - $blockSkip - 2, "+", $cgrey);
+}
 
 Header("Content-type: image/png");
 Header("Cache-Control: public");
