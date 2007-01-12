d47a5fd6450509f543a5013842d897aca9e6daae
Eddie Kohler <ekohler@gmail.com>
2007-01-12 06:58:59 +0000

    whoops

--- reload.sh
+++ reload.sh
@@ -1,2 +1,2 @@
 #! /bin/sh
-perl -ne 'if($x||/RELOAD HERE/){$x=1;print;}' < Code/schema.sql | mysql -uTestConf -pTestConf TestConf
+perl -ne 'if($x||/RELOAD HERE/){$x=1;print;}' < Code/schema.sql | mysql -uHotNetsV -pHotNetsV HotNetsV
