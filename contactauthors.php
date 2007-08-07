c22a1fd41047c428fa364d1cd71bfe23980cf0a7
Eddie Kohler <ekohler@gmail.com>
2007-08-07 00:05:04 +0000

    change the control names of a couple buttons; maybe that will improve IE's response to this page

--- contactauthors.php
+++ contactauthors.php
@@ -67,26 +67,33 @@ confHeader();
 
 
 if (!$Me->canManagePaper($prow))
-    $Conf->errorMsg("You can't manage paper #$paperId since you are not a contact author.  If you believe this is incorrect, get a registered author to list you as a coauthor, or contact the site administrator.");
-else if (isset($_REQUEST["update"])) {
+    errorMsgExit("You can't manage paper #$paperId since you are not a contact author.  If you believe this is incorrect, get a registered author to list you as a coauthor, or contact the site administrator.");
+
+
+$needMsg = true;
+
+if (isset($_REQUEST["add"])) {
+    $needMsg = false;
     if (!isset($_REQUEST["email"]) || trim($_REQUEST["email"]) == "")
 	$Conf->errorMsg("You must enter the new contact author's email address."); 
     else if (($id = $Conf->getContactId($_REQUEST["email"], true)) > 0) {
 	if (addContactAuthor($paperId, $id))
 	    $Conf->confirmMsg("Contact author added.");
     }
-} else if (isset($_REQUEST["remove"])) {
-    if (!$Me->privChair)
-	$Conf->errorMsg("Only system administrators can remove contact authors from a paper.");
-    else if (($id = cvtint($_REQUEST['remove'])) <= 0)
-	$Conf->errorMsg("Invalid contact author ID in request.");
-    else {
-	if (removeContactAuthor($paperId, $id))
+}
+
+foreach ($_REQUEST as $k => $v)
+    if (substr($k, 0, 3) == "rem" && ($id = cvtint(substr($k, 3))) > 0) {
+	$needMsg = false;
+	if (!$Me->privChair)
+	    $Conf->errorMsg("Only system administrators can remove contact authors.");
+	else if (removeContactAuthor($paperId, $id))
 	    $Conf->confirmMsg("Contact author removed.");
     }
-} else
+
+if ($needMsg)
     $Conf->infoMsg("Use this screen to add more contact authors for your paper.  Any contact author can edit paper information, upload new versions, submit the paper, and view reviews." . ($Me->privChair ? "" : "  Only system administrators can <i>remove</i> contact authors from the paper, so act carefully."));
-    
+
 
 
 
@@ -123,13 +130,13 @@ if ($OK) {
     while ($row = edb_row($result)) {
 	echo "<tr><td class='pad'>", contactHtml($row[0], $row[1]), "</td> <td class='pad'>", htmlspecialchars($row[2]), "</td>";
 	if ($Me->privChair)
-	    echo " <td class='pad'><button class='button_small' type='submit' name='remove' value='$row[3]'>Remove contact author</button></td>";
+	    echo " <td class='pad'><button class='button_small' type='submit' name='rem$row[3]' value='1'>Remove contact author</button></td>";
 	echo "</tr>\n    ";
     }
 
     echo "    <tr><td class='pad'><input class='textlite' type='text' name='name' size='20' onchange='highlightUpdate()' /></td>
 	<td class='pad'><input class='textlite' type='text' name='email' size='20' onchange='highlightUpdate()' /></td>
-	<td class='pad'><input class='button' type='submit' name='update' value='Add contact author' /></td>
+	<td class='pad'><input class='button' type='submit' name='add' value='Add contact author' /></td>
     </tr>
   </table></td>
 </tr>
