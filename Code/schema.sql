67d21c9f8b3930106753b37361d821c0e9955f50
Eddie Kohler <ekohler@gmail.com>
2009-01-26 08:41:05 -0800

    Set schema version number correctly.

--- Code/schema.sql
+++ Code/schema.sql
@@ -479,7 +479,7 @@ CREATE TABLE `TopicInterest` (
 
 delete from Settings where name='setupPhase';
 insert into Settings (name, value) values ('setupPhase', 1);
-insert into Settings (name, value) values ('allowPaperOption', 24);
+insert into Settings (name, value) values ('allowPaperOption', 25);
 # collect PC conflicts from authors by default, but not collaborators
 insert into Settings (name, value) values ('sub_pcconf', 1);
 # default chair-only tags
