c108a29458582c4e1e6df819856bf0ec5903d15e
Eddie Kohler <ekohler@gmail.com>
2009-12-09 15:34:23 -0800

    Remove old debugging error_log.

--- review.php
+++ review.php
@@ -15,7 +15,6 @@ if (isset($_REQUEST["email"]) && isset($_REQUEST["password"])
     foreach (array("paperId", "reviewId", "commentId", "p", "r", "c", "accept", "refuse") as $opt)
 	if (isset($_REQUEST[$opt]))
 	    $after .= ($after === "" ? "" : "&") . $opt . "=" . urlencode($_REQUEST[$opt]);
-    error_log(("index$ConfSiteSuffix?email=" . urlencode($_REQUEST["email"]) . "&password=" . urlencode($_REQUEST["password"]) . "&go=" . urlencode("review$ConfSiteSuffix?$after")));
     $Me->go("index$ConfSiteSuffix?email=" . urlencode($_REQUEST["email"]) . "&password=" . urlencode($_REQUEST["password"]) . "&go=" . urlencode("review$ConfSiteSuffix?$after"));
 }
 
