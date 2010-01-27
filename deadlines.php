92b81426140604a2e3952339c30aad8c40f2d070
Eddie Kohler <ekohler@gmail.com>
2010-01-27 11:34:58 -0800

    Update copyright year.

--- deadlines.php
+++ deadlines.php
@@ -1,6 +1,6 @@
 <?php
 // deadlines.php -- HotCRP deadline reporting page
-// HotCRP is Copyright (c) 2006-2009 Eddie Kohler and Regents of the UC
+// HotCRP is Copyright (c) 2006-2010 Eddie Kohler and Regents of the UC
 // Distributed under an MIT-like license; see LICENSE
 
 require_once("Code/header.inc");
--- help.php
+++ help.php
@@ -1,6 +1,6 @@
 <?php
 // help.php -- HotCRP help page
-// HotCRP is Copyright (c) 2006-2009 Eddie Kohler and Regents of the UC
+// HotCRP is Copyright (c) 2006-2010 Eddie Kohler and Regents of the UC
 // Distributed under an MIT-like license; see LICENSE
 
 require_once("Code/header.inc");
