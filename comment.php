kohler/hotcrp

commit e1d0d111ce28332997122bd9692132c98d94b618

Redirect after submitting most POST forms (and some GET forms).
This gives a better UI since reload works, and avoids problems with AJAX
checkboxes being overridden by URL parameters.

comment.php
@@ -235,6 +235,11 @@ function saveComment($text) {
 	unset($_REQUEST["commentId"]);
     $_REQUEST["noedit"] = 1;
 
+    if ($text != "")
+	redirectSelf(array("anchor" => "comment$savedCommentId"));
+    else
+	redirectSelf();
+
     return ($crow ? false : true);
 }
 
@@ -251,11 +256,11 @@ function saveResponse($text) {
     return saveComment($text);
 }
 
-if (isset($_REQUEST['submit']) && defval($_REQUEST, 'response')) {
+if (isset($_REQUEST["submit"]) && defval($_REQUEST, "response")) {
     if (!$Me->canRespond($prow, $crow, $whyNot, true)) {
-	$Conf->errorMsg(whyNotText($whyNot, "respond"));
+	$Conf->errorMsg(whyNotText($whyNot, "respond to reviews for"));
 	$useRequest = true;
-    } else if (!($text = defval($_REQUEST, 'comment')) && !$crow) {
+    } else if (!($text = defval($_REQUEST, "comment")) && !$crow) {
 	$Conf->errorMsg("Enter a comment.");
 	$useRequest = true;
     } else {
@@ -265,11 +270,11 @@ if (isset($_REQUEST['submit']) && defval($_REQUEST, 'response')) {
 	loadRows();
 	watch($newComment);
     }
-} else if (isset($_REQUEST['submit'])) {
+} else if (isset($_REQUEST["submit"])) {
     if (!$Me->canSubmitComment($prow, $crow, $whyNot)) {
-	$Conf->errorMsg(whyNotText($whyNot, "comment"));
+	$Conf->errorMsg(whyNotText($whyNot, "comment on"));
 	$useRequest = true;
-    } else if (!($text = defval($_REQUEST, 'comment')) && !$crow) {
+    } else if (!($text = defval($_REQUEST, "comment")) && !$crow) {
 	$Conf->errorMsg("Enter a comment.");
 	$useRequest = true;
     } else {
@@ -277,9 +282,9 @@ if (isset($_REQUEST['submit']) && defval($_REQUEST, 'response')) {
 	loadRows();
 	watch($newComment);
     }
-} else if (isset($_REQUEST['delete']) && $crow) {
+} else if (isset($_REQUEST["delete"]) && $crow) {
     if (!$Me->canSubmitComment($prow, $crow, $whyNot)) {
-	$Conf->errorMsg(whyNotText($whyNot, "comment"));
+	$Conf->errorMsg(whyNotText($whyNot, "comment on"));
 	$useRequest = true;
     } else {
 	saveComment("");

paper.php
@@ -570,6 +570,7 @@ function updatePaper($Me, $isSubmit, $isSubmitFinal) {
     }
 
     $Conf->log($actiontext, $Me, $paperId);
+    redirectSelf();
     return true;
 }
 
@@ -597,10 +598,8 @@ if (isset($_REQUEST["update"]) || isset($_REQUEST["submitfinal"])) {
 	else
 	    $action = ($newPaper ? "register" : "update");
 	$Conf->errorMsg(whyNotText($whyNot, $action));
-    } else if (updatePaper($Me, isset($_REQUEST["submit"]), isset($_REQUEST["submitfinal"]))) {
-	if ($newPaper)
-	    $Conf->go("paper$ConfSiteSuffix?p=$paperId&m=pe");
-    }
+    } else if (updatePaper($Me, isset($_REQUEST["submit"]), isset($_REQUEST["submitfinal"])))
+	redirectSelf(array("p" => $paperId, "m" => "pe"));
 
     // use request?
     $useRequest = ($ok || $Me->privChair);

review.php
@@ -170,6 +170,7 @@ if (isset($_REQUEST['update'])) {
     } else if ($rf->checkRequestFields($_REQUEST, $paperTable->editrrow)) {
 	if ($rf->saveRequest($_REQUEST, $paperTable->editrrow, $prow)) {
 	    $Conf->confirmMsg(defval($_REQUEST, "ready", false) && !defval($_REQUEST, "unready", false) ? "Review submitted." : "Review saved.  However, this version is marked as not ready for others to see.  Please finish the review and submit again.");
+	    redirectSelf();
 	    loadRows();
 	} else
 	    $useRequest = true;

search.php
@@ -933,6 +933,8 @@ if (isset($_REQUEST["scoresort"]) && isset($scoreSorts[$_REQUEST["scoresort"]]))
     $_SESSION["scoresort"] = $_REQUEST["scoresort"];
 if (!isset($_SESSION["scoresort"]))
     $_SESSION["scoresort"] = $Conf->settingText("scoresort_default", $defaultScoreSort);
+if (isset($_REQUEST["redisplay"]))
+    redirectSelf(array("tab" => "display"));
 
 
 // save display options

commit 9909e9ab337a9ee8fc0ae675215e2df7737ba6b1

Bug fix: Actually send email to users "watching" a paper's comments.
And improve some other behaviors broken in the switch from POST to GET.

This bug was introduced in commit e1d0d111ce28332997122bd9692132c98d94b618.

Code/helpers.inc
@@ -175,7 +175,7 @@ function selfHref($extra = array(), $self = null, $htmlspecialchars = true) {
 	$_REQUEST["r"] = $_REQUEST["reviewId"];
     if (isset($_REQUEST["commentId"]) && !isset($_REQUEST["c"]))
 	$_REQUEST["c"] = $_REQUEST["commentId"];
-    foreach (array("p", "r", "c", "m", "mode", "forceShow", "validator", "ls", "list", "t", "q", "qo", "qx", "qt", "tab", "atab", "group", "sort", "monreq") as $what)
+    foreach (array("p", "r", "c", "m", "mode", "forceShow", "validator", "ls", "list", "t", "q", "qo", "qx", "qt", "tab", "atab", "group", "sort", "monreq", "noedit") as $what)
 	if (isset($_REQUEST[$what]) && !isset($extra[$what])) {
 	    $x .= "$sep$what=" . urlencode($_REQUEST[$what]);
 	    $sep = "&";

comment.php
@@ -90,7 +90,7 @@ function setReviewInfo($dst, $src) {
     $dst->conflictType = $src->conflictType;
 }
 
-function watch($newComment) {
+function watch() {
     global $Conf, $Me, $prow, $savedCrow;
 
     $apo = $Conf->sversion;
@@ -146,7 +146,7 @@ function watch($newComment) {
 
 
 // update comment action
-function saveComment($text) {
+function saveComment($text, $locked) {
     global $Me, $Conf, $prow, $crow, $savedCommentId;
 
     // options
@@ -202,6 +202,10 @@ function saveComment($text) {
     else if (!($savedCommentId = $Conf->lastInsertId($while)))
 	return false;
 
+    // we are done saving the comment; unlock tables
+    if ($locked)
+	$Conf->q("unlock tables");
+
     // log, end
     $what = ($forAuthors > 1 ? "Response" : "Comment");
     if ($text != "" && !isset($_SESSION["comment_msgs"]))
@@ -222,7 +226,6 @@ function saveComment($text) {
 
     // adjust comment counts
     if ($change) {
-	$Conf->q("unlock tables");	// just in case
 	// see also account.php:delete user
 	$Conf->qe("update Paper set numComments=(select count(commentId) from PaperComment where paperId=$prow->paperId), numAuthorComments=(select count(commentId) from PaperComment where paperId=$prow->paperId and forAuthors>0) where paperId=$prow->paperId", $while);
     }
@@ -235,12 +238,14 @@ function saveComment($text) {
 	unset($_REQUEST["commentId"]);
     $_REQUEST["noedit"] = 1;
 
-    if ($text != "")
+    loadRows();
+    if ($text != "") {
+	watch();
 	redirectSelf(array("anchor" => "comment$savedCommentId"));
-    else
+    } else
 	redirectSelf();
-
-    return ($crow ? false : true);
+    // NB normally redirectSelf() does not return
+    return true;
 }
 
 function saveResponse($text) {
@@ -253,7 +258,10 @@ function saveResponse($text) {
 	    return $Conf->errorMsg("A paper response has already been entered.  <a href=\"comment$ConfSiteSuffix?c=$row[0]$linkExtra\">Edit that response</a>");
     }
 
-    return saveComment($text);
+    $Conf->qe("lock tables Paper write, PaperComment write, ActionLog write");
+    $success = saveComment($text, true);
+    if (!$success)
+	$Conf->qe("unlock tables");
 }
 
 if (isset($_REQUEST["submit"]) && defval($_REQUEST, "response")) {
@@ -263,13 +271,8 @@ if (isset($_REQUEST["submit"]) && defval($_REQUEST, "response")) {
     } else if (!($text = defval($_REQUEST, "comment")) && !$crow) {
 	$Conf->errorMsg("Enter a comment.");
 	$useRequest = true;
-    } else {
-	$Conf->qe("lock tables Paper write, PaperComment write, ActionLog write");
-	$newComment = saveResponse($text);
-	$Conf->qe("unlock tables");
-	loadRows();
-	watch($newComment);
-    }
+    } else
+	saveResponse($text);
 } else if (isset($_REQUEST["submit"])) {
     if (!$Me->canSubmitComment($prow, $crow, $whyNot)) {
 	$Conf->errorMsg(whyNotText($whyNot, "comment on"));
@@ -277,19 +280,14 @@ if (isset($_REQUEST["submit"]) && defval($_REQUEST, "response")) {
     } else if (!($text = defval($_REQUEST, "comment")) && !$crow) {
 	$Conf->errorMsg("Enter a comment.");
 	$useRequest = true;
-    } else {
-	$newComment = saveComment($text);
-	loadRows();
-	watch($newComment);
-    }
+    } else
+	saveComment($text, false);
 } else if (isset($_REQUEST["delete"]) && $crow) {
     if (!$Me->canSubmitComment($prow, $crow, $whyNot)) {
 	$Conf->errorMsg(whyNotText($whyNot, "comment on"));
 	$useRequest = true;
-    } else {
-	saveComment("");
-	loadRows();
-    }
+    } else
+	saveComment("", false);
 } else if (isset($_REQUEST["cancel"]) && $crow)
     $_REQUEST["noedit"] = 1;
 

paper.php
@@ -571,6 +571,7 @@ function updatePaper($Me, $isSubmit, $isSubmitFinal) {
 
     $Conf->log($actiontext, $Me, $paperId);
     redirectSelf();
+    // NB normally redirectSelf() does not return
     return true;
 }
 
@@ -598,8 +599,10 @@ if (isset($_REQUEST["update"]) || isset($_REQUEST["submitfinal"])) {
 	else
 	    $action = ($newPaper ? "register" : "update");
 	$Conf->errorMsg(whyNotText($whyNot, $action));
-    } else if (updatePaper($Me, isset($_REQUEST["submit"]), isset($_REQUEST["submitfinal"])))
+    } else if (updatePaper($Me, isset($_REQUEST["submit"]), isset($_REQUEST["submitfinal"]))) {
 	redirectSelf(array("p" => $paperId, "m" => "pe"));
+	// NB normally redirectSelf() does not return
+    }
 
     // use request?
     $useRequest = ($ok || $Me->privChair);

review.php
@@ -170,6 +170,7 @@ if (isset($_REQUEST['update'])) {
 	if ($rf->saveRequest($_REQUEST, $paperTable->editrrow, $prow)) {
 	    $Conf->confirmMsg(defval($_REQUEST, "ready", false) && !defval($_REQUEST, "unready", false) ? "Review submitted." : "Review saved.  However, this version is marked as not ready for others to see.  Please finish the review and submit again.");
 	    redirectSelf();
+	    // NB normally redirectSelf() does not return
 	    loadRows();
 	} else
 	    $useRequest = true;