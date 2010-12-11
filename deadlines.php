a50fce055349627b0632b2a484eb8c1f488f0880
Eddie Kohler <ekohler@gmail.com>
2010-12-10 16:14:04 -0800

    Deadlines page visual improvements.

--- deadlines.php
+++ deadlines.php
@@ -31,66 +31,47 @@ if ($Me->privChair)
 
 echo "</p>
 
-<table>
-<tr><th></th><th>Deadline</th><th>Time&nbsp;left</th></tr>\n";
+<dl>\n";
 
 
-function printableInterval($amt) {
+function printDeadline($dl, $name, $phrase, $description) {
     global $Conf;
-    return $amt > 0 ? "less than " . $Conf->printableInterval($amt) : "past";
+    echo "<dt><strong>", $phrase, "</strong>: ", $Conf->printableTime($dl[$name], "span") , "</dt>\n",
+	"<dd>", $description, ($description ? "<br />" : "");
+    if ($dl[$name] > $dl["now"])
+	echo "<strong>Time left</strong>: less than " . $Conf->printableInterval($dl[$name] - $dl["now"]);
+    echo "</dd>\n";
 }
 
-if (defval($dl, "sub_reg")) {
-    echo "<tr><td class='rcaption nowrap'>Paper registration deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["sub_reg"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["sub_reg"] - $dl["now"]), "</td>";
-    echo "<td>You can register new papers until this deadline.</td></tr>\n";
-}
+if (defval($dl, "sub_reg"))
+    printDeadline($dl, "sub_reg", "Paper registration deadline",
+		  "You can register new papers until this deadline.");
 
-if (defval($dl, "sub_update")) {
-    echo "<tr><td class='rcaption nowrap'>Paper update deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["sub_update"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["sub_update"] - $dl["now"]), "</td>";
-    echo "<td>You can upload new versions of your paper and change other paper information until this deadline.</td></tr>\n";
-}
+if (defval($dl, "sub_update"))
+    printDeadline($dl, "sub_update", "Paper update deadline",
+		  "You can upload new versions of your paper and change other paper information until this deadline.");
 
-if ($dl["sub_sub"]) {
-    echo "<tr><td class='rcaption nowrap'>Paper submission deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["sub_sub"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["sub_sub"] - $dl["now"]), "</td>";
-    echo "<td>Only papers submitted by this deadline will be reviewed.</td></tr>\n";
-}
+if (defval($dl, "sub_sub"))
+    printDeadline($dl, "sub_sub", "Paper submission deadline",
+		  "Papers must be submitted by this deadline to be reviewed.");
 
-if ($dl["resp_open"] && $dl["resp_done"]) {
-    echo "<tr><td class='rcaption nowrap'>Response deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["resp_done"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["resp_done"] - $dl["now"]), "</td>";
-    echo "<td>This deadline controls when you can submit a response to the reviews.</td></tr>\n";
-}
+if ($dl["resp_open"] && $dl["resp_done"])
+    printDeadline($dl, "resp_done", "Response deadline",
+		  "This deadline controls when you can submit a response to the reviews.");
 
-if ($dl["rev_open"] && defval($dl, "pcrev_done") && !defval($dl, "pcrev_ishard")) {
-    echo "<tr><td class='rcaption nowrap'>PC review deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["pcrev_done"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["pcrev_done"] - $dl["now"]), "</td>";
-    echo "<td>Reviews are requested by this deadline.</td></tr>\n";
-} else if ($dl["rev_open"] && defval($dl, "pcrev_done")) {
-    echo "<tr><td class='rcaption nowrap'>PC review hard deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["pcrev_done"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["pcrev_done"] - $dl["now"]), "</td>";
-    echo "<td>This deadline controls when you can submit or change your reviews.</td></tr>\n";
-}
+if ($dl["rev_open"] && defval($dl, "pcrev_done") && !defval($dl, "pcrev_ishard"))
+    printDeadline($dl, "pcrev_done", "PC review deadline",
+		  "Reviews are requested by this deadline.");
+else if ($dl["rev_open"] && defval($dl, "pcrev_done"))
+    printDeadline($dl, "pcrev_done", "PC review hard deadline",
+		  "This deadline controls when you can submit or change your reviews.");
 
-if ($dl["rev_open"] && defval($dl, "extrev_done") && !defval($dl, "extrev_ishard")) {
-    echo "<tr><td class='rcaption nowrap'>External review deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["extrev_done"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["extrev_done"] - $dl["now"]), "</td>";
-    echo "<td>Reviews are requested by this deadline.</td></tr>\n";
-} else if ($dl["rev_open"] && defval($dl, "extrev_done")) {
-    echo "<tr><td class='rcaption nowrap'>External review hard deadline</td>";
-    echo "<td class='nowrap entry'>", $Conf->printableTime($dl["extrev_done"], "div"), "</td>";
-    echo "<td class='nowrap entry'>", printableInterval($dl["extrev_done"] - $dl["now"]), "</td>";
-    echo "<td>This deadline controls when you can submit or change your reviews.</td></tr>\n";
-}
+if ($dl["rev_open"] && defval($dl, "extrev_done") && !defval($dl, "extrev_ishard"))
+    printDeadline($dl, "extrev_done", "External review deadline",
+		  "Reviews are requested by this deadline.");
+else if ($dl["rev_open"] && defval($dl, "extrev_done"))
+    printDeadline($dl, "extrev_done", "External review hard deadline",
+		  "This deadline controls when you can submit or change your reviews.");
 
 echo "</table>\n";
 
