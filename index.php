kohler/hotcrp

commit f2279337e45d90ddb99bbf0dcda5e9b4f1d67d78

Cross-site request forgery protection.
Requests that modify state should now be labeled with hoturl_post(). They
should then check_post() before saving the data. hoturl_post() sets the
"post" request variable to a random string derived from the session ID;
check_post() checks that variable is present.

Most likely this breaks some UI, but I tested most of them, and they seem
OK. "Oh well."

Taesoo Kim asked about this. In an excruciating meeting; might as well
use time well.

Code/paperlist.inc
@@ -89,6 +89,7 @@ class PaperList extends BaseList {
     var $formulas;
     var $viewmap;
     var $reviewList;
+    var $atab;
 
     function __construct($search, $args = array()) {
 	global $scoreSorts, $defaultScoreSort, $Conf;
@@ -136,6 +137,8 @@ class PaperList extends BaseList {
 	    $this->display = defval($_SESSION, defval($args, "foldtype", "pl") . "display", "");
 	$this->rf = reviewForm();
 	$this->formulas = array();
+	$this->atab = defval($_REQUEST, "atab", "");
+	unset($_REQUEST["atab"]);
     }
 
     function _normalizeField($fieldId) {
@@ -1130,7 +1133,7 @@ class PaperList extends BaseList {
 	$want_plactions_dofold = false;
 
 	// Download
-	if (defval($_REQUEST, "atab") == "download")
+	if ($this->atab == "download")
 	    $whichlll = $nlll;
 	$t = "<td class='lll$nlll nowrap'><a href=\"" . selfHref(array("atab" => "download")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Download</a></td><td class='lld$nlll nowrap'><b>:</b> &nbsp;";
 	$sel_opt = array();
@@ -1179,8 +1182,7 @@ class PaperList extends BaseList {
 
 	// Upload preferences (review preferences only)
 	if ($revpref) {
-	    if (isset($_REQUEST["upload"])
-		|| defval($_REQUEST, "atab") == "uploadpref")
+	    if (isset($_REQUEST["upload"]) || $this->atab == "uploadpref")
 		$whichlll = $nlll;
 	    $t .= $barsep;
 	    $t .= "<td class='lll$nlll nowrap'><a href=\"" . selfHref(array("atab" => "uploadpref")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Upload</a></td><td class='lld$nlll nowrap'><b>&nbsp;preference file:</b> &nbsp;";
@@ -1190,8 +1192,7 @@ class PaperList extends BaseList {
 
 	// Set preferences (review preferences only)
 	if ($revpref) {
-	    if (isset($_REQUEST["setpaprevpref"])
-		|| defval($_REQUEST, "atab") == "setpref")
+	    if (isset($_REQUEST["setpaprevpref"]) || $this->atab == "setpref")
 		$whichlll = $nlll;
 	    $t .= $barsep;
 	    $t .= "<td class='lll$nlll nowrap'><a href=\"" . selfHref(array("atab" => "setpref")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Set preferences</a></td><td class='lld$nlll nowrap'><b>:</b> &nbsp;";
@@ -1201,8 +1202,7 @@ class PaperList extends BaseList {
 
 	// Tags (search+PC only)
 	if ($this->contact->isPC && !$revpref) {
-	    if (isset($_REQUEST["tagact"])
-		|| defval($_REQUEST, "atab") == "tags")
+	    if (isset($_REQUEST["tagact"]) || $this->atab == "tags")
 		$whichlll = $nlll;
 	    $t .= $barsep;
 	    $t .= "<td class='lll$nlll nowrap'><a href=\"" . selfHref(array("atab" => "tags")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Tag</a></td><td class='lld$nlll nowrap'><table id='foldplacttags' class='foldc fold99c'><tr><td><b>:</b><a class='help' href='" . hoturl("help", "t=tags") . "' target='_blank' title='Learn more'>?</a> &nbsp;";
@@ -1239,8 +1239,7 @@ class PaperList extends BaseList {
 
 	// Assignments (search+admin only)
 	if ($this->contact->privChair && !$revpref) {
-	    if (isset($_REQUEST["setassign"])
-		|| defval($_REQUEST, "atab") == "assign")
+	    if (isset($_REQUEST["setassign"]) || $this->atab == "assign")
 		$whichlll = $nlll;
 	    $t .= $barsep;
 	    $t .= "<td class='lll$nlll'><a href=\"" . selfHref(array("atab" => "assign")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Assign</a></td><td id='foldass' class='lld$nlll foldo'><b>:</b> &nbsp;";
@@ -1275,15 +1274,14 @@ class PaperList extends BaseList {
 
 	// Decide, Mail (search+admin only)
 	if ($this->contact->privChair && !$revpref) {
-	    if (defval($_REQUEST, "atab") == "decide")
+	    if ($this->atab == "decide")
 		$whichlll = $nlll;
 	    $t .= $barsep;
 	    $t .= "<td class='lll$nlll'><a href=\"" . selfHref(array("atab" => "decide")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Decide</a></td><td class='lld$nlll'><b>:</b> Set to &nbsp;";
-	    $t .= decisionSelector(0, "plact${nlll}_d") . " &nbsp;<input type='submit' class='b' name='setdecision' value='Go' /></td>";
+	    $t .= decisionSelector(defval($_REQUEST, "decision", 0), "plact${nlll}_d") . " &nbsp;<input type='submit' class='b' name='setdecision' value='Go' /></td>";
 	    $nlll++;
 
-	    if (isset($_REQUEST["sendmail"])
-		|| defval($_REQUEST, "atab") == "mail")
+	    if (isset($_REQUEST["sendmail"]) || $this->atab == "mail")
 		$whichlll = $nlll;
 	    $t .= $barsep
 		. "<td class='lll$nlll'><a href=\"" . selfHref(array("atab" => "mail")) . "#plact\" onclick='return crpfocus(\"plact\",$nlll)'>Mail</a></td><td class='lld$nlll'><b>:</b> &nbsp;"

index.php
@@ -196,7 +196,8 @@ function doLogin() {
     exit;
 }
 
-if (isset($_REQUEST["email"]) && isset($_REQUEST["action"]) && isset($_REQUEST["signin"])) {
+if (isset($_REQUEST["email"]) && isset($_REQUEST["action"])
+    && isset($_REQUEST["signin"])) {
     if (doLogin() !== true) {
 	// if we get here, login failed
 	$Me->invalidate();

search.php
@@ -22,7 +22,7 @@ else if (isset($_REQUEST["default"]))
 // paper group
 $tOpt = PaperSearch::searchTypes($Me);
 if (count($tOpt) == 0) {
-    $Conf->header("Search", 'search', actionBar());
+    $Conf->header("Search", "search", actionBar());
     $Conf->errorMsg("You are not allowed to search for papers.");
     exit;
 }
@@ -347,7 +347,8 @@ function tagaction() {
 	redirectSelf($args);
     }
 }
-if (isset($_REQUEST["tagact"]) && $Me->isPC && isset($papersel) && isset($_REQUEST["tag"]))
+if (isset($_REQUEST["tagact"]) && $Me->isPC && isset($papersel)
+    && isset($_REQUEST["tag"]) && check_post())
     tagaction();
 
 
@@ -746,7 +747,8 @@ if ($getaction == "checkformat" && $Me->privChair && isset($papersel)) {
 
 
 // set outcome for selected papers
-if (isset($_REQUEST["setdecision"]) && defval($_REQUEST, "decision", "") != "" && isset($papersel))
+if (isset($_REQUEST["setdecision"]) && defval($_REQUEST, "decision", "") != ""
+    && isset($papersel) && check_post())
     if (!$Me->canSetOutcome(null))
 	$Conf->errorMsg("You cannot set paper decisions.");
     else {
@@ -755,13 +757,16 @@ if (isset($_REQUEST["setdecision"]) && defval($_REQUEST, "decision", "") != "" &
 	if (isset($rf->options['outcome'][$o])) {
 	    $Conf->qe("update Paper set outcome=$o where " . paperselPredicate($papersel), "while changing decision");
 	    $Conf->updatePaperaccSetting($o > 0);
+	    redirectSelf(array("atab" => "decide", "decision" => $o));
+	    // normally does not return
 	} else
 	    $Conf->errorMsg("Bad decision value!");
     }
 
 
 // mark conflicts/PC-authored papers
-if (isset($_REQUEST["setassign"]) && defval($_REQUEST, "marktype", "") != "" && isset($papersel)) {
+if (isset($_REQUEST["setassign"]) && defval($_REQUEST, "marktype", "") != ""
+    && isset($papersel) && check_post()) {
     $mt = $_REQUEST["marktype"];
     $mpc = defval($_REQUEST, "markpc", "");
     $pc = new Contact();
@@ -931,7 +936,8 @@ function saveformulas() {
     }
 }
 
-if (isset($_REQUEST["saveformulas"]) && $Me->isPC && $Conf->sversion >= 32)
+if (isset($_REQUEST["saveformulas"]) && $Me->isPC && $Conf->sversion >= 32
+    && check_post())
     saveformulas();
 
 
@@ -998,7 +1004,7 @@ function savesearch() {
 }
 
 if ((isset($_REQUEST["savesearch"]) || isset($_REQUEST["deletesearch"]))
-    && $Me->isPC) {
+    && $Me->isPC && check_post()) {
     savesearch();
     $_REQUEST["tab"] = "ss";
 }
@@ -1020,7 +1026,7 @@ if ($Me->privChair) {
 
 
 // search
-$Conf->header("Search", 'search', actionBar());
+$Conf->header("Search", "search", actionBar());
 unset($_REQUEST["urlbase"]);
 $Search = new PaperSearch($Me, $_REQUEST);
 if (isset($_REQUEST["q"])) {

Code/helpers.inc
@@ -373,7 +373,7 @@ function hoturl($page, $options = null) {
 }
 
 function hoturl_post($page, $options = null) {
-    $options = ($options ? $options . "&amp;" : "") . "post=1";
+    $options = ($options ? $options . "&amp;" : "") . "post=" . post_value();
     return hoturl($page, $options);
 }
 
@@ -428,6 +428,22 @@ function go($url) {
     exit();
 }
 
+function post_value() {
+    global $Conf;
+    $Conf->ensureSession();
+    if (($sid = session_id())) {
+	if (strlen($sid) > 16)
+	    $sid = substr($sid, 8);
+	$sid = substr($sid, 0, 8);
+    } else
+	$sid = "1";
+    return urlencode($sid);
+}
+
+function check_post() {
+    return isset($_REQUEST["post"]) && $_REQUEST["post"] == post_value();
+}
+
 function fileUploaded(&$var, $conf) {
     if (!isset($var) || ($var['error'] != UPLOAD_ERR_OK && !$conf))
 	return false;
@@ -458,7 +474,7 @@ function selfHref($extra = array(), $self = null, $htmlspecialchars = true) {
 	else if (($pos = strpos($x, ".php/")) !== false)
 	    $x = substr($x, 0, $pos) . substr($x, $pos + 4);
     }
-    $sep = "?";
+    $sep = (strpos($self, "?") === false ? "?" : "&");
     foreach (array("paperId" => "p", "pap" => "p", "reviewId" => "r", "commentId" => "c") as $k => $v)
 	if (isset($_REQUEST[$k]) && !isset($_REQUEST[$v]))
 	    $_REQUEST[$v] = $_REQUEST[$k];
@@ -1756,6 +1772,8 @@ function decisionSelector($curOutcome = 0, $id = null, $extra = "") {
     $text = "<select" . ($id === null ? "" : " id='$id'") . " name='decision'$extra>\n";
     $rf = reviewForm();
     $outcomeMap = $rf->options['outcome'];
+    if (!isset($outcomeMap[$curOutcome]))
+	$curOutcome = null;
     $outcomes = array_keys($outcomeMap);
     sort($outcomes);
     $outcomes = array_unique(array_merge(array(0), $outcomes));

Code/review.inc
@@ -1213,7 +1213,7 @@ $blind\n";
 		$ratesep = " &nbsp;<span class='barsep'>|</span>&nbsp; ";
 	    }
 	    if ($rrow->contactId != $Me->contactId) {
-		$ratinglink = hoturl("review", "r=$reviewOrdinal");
+		$ratinglink = hoturl_post("review", "r=$reviewOrdinal");
 		if (!isset($_REQUEST["reviewId"]))
 		    $ratinglink .= "&amp;allr=1";
 		echo $ratesep, "<form id='ratingform_$reviewOrdinal' action='$ratinglink$linkExtra' method='post' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>",

Code/reviewsetform.inc
@@ -85,7 +85,7 @@ function rf_update($lock) {
     } else
 	$_REQUEST['sample'] = 'none';
 
-    if (!isset($_REQUEST['update']))
+    if (!isset($_REQUEST["update"]) || !check_post())
 	return;
 
     $while = "while updating review form";

account.php
@@ -324,8 +324,10 @@ function parseBulkFile($text, $filename) {
 	$Conf->warnMsg("Nothing to do.");
 }
 
-if (isset($_REQUEST["register"]) && $newProfile
-    && fileUploaded($_FILES["bulk"], $Conf)) {
+if (!check_post())
+    /* do nothing */;
+else if (isset($_REQUEST["register"]) && $newProfile
+	 && fileUploaded($_FILES["bulk"], $Conf)) {
     if (($text = file_get_contents($_FILES["bulk"]["tmp_name"])) === false)
 	$Conf->errorMsg("Internal error: cannot read file.");
     else
@@ -345,7 +347,8 @@ if (isset($_REQUEST["register"]) && $newProfile
 	else
 	    redirectSelf();
     }
-} else if (isset($_REQUEST["merge"]) && !$newProfile && $Acct->contactId == $Me->contactId)
+} else if (isset($_REQUEST["merge"]) && !$newProfile
+	   && $Acct->contactId == $Me->contactId)
     $Me->go(hoturl("mergeaccounts"));
 
 function databaseTracks($who) {
@@ -389,7 +392,7 @@ function textArrayPapers($pids) {
     return commajoin(preg_replace('/(\d+)/', "<a href='" . hoturl("paper", "p=\$1$ls") . "'>\$1</a>", $pids));
 }
 
-if (isset($_REQUEST["delete"]) && $OK) {
+if (isset($_REQUEST["delete"]) && $OK && check_post()) {
     if (!$Me->privChair)
 	$Conf->errorMsg("Only administrators can delete users.");
     else if ($Acct->contactId == $Me->contactId)

assign.php
@@ -116,7 +116,8 @@ function retractRequest($reviewId, $lock = true, $confirm = true) {
 	$Conf->confirmMsg("Removed request that " . contactHtml($row) . " review paper #$prow->paperId.");
 }
 
-if (isset($_REQUEST['retract']) && ($retract = rcvtint($_REQUEST['retract'])) > 0) {
+if (isset($_REQUEST["retract"])
+    && ($retract = rcvtint($_REQUEST["retract"])) > 0 && check_post()) {
     retractRequest($retract, $prow->paperId);
     $Conf->qe("unlock tables");
     $Conf->updateRevTokensSetting(false);
@@ -175,7 +176,7 @@ function pcAssignments() {
     }
 }
 
-if (isset($_REQUEST['update']) && $Me->privChair) {
+if (isset($_REQUEST["update"]) && $Me->privChair && check_post()) {
     pcAssignments();
     $Conf->qe("unlock tables");
     $Conf->updateRevTokensSetting(false);
@@ -371,7 +372,7 @@ function createAnonymousReview() {
     return true;
 }
 
-if (isset($_REQUEST['add'])) {
+if (isset($_REQUEST["add"]) && check_post()) {
     if (!$Me->canRequestReview($prow, true, $whyNot))
 	$Conf->errorMsg(whyNotText($whyNot, "request reviews for"));
     else if (!isset($_REQUEST["email"]) || !isset($_REQUEST["name"]))
@@ -401,8 +402,8 @@ if (isset($_REQUEST['add'])) {
 
 
 // deny review request
-if (isset($_REQUEST['deny']) && $Me->privChair
-    && ($email = trim(defval($_REQUEST, 'email', "")))) {
+if (isset($_REQUEST["deny"]) && $Me->privChair && check_post()
+    && ($email = trim(defval($_REQUEST, "email", "")))) {
     $Conf->qe("lock tables ReviewRequest write, ContactInfo read, PaperConflict read, PaperReview read, PaperReviewRefused write");
     $while = "while denying review request";
     // Need to be careful and not expose inappropriate information:
@@ -430,7 +431,7 @@ if (isset($_REQUEST['deny']) && $Me->privChair
 
 
 // add primary or secondary reviewer
-if (isset($_REQUEST['addpc']) && $Me->privChair) {
+if (isset($_REQUEST["addpc"]) && $Me->privChair && check_post()) {
     if (($pcid = rcvtint($_REQUEST["pcid"])) <= 0)
 	$Conf->errorMsg("Enter a PC member.");
     else if (($pctype = rcvtint($_REQUEST["pctype"])) == REVIEW_PRIMARY
@@ -443,7 +444,7 @@ if (isset($_REQUEST['addpc']) && $Me->privChair) {
 
 
 // paper actions
-if (isset($_REQUEST["settingtags"])) {
+if (isset($_REQUEST["settingtags"]) && check_post()) {
     require_once("Code/paperactions.inc");
     PaperActions::setTags($prow);
     loadRows();

commit cd6a14a188c0e90759ac722234b6f57987be3672

Bug fix: All POST form requests use hoturl_post (fixes tag setting).
Bug introduced in f2279337e45d90ddb99bbf0dcda5e9b4f1d67d78 (cross-site
forgery protection). Bug reported by Alex Snoeren, Patrick Thiran, and
others.

Code/paperlist.inc
@@ -1434,7 +1434,7 @@ class PaperList extends BaseList {
 
     function _addAjaxLoadForm($pap, $extra = "") {
 	global $Conf;
-	$t = "<div><form id='plloadform' method='post' action='" . hoturl("search", "ajax=1" . $this->paperLinkArgs) . "' accept-charset='UTF-8'>"
+	$t = "<div><form id='plloadform' method='post' action='" . hoturl_post("search", "ajax=1" . $this->paperLinkArgs) . "' accept-charset='UTF-8'>"
 	    . "<div class='inform'>";
 	$s = $this->search;
 	if ($s->q)

Code/papertable.inc
@@ -1135,7 +1135,7 @@ class PaperTable {
 		echo "<div class='pscopen ${color}'>";
 
 	    if ($editable)
-		echo "<form method='post' action=\"$site\" enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>";
+		echo "<form method='post' action='", hoturl_post($site, "p=$prow->paperId$linkExtra") , "' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>";
 
 	    echo $this->papt("tags", "Tags", array("type" => "ps", "editfolder" => ($editable ? "tags" : 0), "oneditunfold" => "Miniajax.submit(\"tagreportform\")")),
 		"<div class='psv'>";
@@ -1466,7 +1466,7 @@ class PaperTable {
 	global $Conf, $Me, $forceShow, $linkExtra;
 	$prow = $this->prow;
 	if ($Me->canViewTags($prow, $forceShow))
-	    $this->papstripTags($prow, hoturl("review", "p=$prow->paperId$linkExtra"));
+	    $this->papstripTags($prow, "review");
 	if ($Me->canSetRank($prow))
 	    $this->papstripRank($prow);
 	$this->papstripWatch($prow);

index.php
@@ -379,7 +379,7 @@ Sign in to submit or review papers.";
     echo "</div>
 <hr class='home' />
 <div class='homegrp' id='homeacct'>
-<form method='post' action='", hoturl("index"), "' accept-charset='UTF-8'><div class='f-contain'>
+<form method='post' action='", hoturl_post("index"), "' accept-charset='UTF-8'><div class='f-contain'>
 <input type='hidden' name='cookie' value='1' />
 <div class='f-ii'>
   <div class='f-c", $email_class, "'>",
@@ -460,7 +460,7 @@ function reviewTokenGroup($close_hr) {
     foreach (defval($_SESSION, "rev_tokens", array()) as $tt)
 	$tokens[] = encodeToken((int) $tt);
     echo "  <h4>Review tokens: &nbsp;</h4> ",
-	"<form action='", hoturl("index"), "' method='post' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>",
+	"<form action='", hoturl_post("index"), "' method='post' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>",
 	"<input class='textlite' type='text' name='token' size='15' value=\"",
 	htmlspecialchars(join(" ", $tokens)), "\" />",
 	" &nbsp;<input class='b' type='submit' value='Go' />",

search.php
@@ -1325,7 +1325,7 @@ if ($Me->isPC || $Me->privChair) {
 if ($pl && $pl->count > 0) {
     echo "<div class='tld3' style='padding-bottom:1ex'>";
 
-    echo "<form id='foldredisplay' class='fn3 fold5c' method='post' action='", hoturl("search", "redisplay=1"), "' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>\n";
+    echo "<form id='foldredisplay' class='fn3 fold5c' method='post' action='", hoturl_post("search", "redisplay=1"), "' enctype='multipart/form-data' accept-charset='UTF-8'><div class='inform'>\n";
     echo_request_as_hidden_inputs();
 
     echo "<table>";