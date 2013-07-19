08b602aa6b486b8ec45634ca72470df7e3fae0d0
Eddie Kohler <ekohler@gmail.com>
2013-07-19 10:58:33 -0400

    To 2.60.

--- tarball.sh
+++ tarball.sh
@@ -1,4 +1,4 @@
-export VERSION=2.59
+export VERSION=2.60
 
 # check that schema.sql and updateschema.inc agree on schema version
 updatenum=`grep 'settings.*allowPaperOption.*=' Code/updateschema.inc | tail -n 1 | sed 's/.*= *//;s/;.*//'`
