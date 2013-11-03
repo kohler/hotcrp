568f3b4dc194b02a3529c294db68b1bcae4dfcb5
Eddie Kohler <ekohler@gmail.com>
2013-11-03 08:02:44 -0500

    Remove obsolete pages.
    And ensure that old versions of these pages won't be executed.

--- contactauthors.php
+++
@@ -1,7 +0,0 @@
-<?php
-// contactauthors.php -- HotCRP paper contact author management page
-// HotCRP is Copyright (c) 2006-2012 Eddie Kohler and Regents of the UC
-// Distributed under an MIT-like license; see LICENSE
-
-$_REQUEST["m"] = "pe";
-include("paper.php");
--- logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-// logout.php -- HotCRP logout page
-// HotCRP is Copyright (c) 2006-2007 Eddie Kohler and Regents of the UC
-// Distributed under an MIT-like license; see LICENSE
-
-require_once("Code/header.inc");
-$_REQUEST["signout"] = 1;
-include("index.php");
