6a851aa7b58298f84773c9a0b40b891fc18431de
Eddie Kohler <ekohler@gmail.com>
2013-12-29 14:09:35 -0500

    Nit

--- src/hotcrpdocument.php
+++ src/hotcrpdocument.php
@@ -1,5 +1,5 @@
 <?php
-// hotcrpdocument.inc -- document helper class for HotCRP papers
+// hotcrpdocument.php -- document helper class for HotCRP papers
 // HotCRP is Copyright (c) 2006-2013 Eddie Kohler and Regents of the UC
 // Distributed under an MIT-like license; see LICENSE
 
