33ff74da51ad50dc3727cf506674c3411185df22
Eddie Kohler <ekohler@gmail.com>
2014-01-21 15:43:04 -0500

    Begin test framework.
    The goal is to be able to run CLI tests with `php test/test.php`.

---
+++ test/testoptions.php
@@ -0,0 +1,19 @@
+<?php
+// testoptions.php -- HotCRP conference options for test databases
+
+global $Opt;
+$Opt["dbName"] = "hotcrp_testdb";
+$Opt["dbPassword"] = "m5LuaN23j26g";
+$Opt["shortName"] = "Testconf I";
+$Opt["longName"] = "Test Conference I";
+$Opt["paperSite"] = "http://hotcrp.lcdf.org/test/";
+$Opt["safePasswords"] = true;
+$Opt["passwordHmacKey"] = "6MFZ8fnvAudRVRn4CXsMNrqVjSvTZqrVBFLEBfxRvxvsEjWn";
+$Opt["conferenceKey"] = "g3aaENZnMNxn6PxcNHnbRPDac9EYTM3k42BY6fGbYWwefD8E";
+$Opt["contactName"] = "Eddie Kohler";
+$Opt["contactEmail"] = "ekohler@hotcrp.lcdf.org";
+$Opt["sendEmail"] = false;
+$Opt["debugShowSensitiveEmail"] = true;
+$Opt["emailFrom"] = "you@example.com";
+$Opt["disablePS"] = true;
+$Opt["smartScoreCompare"] = true;
---
+++ test/testsetup.php
@@ -0,0 +1,28 @@
+<?php
+// testsetup.php -- HotCRP helper file to initialize tests
+// HotCRP is Copyright (c) 2006-2014 Eddie Kohler and Regents of the UC
+// Distributed under an MIT-like license; see LICENSE
+
+global $ConfSitePATH;
+$ConfSitePATH = preg_replace(",/[^/]+/[^/]+$,", "", __FILE__);
+
+global $Opt;
+$Opt = array();
+if ((@include "$ConfSitePATH/test/testoptions.php") === false)
+    die("* Can't load test/testoptions.php.\n");
+$Opt["loaded"] = true;
+
+global $Conf;
+require_once("$ConfSitePATH/lib/base.php");
+require_once("$ConfSitePATH/lib/redirect.php");
+require_once("$ConfSitePATH/src/helpers.php");
+require_once("$ConfSitePATH/src/conference.php");
+$Opt["dsn"] = Conference::make_dsn($Opt);
+$Conf = new Conference($Opt["dsn"]);
+if (!$Conf->dblink)
+    die("* Can't load database " . $Opt["dsn"] . " specified by test/testoptions.php.\n"
+        . "* Run `lib/createdb.sh -c test/testoptions.php` to create the database.\n");
+
+// Initialize from an empty database.
+if (!$Conf->dblink->multi_query(file_get_contents("$ConfSitePATH/src/schema.sql")))
+    die("* Can't reinitialize database.\n" . $Conf->dblink->error);
