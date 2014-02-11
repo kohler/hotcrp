kohler/hotcrp

commit 80c36df2ed4695bd200d0997dcf040e74a8ef35c

Move BLIND settings into Conference as constants.

src/contact.php
@@ -723,10 +723,10 @@ class Contact {
 
             // check blindness
             $bs = $Conf->setting("sub_blind");
-            $ci->nonblind = $bs == BLIND_NEVER
-                || ($bs == BLIND_OPTIONAL
+            $ci->nonblind = $bs == Conference::BLIND_NEVER
+                || ($bs == Conference::BLIND_OPTIONAL
                     && !(isset($prow->paperBlind) ? $prow->paperBlind : $prow->blind))
-                || ($bs == BLIND_UNTILREVIEW
+                || ($bs == Conference::BLIND_UNTILREVIEW
                     && $ci->review_type > 0
                     && $ci->review_submitted > 0)
                 || ($prow->outcome > 0
@@ -1482,7 +1482,7 @@ class Contact {
                 && $prow->review_not_incomplete($this)
 		&& ($rights->allow_core_pc
                     || $Conf->settings["extrev_view"] >= 2))
-	    || !reviewBlind($rrow))
+	    || !$Conf->is_review_blind($rrow))
 	    return true;
 	return false;
     }
@@ -1512,9 +1512,7 @@ class Contact {
 	    || $rights->allow_core_pc
 	    || ($rights->allow_review
 		&& $Conf->settings["extrev_view"] >= 2)
-            || ($br = $Conf->blindReview()) == BLIND_NEVER
-            || ($br == BLIND_OPTIONAL && $crow
-                && !($crow->commentType & COMMENTTYPE_BLIND)))
+            || $Conf->is_review_blind(!$crow || ($crow->commentType & COMMENTTYPE_BLIND) != 0))
 	    return true;
 	return false;
     }

Code/header.inc
@@ -53,11 +53,6 @@ define("SEEDEC_ALL", 2);
 define("SEEDEC_REV", 1);
 define("SEEDEC_ADMIN", 0);
 
-define("BLIND_UNTILREVIEW", 3);
-define("BLIND_ALWAYS", 2);	/* ordering is important for rev/cmt */
-define("BLIND_OPTIONAL", 1);
-define("BLIND_NEVER", 0);
-
 define("DTYPE_SUBMISSION", 0);
 define("DTYPE_FINAL", -1);
 define("DTYPE_COMMENT", -2);

comment.php
@@ -115,9 +115,7 @@ function saveComment($text) {
         $ctype = COMMENTTYPE_REVIEWER;
     if (isset($_REQUEST["response"])
         ? $prow->blind
-        : ($Conf->blindReview() > BLIND_OPTIONAL
-           || ($Conf->blindReview() == BLIND_OPTIONAL
-               && defval($_REQUEST, "blind"))))
+        : $Conf->is_review_blind(!!@$_REQUEST["blind"]))
         $ctype |= COMMENTTYPE_BLIND;
 
     // backwards compatibility

settings.php
@@ -1143,7 +1143,10 @@ function doSubGroup() {
 
     echo "<div class='g'></div>\n";
     echo "<strong>Blind submission:</strong> Are author names hidden from reviewers?<br />\n";
-    doRadio("sub_blind", array(BLIND_ALWAYS => "Yes&mdash;submissions are anonymous", BLIND_NEVER => "No&mdash;author names are visible to reviewers", BLIND_UNTILREVIEW => "Blind until review&mdash;author names become visible after review submission", BLIND_OPTIONAL => "Depends&mdash;authors decide whether to expose their names"));
+    doRadio("sub_blind", array(Conference::BLIND_ALWAYS => "Yes—submissions are anonymous",
+                               Conference::BLIND_NEVER => "No—author names are visible to reviewers",
+                               Conference::BLIND_UNTILREVIEW => "Blind until review—author names become visible after review submission",
+                               Conference::BLIND_OPTIONAL => "Depends—authors decide whether to expose their names"));
 
     echo "<div class='g'></div>\n<table>\n";
     doDateRow("sub_reg", "Paper registration deadline", "sub_sub");
@@ -1426,7 +1429,9 @@ function doRevGroup() {
 
     echo "<div class='g'></div>\n";
     echo "<strong>Review anonymity:</strong> Are reviewer names hidden from authors?<br />\n";
-    doRadio("rev_blind", array(BLIND_ALWAYS => "Yes&mdash;reviews are anonymous", BLIND_NEVER => "No&mdash;reviewer names are visible to authors", BLIND_OPTIONAL => "Depends&mdash;reviewers decide whether to expose their names"));
+    doRadio("rev_blind", array(Conference::BLIND_ALWAYS => "Yes—reviews are anonymous",
+                               Conference::BLIND_NEVER => "No—reviewer names are visible to authors",
+                               Conference::BLIND_OPTIONAL => "Depends—reviewers decide whether to expose their names"));
 
     echo "<div class='g'></div>\n";
     doCheckbox('rev_notifychair', 'PC chairs are notified of new reviews by email');

src/commentview.php
@@ -187,9 +187,9 @@ class CommentView {
     <td><table id='foldcmtvis' class='fold2o'>";
         $ctype = $crow ? $crow->commentType : COMMENTTYPE_REVIEWER | COMMENTTYPE_BLIND;
 	echo "<tr><td>", Ht::radio_h("visibility", "a", ($useRequest ? defval($_REQUEST, "visibility") == "a" : $ctype >= COMMENTTYPE_AUTHOR), array("id" => "cmtvis_a", "onchange" => "docmtvis(this)")),
-	    "&nbsp;</td><td>", Ht::label("Authors and reviewers" . ($Conf->blindReview() == BLIND_ALWAYS ? " (anonymous to authors)" : ""));
+	    "&nbsp;</td><td>", Ht::label("Authors and reviewers" . ($Conf->review_blindness() == Conference::BLIND_ALWAYS ? " (anonymous to authors)" : ""));
 	// blind?
-	if ($Conf->blindReview() == BLIND_OPTIONAL)
+	if ($Conf->review_blindness() == Conference::BLIND_OPTIONAL)
 	    echo " &nbsp; (",
 		Ht::checkbox_h("blind", 1, ($useRequest ? defval($_REQUEST, "blind") : $ctype & COMMENTTYPE_BLIND)),
 		"&nbsp;", Ht::label("Anonymous to authors"), ")";

src/conference.php
@@ -21,6 +21,11 @@ class Conference {
     private $footerMap = null;
     private $usertimeId = 1;
 
+    const BLIND_NEVER = 0;
+    const BLIND_OPTIONAL = 1;
+    const BLIND_ALWAYS = 2;
+    const BLIND_UNTILREVIEW = 3;
+
     function __construct($dsn) {
 	global $Opt;
 
@@ -121,9 +126,9 @@ class Conference {
 	    if (!isset($this->settings[$x]))
 		$this->settings[$x] = 0;
 	if (!isset($this->settings["sub_blind"]))
-	    $this->settings["sub_blind"] = BLIND_ALWAYS;
+	    $this->settings["sub_blind"] = self::BLIND_ALWAYS;
 	if (!isset($this->settings["rev_blind"]))
-	    $this->settings["rev_blind"] = BLIND_ALWAYS;
+	    $this->settings["rev_blind"] = self::BLIND_ALWAYS;
 	if (!isset($this->settings["seedec"])) {
 	    if (@$this->settings["au_seedec"])
 		$this->settings["seedec"] = SEEDEC_ALL;
@@ -661,19 +666,27 @@ class Conference {
     }
 
     function subBlindAlways() {
-	return $this->settings["sub_blind"] == BLIND_ALWAYS;
+	return $this->settings["sub_blind"] == self::BLIND_ALWAYS;
     }
     function subBlindNever() {
-	return $this->settings["sub_blind"] == BLIND_NEVER;
+	return $this->settings["sub_blind"] == self::BLIND_NEVER;
     }
     function subBlindOptional() {
-	return $this->settings["sub_blind"] == BLIND_OPTIONAL;
+	return $this->settings["sub_blind"] == self::BLIND_OPTIONAL;
     }
     function subBlindUntilReview() {
-	return $this->settings["sub_blind"] == BLIND_UNTILREVIEW;
+	return $this->settings["sub_blind"] == self::BLIND_UNTILREVIEW;
     }
 
-    function blindReview() {
+    function is_review_blind($rrow) {
+        if (is_object($rrow))
+            $rrow = (bool) $rrow->reviewBlind;
+        $rb = $this->settings["rev_blind"];
+        return $rb == self::BLIND_ALWAYS
+            || ($rb == self::BLIND_OPTIONAL
+                && ($rrow === null || $rrow));
+    }
+    function review_blindness() {
         return $this->settings["rev_blind"];
     }
 

src/helpers.php
@@ -804,13 +804,6 @@ function tempdir($mode = 0700) {
 }
 
 
-function reviewBlind($rrow) {
-    global $Conf;
-    $br = $Conf->blindReview();
-    return $br == BLIND_ALWAYS
-	|| ($br == BLIND_OPTIONAL && (!$rrow || $rrow->reviewBlind));
-}
-
 function setCommentType($crow) {
     if ($crow && !isset($crow->commentType)) {
         if ($crow->forAuthors == 2)

src/mailer.php
@@ -348,7 +348,7 @@ class Mailer {
 	}
 
 	if ($what == "%REVIEWAUTHOR%" && $this->contacts[1]) {
-	    if (reviewBlind($this->rrow)
+	    if ($Conf->is_review_blind($this->rrow)
 		&& defval($this->permissionContact, "privChair") <= 0
 		&& (!isset($this->permissionContact->canViewReviewerIdentity)
 		    || !$this->permissionContact->canViewReviewerIdentity($this->row, $this->rrow, false))) {

commit 83d350b978b0da8a43d3cc77b7a7a9e03f0ef17a

Dear idiot moron, please get the direction of comment anonymity correct.
Broken in 80c36df2 (i.e. for the last 2 weeks authors could see the
identities of supposedly-blind comments).

src/contact.php
@@ -1649,7 +1649,7 @@ class Contact {
 	    || $rights->allow_pc
 	    || ($rights->allow_review
 		&& $Conf->settings["extrev_view"] >= 2)
-            || $Conf->is_review_blind(!$crow || ($crow->commentType & COMMENTTYPE_BLIND) != 0))
+            || !$Conf->is_review_blind(!$crow || ($crow->commentType & COMMENTTYPE_BLIND) != 0))
 	    return true;
 	return false;
     }