811235432093eb37ee346fdd722aafcbd831783a
Eddie Kohler <ekohler@gmail.com>
2014-02-11 19:59:54 -0500

    Add some comment visibility & identity visibility tests.

--- test/test01.php
+++ test/test01.php
@@ -132,6 +132,26 @@ $pl = new PaperList(new PaperSearch($user_mgbaker, "re:varghese"));
 $j = $pl->text_json("id");
 assert(join(";", array_keys($j)) == "1");
 
+// check comment identity
+$comment1 = CommentSave::save(array("text" => "test", "visibility" => "a", "blind" => false),
+                              $paper1, null, $user_mgbaker, false);
+assert($comment1);
+assert(!$user_van->canViewComment($paper1, $comment1, false));
+assert(!$user_van->canViewCommentIdentity($paper1, $comment1, false));
+$Conf->save_setting("au_seerev", AU_SEEREV_ALWAYS);
+assert($user_van->canViewComment($paper1, $comment1, false));
+assert(!$user_van->canViewCommentIdentity($paper1, $comment1, false));
+$Conf->save_setting("rev_blind", Conference::BLIND_OPTIONAL);
+assert($user_van->canViewComment($paper1, $comment1, false));
+assert(!$user_van->canViewCommentIdentity($paper1, $comment1, false));
+$comment1 = CommentSave::save(array("text" => "test", "visibility" => "a", "blind" => false),
+                              $paper1, null, $user_mgbaker, false);
+assert($comment1);
+assert($user_van->canViewCommentIdentity($paper1, $comment1, false));
+$Conf->save_setting("rev_blind", null);
+assert(!$user_van->canViewCommentIdentity($paper1, $comment1, false));
+$Conf->save_setting("au_seerev", AU_SEEREV_NO);
+
 // set up some tags and tracks
 $tagger = new Tagger($user_chair);
 $tagger->save(array(3, 9, 13, 17), "green", "a");
