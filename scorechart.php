31b01fca938aa5bf8a6b6ba8530c2c7924db9964
Eddie Kohler <ekohler@gmail.com>
2014-05-22 10:30:50 -0400

    Whitespace, simplify.

--- scorechart.php
+++ scorechart.php
@@ -21,10 +21,7 @@ if (!function_exists("imagecreate")) {
 
 // parse values
 $s = (isset($_REQUEST["s"]) ? $_REQUEST["s"] : 0);
-if (isset($_REQUEST["first"]) && is_numeric($_REQUEST["first"]))
-    $valMin = $valMax = intval($_REQUEST["first"]);
-else
-    $valMin = $valMax = 1;
+$valMax = 1;
 $values = array();
 $maxY = $sum = 0;
 foreach (explode(",", $_REQUEST["v"]) as $value) {
@@ -36,27 +33,27 @@ foreach (explode(",", $_REQUEST["v"]) as $value) {
 if (isset($_REQUEST["h"]) && is_numeric($_REQUEST["h"]))
     $valLight = intval($_REQUEST["h"]);
 else
-    $valLight = $valMin - 1;
-if ($valLight < $valMin || $valLight >= $valMax)
-    $valLight = $valMin - 1;
+    $valLight = 0;
+if ($valLight < 1 || $valLight >= $valMax)
+    $valLight = 0;
 $levelChar = (isset($_REQUEST["c"]) && ord($_REQUEST["c"]) >= 65
-	      ? ord($_REQUEST["c"]) : 0);
+              ? ord($_REQUEST["c"]) : 0);
 
 
 // set shape constants
 if ($s == 0) {
     list($blockHeight, $blockWidth, $blockSkip, $blockPad, $textWidth)
-	= array(8, 8, 2, 4, 12);
+        = array(8, 8, 2, 4, 12);
 } else if ($s == 1) {
     list($blockHeight, $blockWidth, $blockSkip, $blockPad, $textWidth)
-	= array(3, 3, 2, 2, 0);
+        = array(3, 3, 2, 2, 0);
 }
 
 if ($s == 0 || $s == 1) {
     $maxY = max($maxY, 3);
-    $picWidth = ($blockWidth + $blockPad) * ($valMax - $valMin)
-	+ $blockPad
-	+ 2 * $textWidth;
+    $picWidth = ($blockWidth + $blockPad) * ($valMax - 1)
+        + $blockPad
+        + 2 * $textWidth;
     $picHeight = $blockHeight * $maxY + $blockSkip * ($maxY + 1);
     $pic = @imagecreate($picWidth + 1, $picHeight + 1);
 } else if ($s == 2) {
@@ -84,39 +81,39 @@ $cv_bad = array(200, 128, 128);
 $cv_good = array(0, 232, 0);
 function quality_color($c1, $c2, $f) {
     return array($c2[0] * $f + $c1[0] * (1 - $f),
-		 $c2[1] * $f + $c1[1] * (1 - $f),
-		 $c2[2] * $f + $c1[2] * (1 - $f));
+                 $c2[1] * $f + $c1[1] * (1 - $f),
+                 $c2[2] * $f + $c1[2] * (1 - $f));
 }
 
 $pos = 0;
 
-for ($value = $valMin; $value < $valMax; $value++) {
-    $vpos = ($levelChar ? $valMax - ($value - $valMin + 1) : $value);
+for ($value = 1; $value < $valMax; $value++) {
+    $vpos = ($levelChar ? $valMax - $value : $value);
     $height = $values[$vpos];
-    $frac = ($vpos - $valMin) / ($valMax - $valMin);
+    $frac = ($vpos - 1) / ($valMax - 1);
     $cv_cur = quality_color($cv_bad, $cv_good, $frac);
     $cFill = imagecolorallocate($pic, $cv_cur[0], $cv_cur[1], $cv_cur[2]);
 
     if ($s == 0 || $s == 1) {
-	$curX = $blockWidth * ($value - $valMin)
-	    + $blockPad * ($value - $valMin + 1) + $textWidth;
-	$curY = $picHeight - ($blockHeight + $blockSkip) * $height + $blockHeight;
+        $curX = $blockWidth * ($value - 1)
+            + $blockPad * $value + $textWidth;
+        $curY = $picHeight - ($blockHeight + $blockSkip) * $height + $blockHeight;
 
-	for ($h = 1; $h <= $height; $h++) {
-	    if ($h == $height && $vpos == $valLight) {
-		$cv_cur = quality_color($cv_black, $cv_cur, 0.5);
-		$cFill = imagecolorallocate($pic, $cv_cur[0], $cv_cur[1], $cv_cur[2]);
-	    }
-	    imagefilledrectangle($pic, $curX, $curY - $blockHeight,
-				 $curX + $blockWidth, $curY, $cFill);
-	    $curY += ($blockHeight + $blockSkip);
-	}
+        for ($h = 1; $h <= $height; $h++) {
+            if ($h == $height && $vpos == $valLight) {
+                $cv_cur = quality_color($cv_black, $cv_cur, 0.5);
+                $cFill = imagecolorallocate($pic, $cv_cur[0], $cv_cur[1], $cv_cur[2]);
+            }
+            imagefilledrectangle($pic, $curX, $curY - $blockHeight,
+                                 $curX + $blockWidth, $curY, $cFill);
+            $curY += ($blockHeight + $blockSkip);
+        }
     } else {
-	if ($height > 0)
-	    imagefilledrectangle($pic, ($picWidth + 1) * $pos / $sum, 0,
-				 ($picWidth + 1) * ($pos + $height) / $sum - 2, $picHeight,
-				 $cFill);
-	$pos += $height;
+        if ($height > 0)
+            imagefilledrectangle($pic, ($picWidth + 1) * $pos / $sum, 0,
+                                 ($picWidth + 1) * ($pos + $height) / $sum - 2, $picHeight,
+                                 $cFill);
+        $pos += $height;
     }
 }
 
@@ -128,15 +125,15 @@ if ($s == 0) {
     $rx = $picWidth - $blockWidth - $textWidth - $blockPad;
     $y = $picHeight - $blockHeight - $blockSkip - 3;
     if (isset($_REQUEST["c"]) && ord($_REQUEST["c"]) >= 65) {
-	if ($values[$valMin] == 0)
-	    imagestring($pic, 1, $rx, $y, $_REQUEST["c"], $cgray);
-	if ($values[$valMax - 1] == 0)
-	    imagestring($pic, 1, $lx, $y, chr(ord($_REQUEST["c"]) - $valMax + 2), $cgray);
+        if ($values[1] == 0)
+            imagestring($pic, 1, $rx, $y, $_REQUEST["c"], $cgray);
+        if ($values[$valMax - 1] == 0)
+            imagestring($pic, 1, $lx, $y, chr(ord($_REQUEST["c"]) - $valMax + 2), $cgray);
     } else {
-	if ($values[$valMin] == 0)
-	    imagestring($pic, 1, $lx, $y, $valMin, $cgray);
-	if ($values[$valMax - 1] == 0)
-	    imagestring($pic, 1, $rx, $y, $valMax - 1, $cgray);
+        if ($values[1] == 0)
+            imagestring($pic, 1, $lx, $y, 1, $cgray);
+        if ($values[$valMax - 1] == 0)
+            imagestring($pic, 1, $rx, $y, $valMax - 1, $cgray);
     }
 }
 
