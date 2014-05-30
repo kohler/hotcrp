c74e5e32af9d3efcbfe1324e651c24fb268fe01a
Eddie Kohler <ekohler@gmail.com>
2014-05-29 22:53:42 -0400

    JSON upload script.

---
+++ batch/jsonupload.php
@@ -0,0 +1,36 @@
+<?php
+require_once("src/init.php");
+
+$arg = getopt_rest($argv, "hn:", array("help", "name:"));
+if (isset($arg["h"]) || isset($arg["help"])
+    || count($arg["_"]) > 1
+    || (count($arg["_"]) && $arg["_"][0] !== "-" && $arg["_"][0][0] === "-")) {
+    fwrite(STDOUT, "Usage: php batch/jsonupload.php FILE\n");
+    exit(0);
+}
+
+$file = count($arg["_"]) ? $arg["_"][0] : "-";
+if ($file === "-")
+    $content = stream_get_contents(STDIN);
+else
+    $content = file_get_contents($file);
+if ($content === false) {
+    fwrite(STDERR, "$file: Read error\n");
+    exit(1);
+}
+if (($jp = json_decode($content)) === false) {
+    fwrite(STDERR, "$file: bad JSON (" . json_last_error_msg() . ")\n");
+    exit(1);
+}
+
+if (is_object($jp))
+    $jp = array($jp);
+foreach ($jp as $j) {
+    $ps = new PaperStatus(array("no_email" => true,
+                                "allow_error" => array("topics", "options")));
+    $res = $ps->save($j);
+    foreach ($ps->error_messages() as $msg)
+        fwrite(STDERR, ($j->id ? "#$j->id: " : "paper: ") . $msg . "\n");
+    if (!$res)
+        exit(1);
+}
