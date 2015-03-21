c40cedfadcc2d2b1ccd55c6fb2f0c161f9099d73
Eddie Kohler <ekohler@gmail.com>
2015-03-20 21:50:05 -0400

    doc.php: Support more filename patterns.

--- doc.php
+++ doc.php
@@ -13,7 +13,7 @@ require_once("src/initweb.php");
 $documentType = HotCRPDocument::parse_dtype(@$_REQUEST["dt"]);
 if ($documentType === null)
     $documentType = @$_REQUEST["final"] ? DTYPE_FINAL : DTYPE_SUBMISSION;
-$need_docid = false;
+$attachment_filename = false;
 $docid = null;
 
 if (isset($_REQUEST["p"]))
@@ -22,36 +22,34 @@ else if (isset($_REQUEST["paperId"]))
     $paperId = cvtint(@$_REQUEST["paperId"]);
 else {
     $s = $orig_s = preg_replace(',\A/*,', "", Navigation::path());
+    $documentType = $dtname = null;
     if (str_starts_with($s, $Opt["downloadPrefix"]))
         $s = substr($s, strlen($Opt["downloadPrefix"]));
-    if (preg_match(',\Ap(?:aper)?([1-9]\d*)/+(.*)\z,', $s, $m)) {
-        $paperId = $m[1];
-        $s = $m[2];
-        if (preg_match(',\A([^/]*)\.[^/]+\z,', $s, $m))
-            $documentType = HotCRPDocument::parse_dtype($m[1]);
-        else if (preg_match(',\A([^/]+)/+(.*)\z,', $s, $m)) {
-            $documentType = HotCRPDocument::parse_dtype($m[1]);
-            if ($documentType
-                && ($o = PaperOption::find($documentType))
-                && $o->type == "attachments") {
-                $need_docid = true;
-                $result = $Conf->q("select o.value from PaperOption o join PaperStorage s on (s.paperStorageId=o.value) where o.paperId=$paperId and o.optionId=$documentType and s.filename='" . sqlq($m[2]) . "'", "while searching for attachment");
-                if (($row = edb_row($result)))
-                    $docid = $row[0];
-            }
-        }
-    } else if (preg_match(',\Apaper([1-9]\d*)-([^/]*)\.[^/]+\z,', $s, $m)) {
-        $paperId = $m[1];
-        $documentType = HotCRPDocument::parse_dtype($m[2]);
-    } else if (preg_match(',\A([-A-Za-z0-9_]*?)?-?([1-9]\d*)\.[^/]*\z,', $s, $m)) {
-        $paperId = $m[2];
-        $documentType = HotCRPDocument::parse_dtype($m[1]);
-    } else
-        $documentType = null;
-    if ($documentType === null)
-        $Error = "Unknown document “" . htmlspecialchars($orig_s) . "”.";
+    if (preg_match(',\Ap(?:aper)?(\d+)/+(.*)\z,', $s, $m)) {
+        $paperId = intval($m[1]);
+        if (preg_match(',\A([^/]+)\.[^/]+\z,', $m[2], $mm))
+            $dtname = $mm[1];
+        else if (preg_match(',\A([^/]+)/+(.*)\z,', $m[2], $mm))
+            list($dtype, $attachment_filename) = array($m[1], $m[2]);
+    } else if (preg_match(',\A(?:paper)?(\d+)-?([-A-Za-z0-9_]*)(?:\.[^/]+|/+(.*))\z,', $s, $m))
+        list($paperId, $dtname, $attachment_filename) = array(intval($m[1]), $m[2], $m[3]);
+    else if (preg_match(',\A([A-Za-z_][-A-Za-z0-9_]*?)?-?(\d+)(?:\.[^/]+|/+(.*))\z,', $s, $m))
+        list($paperId, $dtname, $attachment_filename) = array(intval($m[2]), $m[1], $m[3]);
+    if ($dtname !== null)
+        $documentType = HotCRPDocument::parse_dtype($dtname ? : "paper");
+    if ($documentType !== null && $attachment_filename) {
+        $o = PaperOption::find($documentType);
+        if ($o && $o->type == "attachments") {
+            $result = Dbl::q("select o.value from PaperOption o join PaperStorage s on (s.paperStorageId=o.value) where o.paperId=$paperId and o.optionId=$documentType and s.filename=?", $attachment_filename);
+            if ($result && ($row = $result->fetch_row()))
+                $docid = $row[0];
+        } else
+            $documentType = null;
+    }
 }
 
+if (!isset($Error) && $documentType === null)
+    $Error = "Unknown document “" . htmlspecialchars($orig_s) . "”.";
 if (!isset($Error)
     && !($prow = $Conf->paperRow($paperId, $Me, $whyNot)))
     $Error = whyNotText($whyNot, "view");
@@ -60,8 +58,8 @@ if (!isset($Error) && ($whyNot = $Me->perm_view_pdf($prow)))
 if (!isset($Error) && $documentType > 0
     && !$Me->can_view_paper_option($prow, $documentType, true))
     $Error = "You don’t have permission to view this document.";
-if (!isset($Error) && $need_docid && !$docid)
-    $Error = "No such attachment.";
+if (!isset($Error) && $attachment_filename && !$docid)
+    $Error = "No such attachment “" . htmlspecialchars($orig_s) . "”.";
 
 // Actually download paper.
 if (!isset($Error)) {
