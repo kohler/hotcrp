4099f30a9936fa6b1c404ba43f1e8068329387ed
Eddie Kohler <ekohler@gmail.com>
2015-04-01 00:52:35 -0400

    Magic of SVG background images for stripes.

--- test/stripe.cc
+++ test/stripe.cc
@@ -7,6 +7,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include <map>
 
 class Rgba {
@@ -31,12 +32,15 @@ class Stripe {
     void output_alpha(FILE* f);
     void output_png(FILE* f);
     void output_png(const char* fname);
+    std::string svg();
+    std::string svg_base64();
 
   private:
     double angle_;
     struct onestripe {
         double width;
         double color[4];
+        std::string fill() const;
     };
     std::vector<onestripe> stripes_;
 
@@ -50,6 +54,7 @@ class Stripe {
     Pixel** p_;
 
     void analyze();
+    bool rest_fully_opaque() const;
 };
 
 Stripe::Stripe(double angle)
@@ -71,6 +76,20 @@ void Stripe::add(uint8_t r, uint8_t g, uint8_t b, double a, int width) {
     stripes_.back().color[1] = g / 255.0;
     stripes_.back().color[2] = b / 255.0;
     stripes_.back().color[3] = a;
+}
+
+std::string Stripe::onestripe::fill() const {
+    char buf[64];
+    unsigned r = (unsigned) (color[0] * 255.0 + 0.5),
+        g = (unsigned) (color[1] * 255.0 + 0.5),
+        b = (unsigned) (color[2] * 255.0 + 0.5);
+    if (color[3] != 1)
+        sprintf(buf, "rgba(%u,%u,%u,%g)", r, g, b, color[3]);
+    else if (r / 16 == r % 16 && g / 16 == g % 16 && b / 16 == b % 16)
+        sprintf(buf, "#%x%x%x", r / 16, g / 16, b / 16);
+    else
+        sprintf(buf, "#%02x%02x%02x", r, g, b);
+    return buf;
 }
 
 void Stripe::analyze() {
@@ -242,6 +261,82 @@ void Stripe::output_png(const char* fname) {
     fclose(f);
 }
 
+bool Stripe::rest_fully_opaque() const {
+    for (unsigned i = 1; i < stripes_.size(); ++i)
+        if (stripes_[i].color[3] != 1)
+            return false;
+    return true;
+}
+
+double fix(double x) {
+    return round(x * 1024.) / 1024.;
+}
+
+std::string Stripe::svg() {
+    if (!p_)
+        analyze();
+    std::ostringstream buf;
+    buf << "<svg xmlns=\"http://www.w3.org/2000/svg\" "
+        << "width=\"" << (rot90_ ? height_ : width_) << "\" "
+        << "height=\"" << (rot90_ ? width_ : height_) << "\">";
+    double pos = -width_;
+    for (unsigned i = 0; i != stripes_.size(); ++i) {
+        auto& s = stripes_[i];
+        if (s.color[3] && i == 0 && rest_fully_opaque())
+            buf << "<rect x=\"0\" y=\"0\" width=\"100%\" height=\"100%\" fill=\"" << s.fill() << "\" />";
+        else if (s.color[3]) {
+            buf << "<polyline points=\""
+                << fix(pos) << ',' << height_ << ','
+                << fix(pos + height_ / tan(angle_)) << ",0,"
+                << fix(pos + s.width / sin(angle_) + height_ / tan(angle_)) << ",0,"
+                << fix(pos + s.width / sin(angle_)) << ',' << height_
+                << "\" fill=\"" << s.fill() << "\" />";
+            buf << "<polyline points=\""
+                << fix(pos + width_) << ',' << height_ << ','
+                << fix(pos + width_ + height_ / tan(angle_)) << ",0,"
+                << fix(pos + width_ + s.width / sin(angle_) + height_ / tan(angle_)) << ",0,"
+                << fix(pos + width_ + s.width / sin(angle_)) << ',' << height_
+                << "\" fill=\"" << s.fill() << "\" />";
+        }
+        pos += s.width / sin(angle_);
+    }
+    buf << "</svg>";
+    return buf.str();
+}
+
+static const char base64_encoding_table[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+std::string base64(std::string x, bool pad = true) {
+    std::ostringstream buf;
+    const unsigned char* s = reinterpret_cast<const unsigned char*>(x.data()),
+        *end = s + x.length();
+    for (; end - s >= 3; s += 3) {
+        unsigned x = (s[0] << 16) | (s[1] << 8) | s[2];
+        buf << base64_encoding_table[x >> 18]
+            << base64_encoding_table[(x >> 12) & 63]
+            << base64_encoding_table[(x >> 6) & 63]
+            << base64_encoding_table[x & 63];
+    }
+    if (end > s) {
+        unsigned x = s[0] << 16;
+        if (end > s + 1)
+            x |= s[1] << 8;
+        buf << base64_encoding_table[x >> 18]
+            << base64_encoding_table[(x >> 12) & 63];
+        if (end > s + 1)
+            buf << base64_encoding_table[(x >> 6) & 63];
+        else if (pad)
+            buf << '=';
+        if (pad)
+            buf << '=';
+    }
+    return buf.str();
+}
+
+std::string Stripe::svg_base64() {
+    return base64(svg());
+}
+
 std::map<std::string, Rgba> colormap;
 
 void makeit(const char* name1, const char* name2) {
@@ -249,7 +344,8 @@ void makeit(const char* name1, const char* name2) {
     Stripe s(M_PI / 4);
     s.add(colormap[name1], 12);
     s.add(colormap[name2], 12);
-    s.output_png(fn.c_str());
+//    s.output_png(fn.c_str());
+    fprintf(stdout, "%s\n", s.svg_base64().c_str());
 }
 
 int main() {
@@ -268,10 +364,12 @@ int main() {
     colormap["purple"] = Rgba{0xf2, 0xd8, 0xf8, 1};
     colormap["purpledark"] = Rgba{0xea, 0xc7, 0xf2, 1};
     colormap["gray"] = Rgba{0xe2, 0xe2, 0xe2, 1};
-    colormap["graydark"] = Rgba{0d9, 0xd9, 0xd9, 1};
+    colormap["graydark"] = Rgba{0xd9, 0xd9, 0xd9, 1};
     colormap["white"] = Rgba{0xff, 0xff, 0xff, 1};
     colormap["whitedark"] = Rgba{0xf8, 0xf8, 0xf8, 1};
+    colormap["transparent"] = Rgba{0, 0, 0, 0};
 
+    makeit("gray", "transparent");
     for (const char** name1 = names; *name1; ++name1)
         for (const char** name2 = name1 + 1; *name2; ++name2)
             makeit(*name1, *name2);
