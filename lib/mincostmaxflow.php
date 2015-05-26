25ae3def558c75c59915c874694936c5cbd3df25
Eddie Kohler <ekohler@gmail.com>
2015-05-26 18:49:34 -0400

    Add (not-yet-working) cost-scaling push-relabel MCMF.

--- lib/mincostmaxflow.php
+++ lib/mincostmaxflow.php
@@ -13,11 +13,22 @@ class MinCostMaxFlow_Node {
     public $cycle = null;
     public $height = 0;
     public $excess = 0;
+    public $price = 0;
     public $ein = array();
     public $eout = array();
     public function __construct($name, $klass) {
         $this->name = $name;
         $this->klass = $klass;
+    }
+    public function check_excess() {
+        $x = 0;
+        foreach ($this->ein as $e)
+            $x += $e->flow;
+        foreach ($this->eout as $e)
+            $x -= $e->flow;
+        if ($x != $this->excess)
+            fwrite(STDERR, "{$this->name}: bad excess e{$this->excess}, have $x\n");
+        assert($x == $this->excess);
     }
 };
 
@@ -36,6 +47,12 @@ class MinCostMaxFlow_Edge {
     public function residual_cap($isrev) {
         return $isrev ? $this->flow : $this->cap - $this->flow;
     }
+    public function reduced_cost($isrev) {
+        if ($isrev)
+            return -$this->cost - $this->src->price + $this->dst->price;
+        else
+            return $this->cost + $this->src->price - $this->dst->price;
+    }
 };
 
 class MinCostMaxFlow {
@@ -49,6 +66,9 @@ class MinCostMaxFlow {
     private $maxcost = 0;
     private $hasrun = false;
     private $hascirculation = false;
+    // cspushrelabel state
+    private $epsilon;
+    private $ltail;
 
     public function __construct() {
         $this->source = $this->add_node(".source", ".internal");
@@ -191,7 +211,6 @@ class MinCostMaxFlow {
         $src = $erev ? $e->dst : $e->src;
         $dst = $erev ? $e->src : $e->dst;
         $amt = min($src->excess, $e->residual_cap($erev));
-        assert($src->height == $dst->height + 1 && $amt > 0);
         $e->flow += $erev ? -$amt : $amt;
         $src->excess -= $amt;
         $dst->excess += $amt;
@@ -210,14 +229,13 @@ class MinCostMaxFlow {
     }
 
     private function pushrelabel_discharge($v) {
-        if ($v->excess == 0)
-            return;
         $nout = count($v->eout);
         $nin = count($v->ein);
+        $notrelabeled = 1;
         while ($v->excess > 0) {
             if ($v->npos == $nout + $nin) {
                 $this->pushrelabel_relabel($v);
-                $v->npos = 0;
+                $v->npos = $notrelabeled = 0;
             } else {
                 $erev = $v->npos >= $nout;
                 $e = $erev ? $v->ein[$v->npos - $nout] : $v->eout[$v->npos];
@@ -229,6 +247,7 @@ class MinCostMaxFlow {
                     ++$v->npos;
             }
         }
+        return !$notrelabeled;
     }
 
     private function pushrelabel_run() {
@@ -255,9 +274,7 @@ class MinCostMaxFlow {
         $l = $lhead;
         $lprev = null;
         while ($l) {
-            $old_height = $l->height;
-            $this->pushrelabel_discharge($l);
-            if ($l->height > $old_height && $l !== $lhead) {
+            if ($this->pushrelabel_discharge($l) && $l !== $lhead) {
                 $lprev->link = $l->link;
                 $l->link = $lhead;
                 $lhead = $l;
@@ -268,14 +285,121 @@ class MinCostMaxFlow {
     }
 
 
+    // cost-scaling push-relabel
+
+    private function cspushrelabel_relabel($v) {
+        $p = -INF;
+        foreach ($v->eout as $e)
+            if ($e->flow < $e->cap) {
+                assert($e->reduced_cost(false) >= 0);
+                $p = max($p, $e->dst->price - $e->cost - $this->epsilon);
+            }
+        foreach ($v->ein as $e)
+            if ($e->flow > 0) {
+                assert($e->reduced_cost(true) >= 0);
+                $p = max($p, $e->src->price + $e->cost - $this->epsilon);
+            }
+        $v->price = $p;
+        fwrite(STDERR, "{$v->name} relabel P{$v->price} e{$v->excess}\n");
+    }
+
+    private function cspushrelabel_discharge($v) {
+        $nout = count($v->eout);
+        $nin = count($v->ein);
+        $notrelabeled = 1;
+        while ($v->excess > 0) {
+            if ($v->npos == $nout + $nin) {
+                $this->cspushrelabel_relabel($v);
+                $v->check_excess();
+                $v->npos = $notrelabeled = 0;
+            } else {
+                $erev = $v->npos >= $nout;
+                $e = $erev ? $v->ein[$v->npos - $nout] : $v->eout[$v->npos];
+                $other = $erev ? $e->src : $e->dst;
+                fwrite(STDERR, " ... {$e->src->name}P{$e->src->price} {$e->dst->name}P{$e->dst->price} {$e->cap} {$e->cost} {$e->flow} / " . $e->residual_cap($erev) . " " . $e->reduced_cost($erev) . "\n");
+                if ($e->residual_cap($erev) > 0
+                    && $e->reduced_cost($erev) < 0) {
+                    $this->pushrelabel_push($e, $erev);
+                    if ($other->excess > 0 && $other->link === false) {
+                        $this->ltail = $this->ltail->link = $other;
+                        $other->link = null;
+                    }
+                } else
+                    ++$v->npos;
+            }
+        }
+        return !$notrelabeled;
+    }
+
+    private function cspushrelabel_refine() {
+        $this->epsilon = $this->epsilon / 4; /* Goldberg has 12 */
+        foreach ($this->e as $e) {
+            if ($e->reduced_cost(false) < 0 && $e->flow < $e->cap) {
+                assert($e->reduced_cost(true) >= 0);
+                $delta = $e->cap - $e->flow;
+                $e->flow = $e->cap;
+                $e->dst->excess += $delta;
+                $e->src->excess -= $delta;
+                fwrite(STDERR, "moving $delta {$e->src->name} > {$e->dst->name}\n");
+            } else if ($e->reduced_cost(true) < 0 && $e->flow) {
+                $delta = $e->flow;
+                $e->flow = 0;
+                $e->src->excess += $delta;
+                $e->dst->excess -= $delta;
+                fwrite(STDERR, "moving $delta {$e->dst->name}.e{$e->dst->excess} > {$e->src->name}.e{$e->src->excess}\n");
+            }
+        }
+        foreach ($this->v as $v)
+            $v->check_excess();
+
+        // initialize lists and neighbor position
+        $lhead = $this->ltail = null;
+        foreach ($this->v as $v) {
+            $v->npos = 0;
+            if ($v === $this->source || $v === $this->sink)
+                $v->link = null;
+            else if ($v->excess > 0) {
+                $this->ltail ? ($this->ltail->link = $v) : ($lhead = $v);
+                $v->link = null;
+                $this->ltail = $v;
+            } else
+                $v->link = false;
+        }
+
+        // relabel-to-front
+        while ($lhead) {
+            fwrite(STDERR, ".");
+            $this->cspushrelabel_discharge($lhead);
+            $l = $lhead->link;
+            $lhead->link = false;
+            $lhead = $l;
+        }
+
+        fwrite(STDERR, "!\n" . $this->debug_info() . "\n");
+        foreach ($this->v as $v) {
+            fwrite(STDERR, "{$v->name}.e{$v->excess} ");
+            $v->check_excess();
+        }
+        fwrite(STDERR, "\n");
+    }
+
+    private function cspushrelabel_run() {
+        $this->epsilon = $this->maxcost;
+        $this->pushrelabel_run();
+        fwrite(STDERR, "====\n" . $this->debug_info() . "\n");
+        while ($this->epsilon >= 1 / count($this->v))
+            $this->cspushrelabel_refine();
+    }
+
+
     public function run() {
         assert(!$this->hasrun);
         $this->hasrun = true;
         $this->shuffle();
-        if ($this->mincost != 0 || $this->maxcost != 0)
-            return $this->cyclecancel_run();
-        else
+        if ($this->mincost == 0 && $this->maxcost == 0)
             return $this->pushrelabel_run();
+        else
+            return $this->cyclecancel_run();
     }
 
 
@@ -302,7 +426,7 @@ class MinCostMaxFlow {
             if ($this->hascirculation)
                 array_pop($this->e);
             foreach ($this->v as $v) {
-                $v->flow = $v->height = $v->excess = 0;
+                $v->flow = $v->height = $v->excess = $v->price = 0;
                 $v->ein = array();
                 $v->eout = array();
             }
