f3704ae1f70c01dca671e4ee7ec45e15bd57e5c3
Eddie Kohler <ekohler@gmail.com>
2015-11-22 18:00:41 +0000

    Don't use Content-Style-Type.

--- src/conference.php
+++ src/conference.php
@@ -2232,8 +2232,6 @@ class Conf {
 <html>
 <head>
 <meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />
-<meta http-equiv=\"Content-Style-Type\" content=\"text/css\" />
-<meta http-equiv=\"Content-Script-Type\" content=\"text/javascript\" />
 <meta http-equiv=\"Content-Language\" content=\"en\" />
 <meta name=\"google\" content=\"notranslate\" />\n";
         if (strstr($title, "<") !== false)
