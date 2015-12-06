9e97ddaacf94dce9f3fc604d0d0c5031ce849d23
Eddie Kohler <ekohler@gmail.com>
2015-12-06 15:37:52 +0000

    Include abstract, authors, topics, etc. in authors' paper list.

--- src/paperlist.php
+++ src/paperlist.php
@@ -558,7 +558,7 @@ class PaperList {
     private function _list_columns($listname) {
         switch ($listname) {
         case "a":
-            return "id title statusfull revstat scores";
+            return "id title statusfull revstat authors collab abstract topics reviewers shepherd scores";
         case "authorHome":
             return "id title statusfull";
         case "s":
