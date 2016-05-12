1ac0d9487144e61def75152331edfd760cf2a9dd
Eddie Kohler <ekohler@gmail.com>
2016-05-12 09:44:04 -0400

    Fix errf usage.

--- settings.php
+++ settings.php
@@ -259,8 +259,8 @@ class SettingValues {
     }
     public function set_warning($field, $html = false) {
         $fname = self::check_error_field($field, $html);
-        if ($field && !isset($this->errf[$field]))
-            $this->errf[$field] = 1;
+        if ($fname && !isset($this->errf[$fname]))
+            $this->errf[$fname] = 1;
         if ($html !== false)
             $this->warnmsg[] = $html;
         return false;
