ea0b3910601b683cd99185db5f7086ab63e62c12
Eddie Kohler <ekohler@gmail.com>
2016-06-05 21:08:53 +0300

    Button.

--- src/settings/s_users.php
+++ src/settings/s_users.php
@@ -9,7 +9,7 @@ class SettingRenderer_Users extends SettingRenderer {
         if ($sv->curv("acct_addr"))
             $sv->echo_checkbox("acct_addr", "Collect users’ addresses and phone numbers");
         echo "<h3 class=\"settings g\">Program committee &amp; system administrators</h3>";
-        echo "<p><a href='", hoturl("profile", "u=new&amp;role=pc"), "' class='button'>Create PC account</a> &nbsp;|&nbsp; ",
+        echo "<p><a href='", hoturl("profile", "u=new&amp;role=pc"), "' class='btn'>Create PC account</a> <span class='barsep'>·</span> ",
             "Select a user’s name to edit a profile.</p>\n";
         $pl = new ContactList($Me, false);
         echo $pl->table_html("pcadminx", hoturl("users", "t=pcadmin"));
