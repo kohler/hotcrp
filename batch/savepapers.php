df477a9a85649c573476e5c058f0637418cdea98
Eddie Kohler <ekohler@gmail.com>
2016-09-14 09:07:01 -0400

    Report more errors about invalid reviews.

--- batch/savepapers.php
+++ batch/savepapers.php
@@ -58,23 +58,32 @@ foreach ($jp as $j) {
             $j->pid = (int) $pids[0];
     }
     if (isset($j->pid) && is_int($j->pid) && $j->pid > 0)
-        $prefix = "#$j->pid: ";
+        $pidtext = "#$j->pid";
     else if (!isset($j->pid) && isset($j->id) && is_int($j->id) && $j->id > 0)
-        $prefix = "#$j->id: ";
+        $pidtext = "#$j->id";
     else if (!isset($j->pid) && !isset($j->id))
-        $prefix = "new paper #$index: ";
+        $pidtext = "new paper @$index";
     else {
-        fwrite(STDERR, "paper #$index: bad pid\n");
+        fwrite(STDERR, "paper @$index: bad pid\n");
         exit(1);
     }
-    if (!$quiet)
-        fwrite(STDERR, $prefix);
+    if (!$quiet) {
+        if (isset($j->title) && is_string($j->title))
+            fwrite(STDERR, $pidtext . " (" . UnicodeHelper::utf8_abbreviate($j->title, 40) . "): ");
+        else
+            fwrite(STDERR, $pidtext . ": ");
+    }
     $ps = new PaperStatus($Conf, null, ["no_email" => true,
                                         "disable_users" => $disable_users,
                                         "allow_error" => ["topics", "options"]]);
     $pid = $ps->save_paper_json($j);
+    if ($pid && str_starts_with($pidtext, "new")) {
+        fwrite(STDERR, "-> #" . $pid . ": ");
+        $pidtext = "#$pid";
+    }
     if (!$quiet)
         fwrite(STDERR, $pid ? "saved\n" : "failed\n");
+    $prefix = $pidtext . ": ";
     foreach ($ps->messages() as $msg)
         fwrite(STDERR, $prefix . htmlspecialchars_decode($msg) . "\n");
     if (!$pid)
@@ -83,7 +92,7 @@ foreach ($jp as $j) {
     if (isset($j->reviews) && is_array($j->reviews) && $reviews) {
         $rform = $Conf->review_form();
         $tf = $rform->blank_text_form();
-        foreach ($j->reviews as $reviewj)
+        foreach ($j->reviews as $reviewindex => $reviewj)
             if (($rreq = $rform->parse_json($reviewj))
                 && isset($rreq["reviewerEmail"])
                 && validate_email($rreq["reviewerEmail"])) {
@@ -91,7 +100,8 @@ foreach ($jp as $j) {
                 $user_req = Text::analyze_name(["name" => get($rreq, "reviewerName"), "email" => $rreq["reviewerEmail"], "affiliation" => get($rreq, "reviewerAffiliation")]);
                 $user = Contact::create($Conf, $user_req);
                 $rform->check_save_review($site_contact, $rreq, $tf, $user);
-            }
+            } else
+                $tf["err"][] = "invalid review @$reviewindex";
         foreach ($tf["err"] as $te)
             fwrite(STDERR, $prefix . htmlspecialchars_decode($te) . "\n");
     }
