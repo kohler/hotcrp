f0479cf0c356476b0d7ae9b28b33aba1863f7e92
Eddie Kohler <ekohler@gmail.com>
2016-11-12 20:35:40 -0500

    Put column lists into a consistent order.

--- src/paperlist.php
+++ src/paperlist.php
@@ -537,15 +537,15 @@ class PaperList {
     private function _list_columns($listname) {
         switch ($listname) {
         case "a":
-            return "id title statusfull revstat authors collab abstract topics reviewers shepherd scores formulas";
+            return "id title revstat statusfull authors collab abstract topics reviewers shepherd scores formulas";
         case "authorHome":
             return "id title statusfull";
         case "s":
         case "acc":
-            return "sel id title revtype revstat status authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "sel id title revtype revstat status authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "all":
         case "act":
-            return "sel id title statusfull revtype authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "sel id title revtype statusfull authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "reviewerHome":
             $this->_default_linkto("finishreview");
             return "id title revtype status";
@@ -554,22 +554,22 @@ class PaperList {
         case "manager":
             if ($listname == "r")
                 $this->_default_linkto("finishreview");
-            return "sel id title revtype revstat status authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "sel id title revtype revstat status authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "rout":
             $this->_default_linkto("review");
-            return "sel id title revtype revstat status authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "sel id title revtype revstat status authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "req":
             $this->_default_linkto("review");
-            return "sel id title revtype revstat status authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "sel id title revtype revstat status authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "reqrevs":
             $this->_default_linkto("review");
-            return "id title revdelegation revsubmitted revstat status authors collab abstract tags tagreports topics reviewers allpref pcconf lead shepherd scores formulas";
+            return "id title revdelegation revsubmitted revstat status authors collab abstract topics tags tagreports reviewers allpref pcconf lead shepherd scores formulas";
         case "reviewAssignment":
             $this->_default_linkto("assign");
-            return "id title revpref topicscore desirability assrev authors tags topics reviewers allrevtopicpref authorsmatch collabmatch scores formulas";
+            return "id title revpref topicscore desirability assrev authors topics tags reviewers allrevtopicpref authorsmatch collabmatch scores formulas";
         case "conflict":
             $this->_default_linkto("assign");
-            return "selconf id title authors abstract tags authorsmatch collabmatch foldall";
+            return "selconf id title authors abstract authorsmatch collabmatch tags foldall";
         case "editpref":
             $this->_default_linkto("paper");
             return "sel id title topicscore revtype editpref authors abstract topics";
