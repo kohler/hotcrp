b079baded7032e9dfa2d292c21e5ab96ec57ba50
Eddie Kohler <ekohler@gmail.com>
2016-11-28 21:34:00 -0500

    Add help for track chairs.

--- help.php
+++ help.php
@@ -680,16 +680,14 @@ function tracks() {
     global $Me;
 
     _subhead("", "
-<p>Tracks control which PC members can view and review
-specific papers. Tracks are managed through the <a href=\"" . hoturl("help", "t=tags") . "\">tags system</a>.
-Without tracks, all PC members are treated equally.
-With tracks, PC members with different tags can have different rights to
-see and review papers, depending on the papers’ tags.</p>
+<p>Tracks give you fine-grained control over PC member rights. With tracks, PC
+members can have different rights to see and review papers, depending on the
+papers’ " . Ht::link("tags", hoturl("help", "t=tags")) . ".</p>
 
 <p>Set up tracks on the <a href=\"" . hoturl("settings", "group=tracks") . "\">Settings &gt;
 Tracks</a> page.</p>");
 
-    _subhead("Examples", "
+    _subhead("Example: External review committee", "
 <p>An <em>external review committee</em> is a subset of the PC that may bid on
 papers to review, and may be assigned reviews (using, for example, the
 <a href=\"" . hoturl("autoassign") . "\">autoassignment tool</a>), but may not
@@ -701,8 +699,9 @@ reviewed. To set this up:</p>
 <li>On Settings &gt; Tracks, “For papers not on other
 tracks,” select “Who can see reviews? &gt; PC members without tag: erc”
 and “Who can self-assign a review? &gt; PC members without tag: erc”.</li>
-</ul>
+</ul>");
 
+    _subhead("Example: PC-paper review committee", "
 <p>A <em>PC-paper review committee</em> is a subset of the PC that reviews papers
 with PC coauthors. PC-paper review committees are kept separate from the main
 PC; they only bid on and review PC papers, while the main PC handles all other
@@ -716,6 +715,23 @@ papers. To set this up:</p>
 <li>For papers not on other tracks, for all permissions, select
   “PC members without tag: pcrc”.</li>
 </ul>");
+
+    _subhead("Example: Track chair", "
+<p>A <em>track chair</em> is a PC member with full administrative
+rights over a subset of papers. To set this up for, say, an “industrial”
+track:</p>
+
+<ul>
+<li>Give the industrial track chair(s) the “industrial-chair” tag.</li>
+<li>Give industrial-track papers the “industrial” tag.</li>
+<li>On Settings &gt; Tracks, add a track for tag “industrial”,
+  and select “Who can administer these papers? &gt; PC members with tag:
+  industrial-chair”.</li>
+</ul>
+
+<p>A track chair can run the autoassigner, make assignments, edit papers, and
+generally administer all papers on their tracks. Track chairs cannot modify
+site settings or change track tags, however.</p>");
 
     _subhead("Understanding permissions", "
 <p>Tracks only restrict permissions.
