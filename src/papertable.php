aba8789e67dee7f5c2f6fe6d2c7abf08900a81dd
Eddie Kohler <ekohler@gmail.com>
2017-01-19 20:27:37 -0500

    Remove unused.

--- src/papertable.php
+++ src/papertable.php
@@ -270,7 +270,6 @@ class PaperTable {
     }
 
     private function papt($what, $name, $extra = array()) {
-        $type = defval($extra, "type", "pav");
         $fold = defval($extra, "fold", false);
         $editfolder = defval($extra, "editfolder", false);
         if ($fold || $editfolder) {
