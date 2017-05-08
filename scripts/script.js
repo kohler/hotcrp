b1c2fb96dcef6552140e4f00fa6a7f38dda6d151
Eddie Kohler <ekohler@gmail.com>
2017-05-07 21:35:03 -0400

    Fix rendering of stripy background images on Chrome.

--- scripts/script.js
+++ scripts/script.js
@@ -5403,7 +5403,7 @@ return function (classes, class_prefix) {
         style || (style = $("<style></style>").appendTo("head")[0].sheet);
         t = '<svg xmlns="http://www.w3.org/2000/svg" width="' + size +
             '" height="' + size + '">' + t + '</svg>';
-        t = 'background: url(data:image/svg+xml;base64,' + btoa(t) + ') fixed;'
+        t = 'background-image: url(data:image/svg+xml;base64,' + btoa(t) + ');'
         x = "." + tags.join(".") + (class_prefix ? $.trim("." + class_prefix) : "");
         style.insertRule(x + " { " + t + " }", 0);
         style.insertRule(x + ".psc { " + t + " }", 0);
