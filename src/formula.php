kohler/hotcrp

commit ecb3f828ad2b739098e6f64cb9f01faf54871a94

Formulas understand min(x,y) and max(x,y) as synonyms for least/greatest.

src/formula.php
@@ -41,6 +41,10 @@ class Fexpr {
         $this->op = $op;
         if ($this->op === "trunc")
             $this->op = "floor";
+        else if ($this->op === "min")
+            $this->op = "least";
+        else if ($this->op === "max")
+            $this->op = "greatest";
         $args = func_get_args();
         if (count($args) == 2 && is_array($args[1]))
             $this->args = $args[1];
@@ -326,6 +330,9 @@ class InFexpr extends Fexpr {
 class AggregateFexpr extends Fexpr {
     static function make($ff, $args) {
         $op = $ff->name;
+        if (($op === "min" || $op === "max")
+            && count($args) > 1)
+            return Fexpr::make($ff, $args);
         if ($op === "average" || $op === "mean"
             || ($op === "wavg" && count($args) == 1))
             $op = "avg";

commit 7f61c50dee96820cbd83f6cb78920cc3a2aee89d

Fix min/max formulas broken in ecb3f828ad2b739098e6f64cb9f01faf54871a94.

src/formula.php
@@ -41,10 +41,6 @@ class Fexpr {
         $this->op = $op;
         if ($this->op === "trunc")
             $this->op = "floor";
-        else if ($this->op === "min")
-            $this->op = "least";
-        else if ($this->op === "max")
-            $this->op = "greatest";
         $args = func_get_args();
         if (count($args) == 2 && is_array($args[1]))
             $this->args = $args[1];
@@ -328,11 +324,14 @@ class InFexpr extends Fexpr {
 }
 
 class AggregateFexpr extends Fexpr {
+    function __construct($fn, array $values) {
+        parent::__construct($fn, $values);
+    }
     static function make($ff, $args) {
         $op = $ff->name;
         if (($op === "min" || $op === "max")
             && count($args) > 1)
-            return Fexpr::make($ff, $args);
+            return new Fexpr($op === "min" ? "least" : "greatest", $args);
         if ($op === "average" || $op === "mean"
             || ($op === "wavg" && count($args) == 1))
             $op = "avg";