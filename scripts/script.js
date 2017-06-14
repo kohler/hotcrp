329dbacac4c2f70f750be877a104cda1b5308da4
Eddie Kohler <ekohler@gmail.com>
2017-06-14 09:52:00 -0400

    Whoopsy

--- scripts/script.js
+++ scripts/script.js
@@ -2216,7 +2216,7 @@ return {
         if (elt) {
             $("#pcs" + which).val(elt.value);
             $$("ass" + which).className = "pctbname pctbname" + elt.value;
-            folder.firstChild.className = "rt rt" + elt.value;
+            folder.firstChild.className = "rto rt" + elt.value;
             folder.firstChild.innerHTML = '<span class="rti">' +
                 (["&minus;", "A", "C", "", "E", "P", "2", "1"])[+elt.value + 3] + "</span>";
             hiliter(folder.firstChild);
@@ -2684,7 +2684,7 @@ function add_review(rrow) {
     // author info
     var revinfo = [], rtype_text = "";
     if (rrow.rtype) {
-        rtype_text = ' &nbsp;<span class="rt rt' + rrow.rtype + (rrow.submitted ? "" : "n") +
+        rtype_text = ' &nbsp;<span class="rto rt' + rrow.rtype + (rrow.submitted ? "" : "n") +
             '" title="' + rtype_info[rrow.rtype][1] + '"><span class="rti">' +
             rtype_info[rrow.rtype][0] + '</span></span>';
         if (rrow.round)
