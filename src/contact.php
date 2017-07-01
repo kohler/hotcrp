e87d34fa1b707def7fcf5054fed743b62c862d9f
Eddie Kohler <ekohler@gmail.com>
2017-07-01 16:23:07 -0400

    More error information about contactTags missing.

--- src/contact.php
+++ src/contact.php
@@ -654,7 +654,7 @@ class Contact {
         if ($this->contactTags)
             return stripos($this->contactTags, " $t#") !== false;
         if ($this->contactTags === false) {
-            trigger_error(caller_landmark(1, "/^Conf::/") . ": Contact $this->email contactTags missing");
+            trigger_error(caller_landmark(1, "/^Conf::/") . ": Contact $this->email contactTags missing " . json_encode(debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)));
             $this->contactTags = null;
         }
         return false;
