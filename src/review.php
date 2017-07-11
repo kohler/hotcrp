kohler/hotcrp

commit a35525c89fa406617d02594f4123161a5f315670

Review HTML form: Prefer radios to select.

src/review.php
@@ -549,19 +549,22 @@ class ReviewForm {
 
             echo '<div class="revev">';
             if ($f->has_options) {
-                echo '<select name="', $field, '" onchange="hiliter(this)">';
-                $noentry = $f->allow_empty ? "No entry" : "(Your choice here)";
+                echo "<table><tbody>\n";
                 if (!$f->parse_value($fval, true))
-                    echo '<option value="0" selected="selected">', $noentry, '</option>';
-                else if ($f->allow_empty)
-                    echo '<option value="0">', $noentry, '</option>';
+                    $fval = 0;
+                if ($f->allow_empty)
+                    echo '<tr><td>',
+                        Ht::radio_h($field, 0, $fval == 0, ["id" => $field . "_0"]),
+                        '&nbsp;</td>',
+                        '<td colspan="2">', Ht::label("No entry"), "</td></tr>\n";
                 foreach ($f->options as $num => $what) {
-                    echo '<option value="', $num, '"';
-                    if ($num == $fval)
-                        echo ' selected="selected"';
-                    echo ">$num. ", htmlspecialchars($what), "</option>";
+                    echo '<tr><td>',
+                        Ht::radio_h($field, $num, $fval == $num, ["id" => $field . "_" . $num]),
+                        '&nbsp;</td>',
+                        '<td>', Ht::label($f->unparse_value($num, ReviewField::VALUE_REV_NUM) . '&nbsp;'), '</td>',
+                        '<td>', Ht::label(htmlspecialchars($what)), "</td></tr>\n";
                 }
-                echo "</select>";
+                echo "</tbody></table>";
             } else {
                 if ($format_description)
                     echo $format_description;

commit 2c7f6107efb6231afcc6112a9457ba19e495eeab

Fix display of initial selection of lettered review field.
It should show as no selection.
Broken in a35525c89fa406617d02594f4123161a5f315670.

src/review.php
@@ -334,13 +334,13 @@ class ReviewField implements Abbreviatable, JsonSerializable {
             && preg_match('/\A\s*([0-9]+|[A-Z])(\W|\z)/', $text, $m))
             $text = $m[1];
         if (!$strict && ctype_digit($text))
-            $text = (int) $text;
+            $text = intval($text);
         if (!$text || !$this->has_options || !isset($this->options[$text]))
             return null;
         else if ($this->option_letter)
             return $this->option_letter - ord($text);
         else
-            return $text;
+            return (int) $text;
     }
 }
 
@@ -542,7 +542,7 @@ class ReviewForm {
                         '<td colspan="2">', Ht::label("No entry"), "</td></tr>\n";
                 foreach ($f->options as $num => $what) {
                     echo '<tr><td class="nw">',
-                        Ht::radio($field, $num, $fval == $num, ["id" => $field . "_" . $num]),
+                        Ht::radio($field, $num, $fval === $num, ["id" => $field . "_" . $num]),
                         '&nbsp;</td>',
                         '<td class="nw">', Ht::label($f->unparse_value($num, ReviewField::VALUE_REV_NUM) . '&nbsp;'), '</td>',
                         '<td>', Ht::label(htmlspecialchars($what)), "</td></tr>\n";