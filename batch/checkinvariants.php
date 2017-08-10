bed1a6cf8d499b08681c71b9b169900a14ce881f
Eddie Kohler <ekohler@gmail.com>
2017-08-10 12:05:34 -0400

    checkinvariants: Add --fix-json-reviews.

--- batch/checkinvariants.php
+++ batch/checkinvariants.php
@@ -3,7 +3,7 @@ $ConfSitePATH = preg_replace(',/batch/[^/]+,', '', __FILE__);
 require_once("$ConfSitePATH/src/init.php");
 require_once("$ConfSitePATH/lib/getopt.php");
 
-$arg = getopt_rest($argv, "hn:q", array("help", "name:", "json-reviews"));
+$arg = getopt_rest($argv, "hn:q", array("help", "name:", "json-reviews", "fix-json-reviews"));
 if (isset($arg["h"]) || isset($arg["help"])
     || count($arg["_"]) > 0) {
     fwrite(STDOUT, "Usage: php batch/checkinvariants.php [-n CONFID]\n");
@@ -12,20 +12,40 @@ if (isset($arg["h"]) || isset($arg["help"])
 
 $Conf->check_invariants();
 
-if ($Conf->sversion == 174 && isset($arg["json-reviews"])) {
+if ($Conf->sversion == 174 && (isset($arg["json-reviews"]) || isset($arg["fix-json-reviews"]))) {
     $result = $Conf->qe("select * from PaperReview");
+    $q = $qv = [];
     while (($rrow = ReviewInfo::fetch($result, $Conf))) {
         $tfields = $rrow->tfields ? json_decode($rrow->tfields) : null;
+        $need_fix = $unfixable = false;
         foreach (ReviewInfo::$text_field_map as $kin => $kout) {
             $oldv = (string) $rrow->$kin;
             $newv = $tfields ? get($tfields, $kout, "") : "";
-            if ($oldv !== $newv)
+            if ($oldv !== $newv) {
                 error_log("{$Conf->dbname}: #{$rrow->paperId}/{$rrow->reviewId}: $kin ["
-                    . UnicodeHelper::utf8_abbreviate($oldv === "" ? "EMPTY" : $oldv, 10)
+                    . simplify_whitespace(UnicodeHelper::utf8_abbreviate($oldv === "" ? "EMPTY" : $oldv, 20))
                     . "] differs from tf/$kout ["
-                    . UnicodeHelper::utf8_abbreviate($newv === "" ? "EMPTY" : $newv, 10)
+                    . simplify_whitespace(UnicodeHelper::utf8_abbreviate($newv === "" ? "EMPTY" : $newv, 20))
                     . "]");
+                $need_fix = true;
+                if ($newv === "") {
+                    $tfields = $tfields ? : (object) [];
+                    $tfields->$kout = $oldv;
+                } else
+                    $unfixable = true;
+            }
+        }
+        if ($need_fix && isset($arg["fix-json-reviews"])) {
+            if (!$unfixable) {
+                $q[] = "update PaperReview set tfields=? where paperId=? and reviewId=? and tfields?e";
+                array_push($qv, json_encode_db($tfields), $rrow->paperId, $rrow->reviewId, $rrow->tfields);
+            } else
+                error_log("{$Conf->dbname}: #{$rrow->paperId}/{$rrow->reviewId}: differences unfixable");
         }
     }
     Dbl::free($result);
+    if ($q) {
+        $mresult = Dbl::multi_ql_apply($Conf->dblink, join("; ", $q), $qv);
+        $mresult->free_all();
+    }
 }
