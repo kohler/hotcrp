9cd4f29a645b87c383ff5f753695eb8dd28510e7
Eddie Kohler <ekohler@gmail.com>
2017-09-11 15:40:15 -0400

    batch/savepapers: Users created by --reviews obey --disable-users.

--- batch/savepapers.php
+++ batch/savepapers.php
@@ -216,6 +216,7 @@ foreach ($jp as &$j) {
                 $tf->req["override"] = true;
                 $tf->paperId = $pid;
                 $user_req = Text::analyze_name(["name" => get($tf->req, "reviewerName"), "email" => $tf->req["reviewerEmail"], "affiliation" => get($tf->req, "reviewerAffiliation")]);
+                $user_req->disabled = $disable_users;
                 $user = Contact::create($Conf, $user_req);
                 $tf->check_and_save($site_contact, $prow, null);
             } else
