6017748021270662afceed65c728f05c49bf1581
Eddie Kohler <ekohler@gmail.com>
2017-12-30 16:21:11 -0500

    Style/spacing.

--- src/settings/s_basics.php
+++ src/settings/s_basics.php
@@ -4,42 +4,59 @@
 
 class Basics_SettingRenderer {
 static function render(SettingValues $sv) {
-    echo '<div class="f-c">', $sv->label("opt.shortName", "Conference abbreviation"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.shortName", "Conference abbreviation"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.shortName");
-    echo '<div class="f-h">Examples: “HotOS XIV”, “NSDI \'14”</div>';
-    echo "<div class=\"g\"></div>\n";
+    echo '<div class="f-h">Examples: “HotOS XIV”, “NSDI \'14”</div>',
+        "</div></div>\n";
 
     if ($sv->oldv("opt.longName") == $sv->oldv("opt.shortName"))
         $sv->set_oldv("opt.longName", "");
-    echo "<div class='f-c'>", $sv->label("opt.longName", "Conference name"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.longName", "Conference name"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.longName");
-    echo '<div class="f-h">Example: “14th Workshop on Hot Topics in Operating Systems”</div>';
-    echo "<div class=\"g\"></div>\n";
+    echo '<div class="f-h">Example: “14th Workshop on Hot Topics in Operating Systems”</div>',
+        "</div></div>\n";
 
-    echo "<div class='f-c'>", $sv->label("opt.conferenceSite", "Conference URL"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.conferenceSite", "Conference URL"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.conferenceSite");
-    echo '<div class="f-h">Example: “http://yourconference.org/”</div>';
+    echo '<div class="f-h">Example: “http://yourconference.org/”</div>',
+        "</div></div>\n";
 
 
     echo '<div class="lg"></div>', "\n";
 
-    echo '<div class="f-c">', $sv->label("opt.contactName", "Name of site contact"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.contactName", "Name of site contact"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.contactName");
-    echo '<div class="g"></div>', "\n";
+    echo "</div></div>\n";
 
-    echo "<div class='f-c'>", $sv->label("opt.contactEmail", "Email of site contact"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.contactEmail", "Email of site contact"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.contactEmail");
-    echo '<div class="f-h">The site contact is the contact point for users if something goes wrong. It defaults to the chair.</div>';
+    echo '<div class="f-h">The site contact is the contact point for users if something goes wrong. It defaults to the chair.</div>',
+        "</div></div>\n";
 
 
     echo '<div class="lg"></div>', "\n";
 
-    echo '<div class="f-c">', $sv->label("opt.emailReplyTo", "Reply-To field for email"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.emailReplyTo", "Reply-To field for email"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.emailReplyTo");
-    echo '<div class="g"></div>', "\n";
+    echo "</div></div>\n";
 
-    echo '<div class="f-c">', $sv->label("opt.emailCc", "Default Cc for reviewer email"), "</div>\n";
+    echo '<div class="f-i"><div class="f-c">',
+        $sv->label("opt.emailCc", "Default Cc for reviewer email"),
+        '</div><div class="f-e">';
     $sv->echo_entry("opt.emailCc");
-    echo '<div class="f-h">This applies to email sent to reviewers and email sent using the <a href="', hoturl("mail"), '">mail tool</a>. It doesn’t apply to account-related email or email sent to submitters.</div>';
+    echo '<div class="f-h">This applies to email sent to reviewers and email sent using the <a href="', hoturl("mail"), '">mail tool</a>. It doesn’t apply to account-related email or email sent to submitters.</div>',
+        "</div></div>\n";
 }
 }
