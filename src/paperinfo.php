kohler/hotcrp

commit 82bdc0ead15aa91a4bf60be02d6acd5f38baf844

Call TagMap::strip_nonviewable(), not Tagger::.

src/paperinfo.php
@@ -1034,12 +1034,15 @@ class PaperInfo {
 
     function viewable_tags(Contact $user, $forceShow = null) {
         // see also Contact::can_view_tag()
-        if ($user->can_view_most_tags($this, $forceShow))
-            return Tagger::strip_nonviewable($this->all_tags_text(), $user);
-        else if ($user->privChair && $user->can_view_tags($this, $forceShow))
-            return Tagger::strip_nonsitewide($this->all_tags_text(), $user);
-        else
-            return "";
+        $tags = (string) $this->all_tags_text();
+        if ($tags !== "" && $user->isPC) {
+            $dt = $this->conf->tags();
+            if ($user->can_view_most_tags($this, $forceShow))
+                $tags = $dt->strip_nonviewable($tags, $user, $this);
+            else if ($dt->has_sitewide && $user->can_view_tags($this, $forceShow))
+                $tags = Tagger::strip_nonsitewide($tags, $user);
+        }
+        return $tags;
     }
 
     function editable_tags(Contact $user) {

autoassign.php
@@ -586,7 +586,7 @@ if (!empty($pctags)) {
         $tagsjson[$pc->contactId] = " " . trim(strtolower($pc->viewable_tags($Me))) . " ";
     Ht::stash_script("var hotcrp_pc_tags=" . json_encode($tagsjson) . ";");
     foreach ($pctags as $tagname => $pctag)
-        if ($tagname !== "pc" && Tagger::strip_nonviewable($tagname, $Me))
+        if ($tagname !== "pc" && $Conf->tags()->strip_nonviewable($tagname, $Me, null))
             $pctyp_sel[] = [$pctag, "#$pctag"];
 }
 $pctyp_sel[] = array("__flip__", "flip");

lib/tagger.php
@@ -473,6 +473,20 @@ class TagMap implements IteratorAggregate {
     }
 
 
+    function strip_nonviewable($tags, Contact $user = null, PaperInfo $prow = null) {
+        if (strpos($tags, "~") !== false) {
+            $re = "{ (?:";
+            if ($user && $user->contactId)
+                $re .= "(?!" . $user->contactId . "~)";
+            $re .= "\\d+~";
+            if (!($user && $user->privChair))
+                $re .= "|~+";
+            $tags = trim(preg_replace($re . ")\\S+}", "", " $tags "));
+        }
+        return $tags;
+    }
+
+
     static function make(Conf $conf) {
         $map = new TagMap($conf);
         $ct = $conf->setting_data("tag_chair", "");

src/commentinfo.php
@@ -223,7 +223,7 @@ class CommentInfo {
     function viewable_tags(Contact $user) {
         if ($this->commentTags
             && $user->can_view_comment_tags($this->prow, $this))
-            return Tagger::strip_nonviewable($this->commentTags, $user);
+            return $this->conf->tags()->strip_nonviewable($this->commentTags, $user, $this->prow);
         else
             return null;
     }
@@ -231,7 +231,7 @@ class CommentInfo {
     function viewable_nonresponse_tags(Contact $user) {
         if ($this->commentTags
             && $user->can_view_comment_tags($this->prow, $this)) {
-            $tags = Tagger::strip_nonviewable($this->commentTags, $user);
+            $tags = $this->conf->tags()->strip_nonviewable($this->commentTags, $user, $this->prow);
             if ($this->commentType & COMMENTTYPE_RESPONSE)
                 $tags = trim(preg_replace('{ \S*response(?:|#\S+)(?= |\z)}i', "", " $tags "));
             return $tags;

src/contact.php
@@ -794,7 +794,7 @@ class Contact {
     function viewable_tags(Contact $viewer) {
         if ($viewer->can_view_contact_tags() || $viewer->contactId == $this->contactId) {
             $tags = $this->all_contact_tags();
-            return Tagger::strip_nonviewable($tags, $viewer);
+            return $this->conf->tags()->strip_nonviewable($tags, $viewer, null);
         } else
             return "";
     }
@@ -3266,7 +3266,8 @@ class Contact {
                 return !$dt || !$dt->autosearch;
             }
         }
-        if ($twiddle > 0 && substr($tag, 0, $twiddle) != $this->contactId
+        if ($twiddle > 0
+            && substr($tag, 0, $twiddle) != $this->contactId
             && !$rights->can_administer)
             return false;
         if ($twiddle !== false) {

commit b1d66e8679df2ec804080516b958738e9480ced7

Fix PaperInfo::viewable_tags().
Broke earlier today in 82bdc0ead15aa91a4bf60be02d6acd5f38baf844.

src/paperinfo.php
@@ -1034,13 +1034,17 @@ class PaperInfo {
 
     function viewable_tags(Contact $user, $forceShow = null) {
         // see also Contact::can_view_tag()
-        $tags = (string) $this->all_tags_text();
-        if ($tags !== "" && $user->isPC) {
+        $tags = "";
+        if ($user->isPC)
+            $tags = (string) $this->all_tags_text();
+        if ($tags !== "") {
             $dt = $this->conf->tags();
             if ($user->can_view_most_tags($this, $forceShow))
                 $tags = $dt->strip_nonviewable($tags, $user, $this);
             else if ($dt->has_sitewide && $user->can_view_tags($this, $forceShow))
                 $tags = Tagger::strip_nonsitewide($tags, $user);
+            else
+                $tags = "";
         }
         return $tags;
     }