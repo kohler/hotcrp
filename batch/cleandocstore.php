18b6b047b0c5cac0d26e9ec95bd14a5631d7b556
Eddie Kohler <ekohler@gmail.com>
2018-05-09 16:39:30 -0400

    Introduce docstore -u argument: remove until below threshold.

--- batch/cleandocstore.php
+++ batch/cleandocstore.php
@@ -1,19 +1,18 @@
 <?php
 
-$arg = getopt("hn:c:Vm:d", array("help", "name:", "count:", "verbose", "match:", "dry-run"));
-if (isset($arg["c"]) && !isset($arg["count"]))
-    $arg["count"] = $arg["c"];
-if (isset($arg["V"]) && !isset($arg["verbose"]))
-    $arg["verbose"] = $arg["V"];
-if (isset($arg["m"]) && !isset($arg["match"]))
-    $arg["match"] = $arg["m"];
-if (isset($arg["d"]) && !isset($arg["dry-run"]))
-    $arg["dry-run"] = $arg["d"];
+$arg = getopt("hn:c:Vm:du:", ["help", "name:", "count:", "verbose", "match:", "dry-run", "max-usage:"]);
+foreach (["c" => "count", "V" => "verbose", "m" => "match", "d" => "dry-run",
+          "u" => "max-usage"] as $s => $l) {
+    if (isset($arg[$s]) && !isset($arg[$l]))
+        $arg[$l] = $arg[$s];
+}
 if (isset($arg["h"]) || isset($arg["help"])) {
-    fwrite(STDOUT, "Usage: php batch/cleandocstore.php [-c COUNT] [-V] [-m MATCH] [--dry-run]\n");
+    fwrite(STDOUT, "Usage: php batch/cleandocstore.php [-c COUNT] [-V] [-m MATCH]\n"
+                 . "           [-n|--dry-run] [-u USAGELIMIT]\n");
     exit(0);
-} else if (isset($arg["count"]) && !ctype_digit($arg["count"])) {
-    fwrite(STDERR, "Usage: php batch/cleandocstore.php [-c COUNT] [-V] [-m MATCH] [--dry-run]\n");
+}
+if (isset($arg["count"]) && !ctype_digit($arg["count"])) {
+    fwrite(STDERR, "batch/cleandocstore.php: `-c` expects integer\n");
     exit(1);
 }
 
@@ -87,15 +86,43 @@ class Cleaner {
     }
 }
 
-if (!($dp = $Conf->docstore())) {
-   fwrite(STDERR, "php batch/cleandocstore.php: Conference doesn't use docstore\n");
+$dp = $Conf->docstore();
+if (!$dp) {
+   fwrite(STDERR, "batch/cleandocstore.php: Conference doesn't use docstore\n");
    exit(1);
 }
+
 $count = isset($arg["count"]) ? intval($arg["count"]) : 10;
 $verbose = isset($arg["verbose"]);
 $dry_run = isset($arg["dry-run"]);
+$usage_threshold = $usage_directory = null;
 if (isset($arg["match"]))
     Cleaner::set_match($arg["match"]);
+
+if (isset($arg["max-usage"])) {
+    if (!is_numeric($arg["max-usage"])
+        || (float) $arg["max-usage"] < 0
+        || (float) $arg["max-usage"] > 1) {
+        fwrite(STDERR, "batch/cleandocstore.php: `-u` expects fraction between 0 and 1\n");
+        exit(1);
+    }
+    preg_match('{\A((?:/[^/%]*(?=/|\z))+)}', $dp, $m);
+    $usage_directory = $m[1];
+    $ts = disk_total_space($usage_directory);
+    $fs = disk_free_space($usage_directory);
+    if ($ts === false || $fs === false) {
+        fwrite(STDERR, "$usage_directory: cannot evaluate free space\n");
+        exit(1);
+    } else if (($ts - $fs) / $ts <= (float) $arg["max-usage"]) {
+        if ($verbose) {
+            fwrite(STDOUT, "$usage_directory: free space above threshold\n");
+            exit(0);
+        }
+    } else if (!isset($arg["count"])) {
+        $count = 5000;
+        $usage_threshold = (1 - (float) $arg["max-usage"]) * $ts;
+    }
+}
 
 class Fparts {
     public $components = [];
@@ -397,6 +424,11 @@ while ($count > 0) {
     }
     if (!$x) {
         fwrite(STDERR, "Can't find anything to delete.\n");
+        break;
+    }
+    if ($usage_threshold && disk_free_space($usage_directory) >= $usage_threshold) {
+        if ($verbose)
+            fwrite(STDOUT, "$usage_directory: free space above threshold\n");
         break;
     }
 
