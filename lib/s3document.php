0244c6cd475a88a80cee35566a933e7dee71c9c1
Eddie Kohler <ekohler@gmail.com>
2018-05-10 19:22:20 -0400

    S3Document: Streamline it.

--- lib/s3document.php
+++ lib/s3document.php
@@ -3,7 +3,6 @@
 // Copyright (c) 2006-2018 Eddie Kohler; see LICENSE.
 
 class S3Document {
-
     const EMPTY_SHA256 = "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855";
     static private $known_headers = array("cache-control" => 1, "content-disposition" => 1,
                                           "content-encoding" => 1, "expires" => 1);
@@ -87,14 +86,11 @@ class S3Document {
             $query = join("&", $a);
         }
 
-        $chdr = array("host" => $host);
-        $hdrtext = "Host: $host\r\n";
+        $chdr = array("Host" => $host);
         foreach ($hdr as $k => $v)
-            if (($lk = strtolower($k)) !== "host"
-                && $lk !== "method") {
+            if (strcasecmp($k, "host") && strcasecmp($k, "method")) {
                 $v = trim($v);
-                $chdr[$lk] = $v;
-                $hdrtext .= "$k: $v\r\n";
+                $chdr[$k] = $v;
             }
         if (!isset($chdr["x-amz-content-sha256"])) {
             if ($content !== false && $content !== "" && $content !== null)
@@ -102,17 +98,17 @@ class S3Document {
             else
                 $h = self::EMPTY_SHA256;
             $chdr["x-amz-content-sha256"] = $h;
-            $hdrtext .= "x-amz-content-sha256: $h\r\n";
         }
         if (!isset($chdr["x-amz-date"])) {
             $d = gmdate("Ymd\\THis\\Z", $current_time);
             $chdr["x-amz-date"] = $d;
-            $hdrtext .= "x-amz-date: $d\r\n";
         }
 
-        ksort($chdr);
+        $shdr = $chdr;
+        ksort($shdr, SORT_STRING | SORT_FLAG_CASE);
         $chk = $chv = "";
-        foreach ($chdr as $k => $v) {
+        foreach ($shdr as $k => $v) {
+            $k = strtolower($k);
             $chk .= ";" . $k;
             $chv .= $k . ":" . $v . "\n";
         }
@@ -131,23 +127,23 @@ class S3Document {
             . $scope . "\n"
             . hash("sha256", $canonical_request);
 
+        $hdrarr = [];
+        foreach ($chdr as $k => $v)
+            $hdrarr[] = $k . ": " . $v;
         $signature = hash_hmac("sha256", $signable, $signing_key);
-        $hdrtext .= "Authorization: AWS4-HMAC-SHA256 Credential="
+        $hdrarr[] = "Authorization: AWS4-HMAC-SHA256 Credential="
             . $this->s3_key . "/" . $scope
             . ",SignedHeaders=" . substr($chk, 1)
-            . ",Signature=" . $signature . "\r\n";
-
-        return array("header" => $hdrtext, "signature" => $signature);
+            . ",Signature=" . $signature;
+        return ["headers" => $hdrarr, "signature" => $signature];
     }
 
-    private function http_headers($filename, $method, $args) {
-        list($content, $content_type, $user_data) =
-            array(get($args, "content"), get($args, "content_type"), get($args, "user_data"));
-        $url = "https://$this->s3_bucket.s3.amazonaws.com/$filename";
-        $hdr = array("method" => $method,
-                     "Date" => gmdate("D, d M Y H:i:s", $this->fixed_time ? : time()) . " GMT");
+    private function stream_headers($filename, $method, $args) {
+        $url = "https://{$this->s3_bucket}.s3.amazonaws.com/{$filename}";
+        $hdr = ["method" => $method,
+                "Date" => gmdate("D, d M Y H:i:s", $this->fixed_time ? : time()) . " GMT"];
         $content = $content_type = null;
-        foreach ($args as $key => $value)
+        foreach ($args as $key => $value) {
             if ($key === "user_data") {
                 foreach ($value as $xkey => $xvalue) {
                     if (!get(self::$known_headers, strtolower($xkey)))
@@ -160,8 +156,9 @@ class S3Document {
                 $content_type = $value;
             else
                 $hdr[$key] = $value;
+        }
         $sig = $this->signature($url, $hdr, $content);
-        $hdr["header"] = $sig["header"] . "Connection: close\r\n";
+        $hdr["header"] = join("\r\n", $sig["headers"]) . "\r\nConnection: close\r\n";
         $content_empty = (string) $content === "";
         if (!$content_empty && $content_type)
             $hdr["header"] .= "Content-Type: $content_type\r\n";
@@ -171,7 +168,7 @@ class S3Document {
         return array($url, $hdr);
     }
 
-    private function parse_response_headers($url, $metadata) {
+    private function parse_stream_response($url, $metadata) {
         $this->response_headers["url"] = $url;
         if ($metadata && ($w = get($metadata, "wrapper_data")) && is_array($w)) {
             if (preg_match(',\AHTTP/[\d.]+\s+(\d+)\s+(.+)\z,', $w[0], $m)) {
@@ -187,19 +184,21 @@ class S3Document {
         }
     }
 
-    private function run_once($filename, $method, $args) {
+    private function run_setup($filename) {
         $this->status = $this->status_text = null;
         $this->response_headers = $this->user_data = array();
         if ((string) $filename === "") {
             $this->status = 404;
             $this->status_text = "Filename missing";
-            return;
         }
+        return $this->status === null;
+    }
 
-        list($url, $hdr) = $this->http_headers($filename, $method, $args);
-        $context = stream_context_create(array("http" => $hdr));
+    private function run_stream_once($filename, $method, $args) {
+        list($url, $hdr) = $this->stream_headers($filename, $method, $args);
+        $context = stream_context_create(["http" => $hdr]);
         if (($stream = fopen($url, "r", false, $context))) {
-            $this->parse_response_headers($url, stream_get_meta_data($stream));
+            $this->parse_stream_response($url, stream_get_meta_data($stream));
             $this->response_headers["content"] = stream_get_contents($stream);
             fclose($stream);
         }
@@ -207,7 +206,9 @@ class S3Document {
 
     private function run($filename, $method, $args) {
         for ($i = 1; $i <= 5; ++$i) {
-            $this->run_once($filename, $method, $args);
+            if (!$this->run_setup($filename))
+                return;
+            $this->run_stream_once($filename, $method, $args);
             if (($this->status !== null && $this->status !== 500)
                 || self::$retry_timeout_allowance <= 0)
                 return;
@@ -219,17 +220,17 @@ class S3Document {
     }
 
     function save($filename, $content, $content_type, $user_data = null) {
-        $this->run($filename, "HEAD", array());
+        $this->run($filename, "HEAD", []);
         if ($this->status != 200
             || get($this->response_headers, "content-length") != strlen($content))
-            $this->run($filename, "PUT", array("content" => $content,
-                                               "content_type" => $content_type,
-                                               "user_data" => $user_data));
+            $this->run($filename, "PUT", ["content" => $content,
+                                          "content_type" => $content_type,
+                                          "user_data" => $user_data]);
         return $this->status == 200;
     }
 
     function load($filename) {
-        $this->run($filename, "GET", array());
+        $this->run($filename, "GET", []);
         if ($this->status == 404 || $this->status == 500)
             return null;
         if ($this->status != 200)
@@ -254,7 +255,7 @@ class S3Document {
 
     function ls($prefix, $args = array()) {
         $suffix = "?list-type=2&prefix=" . urlencode($prefix);
-        foreach (array("max-keys", "start-after", "continuation-token") as $k)
+        foreach (["max-keys", "start-after", "continuation-token"] as $k)
             if (isset($args[$k]))
                 $suffix .= "&" . $k . "=" . urlencode($args[$k]);
         $this->run($suffix, "GET", array());
