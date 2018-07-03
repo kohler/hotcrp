0d1ae4761200bd2f75bfd42288865a3b3a4a3e69
Eddie Kohler <ekohler@gmail.com>
2018-07-02 20:49:52 -0400

    Add test/check.sh script.

---
+++ test/check.sh
@@ -0,0 +1,18 @@
+#! /bin/sh
+a=0
+runcheck () {
+    echo $1:
+    php -d error_reporting=E_ALL $1
+    z=$?
+    echo
+    if [ "$a" = 0 ]; then a=$z; fi
+}
+
+runcheck test/test01.php
+runcheck test/test02.php
+runcheck test/test03.php
+runcheck test/test04.php
+runcheck test/test05.php
+runcheck test/test06.php
+
+exit $a
