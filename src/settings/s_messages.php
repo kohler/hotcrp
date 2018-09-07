382e483dc80341dc087b62d5ec7c935dee7a88a0
Eddie Kohler <ekohler@gmail.com>
2018-09-07 09:29:02 -0400

    Setting rendering.

--- src/settings/s_messages.php
+++ src/settings/s_messages.php
@@ -6,11 +6,11 @@ class Messages_SettingRenderer {
     static function render(SettingValues $sv) {
         $sv->echo_message("msg.home", "Home page message");
         $sv->echo_message("msg.clickthrough_submit", "Clickthrough submission terms",
-                   "<div class=\"hint fx\">Users must “accept” these terms to edit a submission. Use HTML and include a headline, such as “&lt;h2&gt;Submission terms&lt;/h2&gt;”.</div>");
+                   "<div class=\"f-h fx\">Users must “accept” these terms to edit a submission. Use HTML and include a headline, such as “&lt;h2&gt;Submission terms&lt;/h2&gt;”.</div>");
         $sv->echo_message("msg.submit", "Submission message",
-                   "<div class=\"hint fx\">This message will appear on submission editing pages.</div>");
+                   "<div class=\"f-h fx\">This message will appear on submission editing pages.</div>");
         $sv->echo_message("msg.clickthrough_review", "Clickthrough reviewing terms",
-                   "<div class=\"hint fx\">Users must “accept” these terms to edit a review. Use HTML and include a headline, such as “&lt;h2&gt;Submission terms&lt;/h2&gt;”.</div>");
+                   "<div class=\"f-h fx\">Users must “accept” these terms to edit a review. Use HTML and include a headline, such as “&lt;h2&gt;Submission terms&lt;/h2&gt;”.</div>");
         $sv->echo_message("msg.conflictdef", "Definition of conflict of interest");
         $sv->echo_message("msg.revprefdescription", "Review preference instructions");
     }
