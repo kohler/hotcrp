d69267132dc23a2fb950d7bbd52b36d63dbbac22
KooSeHwa <44987426+KooSeHwa@users.noreply.github.com>
2018-11-29 23:23:34 +0900

    dataset

---
+++ OSS_Final_Project/Visualization/topicPaper/index.js
@@ -0,0 +1,27 @@
+define(function() {
+    return {
+        name: "flare", children:
+        [
+		{ name: "BIG DATA Security", size: 2*100},
+		{ name: "Cloud Security", size: 6*100},
+		{ name: "Content Security", size: 5*100},
+		{ name: "Convergence and Industrial Security", size: 1*100},
+		{ name: "CPS Security", size: 2*100},
+		{ name: "Cryptographic Theory", size: 1*100},
+		{ name: "Digital forensics", size: 0*100},
+		{ name: "Financial Security",size: 12*100},
+		{ name: "Hacking and Vulnerability Analysis", size: 2*100},
+		{ name: "Information Protection Policy, Law, System", size: 0*100},
+		{ name: "Information Protection Standard", size: 4*100},
+		{ name: "Information Security Education", size: 5*100},
+		{ name: "Information Security Evaluation and Certification", size: 1*100},
+		{ name: "IoT Security", size: 2*100},
+		{ name: "Mobile Security", size: 6*100},
+		{ name: "Network Security", size: 0*100},
+		{ name: "Privacy, Identity Management", size: 1*100},
+		{ name: "SNS Security", size: 1*100},
+		{ name: "System Security", size: 0*100},
+		{ name: "Other Information Protection related fields", size: 3*100
+		}]
+    };
+});
