96101eb89a33011ff5d35a0b37776298e49797f4
Eddie Kohler <ekohler@gmail.com>
2019-01-03 12:48:01 -0500

    Multiuser: Handle `/u` and `/u/XX` and `/u/0` prefixes by redirecting.

--- index.php
+++ index.php
@@ -3,26 +3,34 @@
 // Copyright (c) 2006-2018 Eddie Kohler; see LICENSE.
 
 require_once("lib/navigation.php");
+$nav = Navigation::get();
 
-$page = Navigation::page();
-
-if ($page === "u"
-    && ($unum = Navigation::path_component(0)) !== false
-    && ctype_digit($unum)) {
-    $page = Navigation::shift_path_components(2);
+// handle `/u/USERINDEX/`
+if ($nav->page === "u") {
+    $unum = $nav->path_component(0);
+    if ($unum !== false && ctype_digit($unum)) {
+        if (!$nav->shift_path_components(2)) {
+            // redirect `/u/USERINDEX` => `/u/USERINDEX/`
+            Navigation::redirect($nav->server . $nav->base_path . "u/" . $unum . "/" . $nav->query);
+        }
+    } else {
+        // redirect `/u/XXXX` => `/`
+        Navigation::redirect($nav->server . $nav->base_path . $nav->query);
+    }
 }
 
-if ($page === "images" || $page === "scripts" || $page === "stylesheets") {
-    $_GET["file"] = $page . Navigation::path();
+// handle special pages
+if ($nav->page === "images" || $nav->page === "scripts" || $nav->page === "stylesheets") {
+    $_GET["file"] = $nav->page . $nav->path;
     include("cacheable.php");
     exit;
-} else if ($page === "api" || $page === "cacheable") {
-    include("$page.php");
+} else if ($nav->page === "api" || $nav->page === "cacheable") {
+    include("{$nav->page}.php");
     exit;
 }
 
 require_once("src/initweb.php");
-$page_template = $Conf->page_template($page);
+$page_template = $Conf->page_template($nav->page);
 
 if (!$page_template) {
     header("HTTP/1.0 404 Not Found");
