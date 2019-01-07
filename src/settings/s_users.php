a1c37a337e42d46719edf17283be37ea28a49627
Eddie Kohler <ekohler@gmail.com>
2019-01-07 08:41:23 -0500

    Remove acct_addr setting entirely.

--- src/settings/s_users.php
+++ src/settings/s_users.php
@@ -4,8 +4,6 @@
 
 class Users_SettingRenderer {
     static function render(SettingValues $sv) {
-        if ($sv->curv("acct_addr"))
-            $sv->echo_checkbox("acct_addr", "Collect users’ addresses and phone numbers");
         echo "<h3 class=\"settings g\">Program committee &amp; system administrators</h3>";
         echo "<p><a href='", hoturl("profile", "u=new&amp;role=pc"), "' class='btn'>Create PC accounts</a> <span class='barsep'>·</span> ",
             "Select a user’s name to edit a profile.</p>\n";
