kohler/hotcrp

commit b3e67545e729b0e6114083f7a52f233b278cb38d

Introduce JSON-extensible capability tokens.
And move capability handling to CapabilityManager.

src/conference.php
@@ -153,6 +153,7 @@ class Conf {
     private $_paper_column_factories;
     private $_option_type_map;
     private $_option_type_factories;
+    private $_capability_factories;
     private $_hook_map;
     private $_hook_factories;
     public $_file_filters; // maintained externally
@@ -861,7 +862,9 @@ class Conf {
         $this->_xt_factory_error = null;
         $xts = [];
         foreach ($factories as $fxt) {
-            if (self::xt_priority_compare($fxt, $found) >= 0)
+            if (empty($xts)
+                ? self::xt_priority_compare($fxt, $found) >= 0
+                : self::xt_priority_compare($fxt, $xts[0]) > 0)
                 break;
             if ($fxt->match === ".*")
                 $m = [$name];
@@ -882,7 +885,7 @@ class Conf {
                 if ($prio <= 0 && call_user_func($checkf, $xt)) {
                     if ($prio < 0)
                         $xts = [];
-                    $xts[] = $found = $xt;
+                    $xts[] = $xt;
                 }
             }
         }
@@ -1791,40 +1794,6 @@ class Conf {
             if (empty($_SESSION[$this->dsn]))
                 unset($_SESSION[$this->dsn]);
         }
-    }
-
-    function capability_text($prow, $capType) {
-        // A capability has the following representation (. is concatenation):
-        //    capFormat . paperId . capType . hashPrefix
-        // capFormat -- Character denoting format (currently 0).
-        // paperId -- Decimal representation of paper number.
-        // capType -- Capability type (e.g. "a" for author view).
-        // To create hashPrefix, calculate a SHA-1 hash of:
-        //    capFormat . paperId . capType . paperCapVersion . capKey
-        // where paperCapVersion is a decimal representation of the paper's
-        // capability version (usually 0, but could allow conference admins
-        // to disable old capabilities paper-by-paper), and capKey
-        // is a random string specific to the conference, stored in Settings
-        // under cap_key (created in load_settings).  Then hashPrefix
-        // is the base-64 encoding of the first 8 bytes of this hash, except
-        // that "+" is re-encoded as "-", "/" is re-encoded as "_", and
-        // trailing "="s are removed.
-        //
-        // Any user who knows the conference's cap_key can construct any
-        // capability for any paper.  Longer term, one might set each paper's
-        // capVersion to a random value; but the only way to get cap_key is
-        // database access, which would give you all the capVersions anyway.
-
-        if (!isset($this->settingTexts["cap_key"])) {
-            $key = base64_encode(random_bytes(16));
-            if ((string) $key === "" || !$this->__save_setting("cap_key", 1, $key))
-                return false;
-        }
-        $start = "0" . $prow->paperId . $capType;
-        $hash = sha1($start . $prow->capVersion . $this->settingTexts["cap_key"], true);
-        $suffix = str_replace(array("+", "/", "="), array("-", "_", ""),
-                              base64_encode(substr($hash, 0, 8)));
-        return $start . $suffix;
     }
 
 
@@ -3925,6 +3894,29 @@ class Conf {
         if (($expansions = $this->xt_search_factories($this->_option_type_factories, $name, [$this, "xt_allowed"], $uf, null, "i")))
             $uf = $expansions[0];
         return $uf;
+    }
+
+
+    // capability tokens
+
+    function _add_capability_json($fj) {
+        if (isset($fj->match) && is_string($fj->match)
+            && isset($fj->callback) && is_string($fj->callback)) {
+            $this->_capability_factories[] = $fj;
+            return true;
+        } else
+            return false;
+    }
+    function capability_handler($cap) {
+        if ($this->_capability_factories === null) {
+            $this->_capability_factories = [];
+            expand_json_includes_callback(["etc/capabilityhandlers.json"], [$this, "_add_capability_json"]);
+            if (($olist = $this->opt("capabilityHandlers")))
+                expand_json_includes_callback($olist, [$this, "_add_capability_json"]);
+            usort($this->_capability_factories, "Conf::xt_priority_compare");
+        }
+        $expansions = $this->xt_search_factories($this->_capability_factories, $cap, [$this, "xt_allowed"], null);
+        return $expansions ? $expansions[0] : null;
     }
 
 

src/capability.php
@@ -58,4 +58,47 @@ class CapabilityManager {
         if ($capdata)
             Dbl::ql($this->dblink, "delete from Capability where salt=?", $capdata->salt);
     }
+
+
+
+    static function capability_text($prow, $capType) {
+        // A capability has the following representation (. is concatenation):
+        //    capFormat . paperId . capType . hashPrefix
+        // capFormat -- Character denoting format (currently 0).
+        // paperId -- Decimal representation of paper number.
+        // capType -- Capability type (e.g. "a" for author view).
+        // To create hashPrefix, calculate a SHA-1 hash of:
+        //    capFormat . paperId . capType . paperCapVersion . capKey
+        // where paperCapVersion is a decimal representation of the paper's
+        // capability version (usually 0, but could allow conference admins
+        // to disable old capabilities paper-by-paper), and capKey
+        // is a random string specific to the conference, stored in Settings
+        // under cap_key (created in load_settings).  Then hashPrefix
+        // is the base-64 encoding of the first 8 bytes of this hash, except
+        // that "+" is re-encoded as "-", "/" is re-encoded as "_", and
+        // trailing "="s are removed.
+        //
+        // Any user who knows the conference's cap_key can construct any
+        // capability for any paper.  Longer term, one might set each paper's
+        // capVersion to a random value; but the only way to get cap_key is
+        // database access, which would give you all the capVersions anyway.
+
+        $key = $prow->conf->setting_data("cap_key");
+        if (!$key) {
+            $key = base64_encode(random_bytes(16));
+            if (!$key || !$prow->conf->save_setting("cap_key", 1, $key))
+                return false;
+        }
+        $start = "0" . $prow->paperId . $capType;
+        $hash = sha1($start . $prow->capVersion . $key, true);
+        $suffix = str_replace(array("+", "/", "="), array("-", "_", ""),
+                              base64_encode(substr($hash, 0, 8)));
+        return $start . $suffix;
+    }
+
+    static function apply_old_author_view(Contact $user, $uf, $isadd) {
+        if (($prow = $user->conf->fetch_paper(["paperId" => $uf->match_data[1]]))
+            && ($uf->name === self::capability_text($prow, "a")))
+            $user->change_paper_capability($prow->paperId, Contact::CAP_AUTHORVIEW, $isadd);
+    }
 }

src/contact.php
@@ -839,13 +839,12 @@ class Contact {
     function apply_capability_text($text) {
         // Add capabilities from arguments
         foreach (preg_split('{\s+}', $text) as $cap) {
-            if (preg_match('{\A([-+]?)0([1-9][0-9]*)(a)(\S+)\z}', $cap, $m)
-                && ($result = $this->conf->ql("select paperId, capVersion from Paper where paperId=$m[2]"))
-                && ($row = edb_orow($result))) {
-                $rowcap = $this->conf->capability_text($row, $m[3]);
-                $text = substr($text, strlen($m[1]));
-                if ($rowcap === $text)
-                    $this->change_paper_capability((int) $m[2], self::CAP_AUTHORVIEW, $m[1] !== "-");
+            if ($cap !== "") {
+                $isadd = $cap[0] !== "-";
+                if ($cap[0] === "-" || $cap[0] === "+")
+                    $cap = substr($cap, 1);
+                if ($cap !== "" && ($uf = $this->conf->capability_handler($cap)))
+                    call_user_func($uf->callback, $this, $uf, $isadd, $cap);
             }
         }
     }

src/hotcrpmailer.php
@@ -291,7 +291,7 @@ class HotCRPMailer extends Mailer {
             && isset($this->row->capVersion)
             && $this->recipient->act_author_view($this->row)) {
             if (!$this->sensitivity)
-                return "cap=" . $this->conf->capability_text($this->row, "a");
+                return "cap=" . CapabilityManager::capability_text($this->row, "a");
             else if ($this->sensitivity === "display")
                 return "cap=HIDDEN";
         }

src/tarball.sh
@@ -93,6 +93,7 @@ etc/.htaccess
 etc/affiliationmatchers.json
 etc/apifunctions.json
 etc/assignmentparsers.json
+etc/capabilityhandlers.json
 etc/emojicodes.json
 etc/formulafunctions.json
 etc/helptopics.json

test/test01.php
@@ -97,7 +97,7 @@ check_paper1($Conf->fetch_paper(1, $user_estrin));
 // grant user capability to read paper 1, check it doesn't allow PC view
 $user_capability = new Contact;
 xassert(!$user_capability->can_view_paper($paper1));
-$user_capability->apply_capability_text($Conf->capability_text($paper1, "a"));
+$user_capability->apply_capability_text(CapabilityManager::capability_text($paper1, "a"));
 xassert(!$user_capability->contactId);
 xassert($user_capability->can_view_paper($paper1));
 xassert(!$user_capability->allow_administer($paper1));

commit f9cd1c381c6f935d2c46ee80f857e4fe91faa99b

Recover checkbox display for review fields.
Broken in b3e67545e729b0e6114083f7a52f233b278cb38d.
Sorry, Julia Lawall!

src/conference.php
@@ -886,7 +886,7 @@ class Conf {
                 if ($prio <= 0 && call_user_func($checkf, $xt)) {
                     if ($prio < 0)
                         $xts = [];
-                    $xts[] = $xt;
+                    $xts[] = $found = $xt;
                 }
             }
         }