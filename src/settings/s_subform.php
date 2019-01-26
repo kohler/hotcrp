ea3eac58257f75e9cc8e0adc1ca36ef64b3de623
Eddie Kohler <ekohler@gmail.com>
2019-01-25 19:15:51 -0500

    Derp

--- src/settings/s_subform.php
+++ src/settings/s_subform.php
@@ -17,7 +17,6 @@ class BanalSettings {
 
         $open = $sv->curv("sub_banal$suffix") > 0;
         $uropen = $open && !in_array($sv->curv("sub_banal_pagelimit$suffix"), ["", "N/A"]);
-        error_log($sv->curv("sub_banal_pagelimit$suffix"));
         $sv->echo_checkbox("sub_banal$suffix", "PDF format checker<span class=\"fx\">:</span>", ["class" => "uich js-foldup", "group_class" => "settings-g has-fold " . ($open ? "foldo" : "foldc") . ($uropen ? " fold2o" : " fold2c"), "group_open" => true]);
         echo Ht::hidden("has_sub_banal$suffix", 1),
             '<div class="settings-2col fx">';
