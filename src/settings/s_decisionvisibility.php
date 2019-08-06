fadb76e194859f67dc250ad8d7065b653cf58051
Eddie Kohler <ekohler@gmail.com>
2019-08-06 11:35:25 -0400

    Add a warning about entirely author-invisible review forms.

--- src/settings/s_decisionvisibility.php
+++ src/settings/s_decisionvisibility.php
@@ -19,14 +19,41 @@ class DecisionVisibility_SettingParser extends SettingParser {
 
         if ($sv->has_interest("seedec")
             && $sv->newv("seedec") == Conf::SEEDEC_ALL
-            && $sv->newv("au_seerev") == Conf::AUSEEREV_NO)
+            && $sv->newv("au_seerev") == Conf::AUSEEREV_NO) {
             $sv->warning_at(null, "Authors can see decisions, but not reviews. This is sometimes unintentional.");
+        }
 
         if (($sv->has_interest("seedec") || $sv->has_interest("sub_sub"))
             && $sv->newv("sub_open")
             && $sv->newv("sub_sub") > $Now
             && $sv->newv("seedec") != Conf::SEEDEC_ALL
-            && $sv->conf->fetch_value("select paperId from Paper where outcome<0 limit 1") > 0)
+            && $sv->conf->fetch_value("select paperId from Paper where outcome<0 limit 1") > 0) {
             $sv->warning_at(null, "Updates will not be allowed for rejected submissions. This exposes decision information that would otherwise be hidden from authors.");
+        }
+
+        if ($sv->has_interest("au_seerev")
+            && !array_filter($sv->conf->review_form()->all_fields(), function ($f) {
+                return $f->view_score >= VIEWSCORE_AUTHORDEC;
+            })) {
+            $sv->warning_at(null, $sv->setting_link("Authors can see reviews", "au_seerev")
+                . ", but the reviews have no author-visible fields. This is sometimes unintentional; you may want to update "
+                . $sv->setting_link("the review form", "review_form") . ".");
+            $sv->warning_at("au_seerev", false);
+        } else if ($sv->has_interest("au_seerev")
+                   && $sv->newv("seedec") != Conf::SEEDEC_ALL
+                   && $sv->newv("au_seerev") != Conf::AUSEEREV_NO
+                   && !array_filter($sv->conf->review_form()->all_fields(), function ($f) {
+                       return $f->view_score >= VIEWSCORE_AUTHOR;
+                   })
+                   && array_filter($sv->conf->review_form()->all_fields(), function ($f) {
+                       return $f->view_score >= VIEWSCORE_AUTHORDEC;
+                   })) {
+            $sv->warning_at(null, $sv->setting_link("Authors can see reviews", "au_seerev")
+                . ", but since " . $sv->setting_link("they cannot see decisions", "seedec")
+                . ", the reviews have no author-visible fields. This is sometimes unintentional; you may want to update "
+                . $sv->setting_link("the review form", "review_form") . ".");
+            $sv->warning_at("au_seerev", false);
+            $sv->warning_at("seedec", false);
+        }
     }
 }
