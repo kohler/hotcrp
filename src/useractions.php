ceb22804660236a34048567994be9ab22e1c8a7f
Eddie Kohler <ekohler@gmail.com>
2020-01-11 09:11:34 -0500

    Braces.

--- src/useractions.php
+++ src/useractions.php
@@ -5,13 +5,16 @@
 class UserActions {
     static function disable(Contact $user, $ids) {
         $enabled_cids = Dbl::fetch_first_columns($user->conf->dblink, "select contactId from ContactInfo where contactId?a and disabled=0 and contactId!=?", $ids, $user->contactId);
-        if (!$enabled_cids)
+        if (!$enabled_cids) {
             return (object) ["ok" => true, "warnings" => ["Those accounts were already disabled."]];
-        if (!$user->conf->qe("update ContactInfo set disabled=1 where contactId?a and disabled=0", $enabled_cids))
+        }
+        if (!$user->conf->qe("update ContactInfo set disabled=1 where contactId?a and disabled=0", $enabled_cids)) {
             return (object) ["error" => true];
+        }
         $user->conf->save_logs(true);
-        foreach ($enabled_cids as $cid)
+        foreach ($enabled_cids as $cid) {
             $user->conf->log_for($user, $cid, "Account disabled");
+        }
         $user->conf->save_logs(false);
         return (object) ["ok" => true];
     }
@@ -26,14 +29,16 @@ class UserActions {
                 $activate_ids[$row[0]] = (int) $row[0];
         }
         Dbl::free($result);
-        if (empty($disabled_ids))
+        if (empty($disabled_ids)) {
             return (object) ["ok" => true, "warnings" => ["Those accounts were already enabled."]];
+        }
 
         // enable them
         $user->conf->qe("update ContactInfo set disabled=0 where contactId?a", $disabled_ids);
         $user->conf->save_logs(true);
-        foreach ($disabled_ids as $cid)
+        foreach ($disabled_ids as $cid) {
             $user->conf->log_for($user, $cid, "Account enabled");
+        }
         $user->conf->save_logs(false);
 
         // maybe create some passwords
@@ -51,18 +56,21 @@ class UserActions {
         $done = $skipped = [];
         $result = $user->conf->qe("select * from ContactInfo where contactId?a", $ids);
         while (($xuser = Contact::fetch($result, $user->conf))) {
-            if ($user->can_change_password($xuser))
+            if ($user->can_change_password($xuser)) {
                 $done[] = $xuser->email;
-            else
+            } else {
                 $skipped[] = $xuser->email;
+            }
         }
         Dbl::free($result);
 
-        if (!empty($done))
+        if (!empty($done)) {
             $user->conf->qe("update ContactInfo set password='', passwordTime=? where email ?a", $Now, $done);
+        }
         $j = (object) ["ok" => true, "users" => $done];
-        if (!empty($skipped))
+        if (!empty($skipped)) {
             $j->warnings[] = $user->conf->_("Skipped accounts %2\$s.", count($skipped), htmlspecialchars(commajoin($skipped)));
+        }
         return $j;
     }
 
@@ -73,14 +81,16 @@ class UserActions {
             if (!$xuser->is_disabled()) {
                 $xuser->sendAccountInfo("send", false);
                 $done[] = $xuser->email;
-            } else
+            } else {
                 $disabled[] = $xuser->email;
+            }
         }
         Dbl::free($result);
 
         $j = (object) ["ok" => true, "users" => $done];
-        if ($disabled)
+        if ($disabled) {
             $j->warnings[] = $user->conf->_("Skipped disabled accounts %2\$s.", count($disabled), htmlspecialchars(commajoin($disabled)));
+        }
         return $j;
     }
 }
