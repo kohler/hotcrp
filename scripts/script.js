22775a5fb0a13e636cbea20d4cc6bf99739c10a5
Eddie Kohler <ekohler@gmail.com>
2020-01-22 14:11:06 -0500

    Goddamnit

--- scripts/script.js
+++ scripts/script.js
@@ -950,7 +950,7 @@ var handle_ui = (function ($) {
 var callbacks = {};
 function handle_ui(event) {
     var e = event.target;
-    if ((e && (hasClass(e, "ui") || hasClass("uin")))
+    if ((e && (hasClass(e, "ui") || hasClass(e, "uin")))
         || (this.tagName === "A" && hasClass(this, "ui"))) {
         event.preventDefault();
     }
