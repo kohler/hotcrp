223017378c8c7939fdca1c70e7ca8987b445cf1b
Eddie Kohler <ekohler@gmail.com>
2020-02-26 12:48:11 -0500

    Improve vote help.

--- src/help/h_votetags.php
+++ src/help/h_votetags.php
@@ -6,35 +6,33 @@ class VoteTags_HelpTopic {
     static function render($hth) {
         $votetag = $hth->example_tag("vote");
         echo "<p>Some conferences have PC members vote for papers.
-Each PC member is assigned a vote allotment, and can distribute that allotment
-arbitrarily among unconflicted papers.
-Alternately, each PC member can vote, once, for as many papers as they like (“approval voting”).
+In <em>allotment voting</em>,
+each PC member is assigned a vote allotment to distribute
+among unconflicted papers; a PC member might assign one vote to one submission
+and five to another.
+In <em>approval voting</em>, each PC member can vote, once, for as many papers as they like.
 The PC’s aggregated vote totals might help determine
 which papers to discuss.</p>
 
 <p>HotCRP supports voting through ", $hth->help_link("tags", "tags"), ".
 The chair can ", $hth->setting_link("define a set of voting tags", "tag_vote"),
 " and allotments" . $hth->current_tag_list("vote") . ".
-PC members vote by assigning the corresponding twiddle tags;
-the aggregated PC vote is visible in the public tag.</p>
+Votes are represented as twiddle tags, and the vote total is automatically
+computed and shown in the public tag.</p>
 
-<p>For example, assume that an administrator defines a voting tag
- “". $votetag . "” with an allotment of 10.
-To use two votes for a paper, a PC member tags the paper as
-“~". $votetag . "#2”. The system
-automatically adds the tag “". $votetag . "#2” to that
-paper (note the
-lack of the “~”), indicating that the paper has two total votes.
-As other PC members add their votes with their own “~” tags, the system
-updates the main tag to reflect the total.
-(The system ensures no PC member exceeds their allotment.) </p>
+<p>For example, an administrator might define an allotment voting tag
+ “". $votetag . "” with an allotment of 10 votes.
+To assign two votes to a submission, a PC member can either enter that vote
+into a text box on the submission page, or directly tag that submission with
+“~". $votetag . "#2”.
+As other PC members add their votes with their own “~vote” tags, the system
+updates the main “vote” tag to reflect the total.
+(An error is reported when PC members exceed their allotment.) </p>
 
-<p>
-To see the current voting status, search by ", $hth->hotlink("rorder:$votetag", "search", ["q" => "rorder:$votetag"]), ". Use view options to show tags
+<p>To see papers with votes in reverse vote-count order, search by ", $hth->hotlink("rorder:$votetag", "search", ["q" => "rorder:$votetag"]), ". Use view options to show tags
 in the search results (or set up a ", $hth->help_link("formula", "formulas"), ").</p>
 
-<p>
-Hover to learn how the PC voted:</p>
+<p>Hover to learn how the PC voted:</p>
 
 <p>" . Ht::img("extagvotehover.png", "[Hovering over a voting tag]", ["width" => 390, "height" => 46]) . "</p>";
     }
