4d91d0e6d7799f35870f49132741195af60b1a48
Eddie Kohler <ekohler@gmail.com>
2020-03-05 21:47:30 -0500

    Improve Firefox column break behavior.

--- src/papercolumns/pc_conflictmatch.php
+++ src/papercolumns/pc_conflictmatch.php
@@ -9,8 +9,9 @@ class ConflictMatch_PaperColumn extends PaperColumn {
     public $nonempty;
     function __construct(Conf $conf, $cj) {
         parent::__construct($conf, $cj);
-        if (($this->show_user = isset($cj->user)))
+        if (($this->show_user = isset($cj->user))) {
             $this->contact = $conf->pc_member_by_email($cj->user);
+        }
     }
     function prepare(PaperList $pl, $visible) {
         $this->contact = $this->contact ? : $pl->reviewer_user();
@@ -19,10 +20,12 @@ class ConflictMatch_PaperColumn extends PaperColumn {
     }
     function header(PaperList $pl, $is_text) {
         $t = "Potential conflict";
-        if ($this->show_user)
+        if ($this->show_user) {
             $t .= " with " . Text::name_html($this->contact);
-        if ($this->show_user && $this->contact->affiliation)
+        }
+        if ($this->show_user && $this->contact->affiliation) {
             $t .= " (" . htmlspecialchars($this->contact->affiliation) . ")";
+        }
         return $is_text ? $t : "<strong>$t</strong>";
     }
     function content_empty(PaperList $pl, PaperInfo $row) {
@@ -62,10 +65,12 @@ class ConflictMatch_PaperColumn extends PaperColumn {
         $this->nonempty = !$row->has_author($this->contact)
             && ($row->potential_conflict_callback($this->contact, [$this, "_conflict_match"])
                 || $pref[0] <= -100);
-        if (!$this->nonempty)
+        if (!$this->nonempty) {
             return "";
-        if ($pref[0] <= -100)
+        }
+        if ($pref[0] <= -100) {
             $this->_potconf["pref"][] = ["<em>reviewer preference</em>", "PC entered preference " . unparse_preference($pref)];
+        }
         $ch = [];
         $nconf = count($this->_potconf);
         foreach ($this->_potconf as &$cx) {
@@ -81,15 +86,22 @@ class ConflictMatch_PaperColumn extends PaperColumn {
                 $cx[0][0] = $n;
             }
             $cn = array_map(function ($c) { return $c[1]; }, $cx);
-            $ch[] = '<div class="potentialconflict"><p>' . $cx[0][0] . '</p><ul><li>' . join('</li><li>', $cn) . '</li></ul></div>';
+            $ch[] = '<ul class="potentialconflict break-avoid"><li>' . $cx[0][0] . '</li><li>' . join('</li><li>', $cn) . '</li></ul>';
         }
         unset($cx);
-        return join(" ", $ch);
+        if (empty($ch)) {
+            return "";
+        } else if (count($ch) === 1) {
+            return '<div class="potentialconflict-one">' . $ch[0] . '</div>';
+        } else {
+            return '<div class="potentialconflict-many">' . join("", $ch) . '</div>';
+        }
     }
 
     static function expand($name, $user, $xfj, $m) {
-        if (!($fj = (array) $user->conf->basic_paper_column("potentialconflict", $user)))
+        if (!($fj = (array) $user->conf->basic_paper_column("potentialconflict", $user))) {
             return null;
+        }
         $rs = [];
         foreach (ContactSearch::make_pc($m[1], $user)->ids as $cid) {
             $u = $user->conf->cached_user_by_id($cid);
@@ -97,8 +109,9 @@ class ConflictMatch_PaperColumn extends PaperColumn {
             $fj["user"] = $u->email;
             $rs[] = (object) $fj;
         }
-        if (empty($rs))
+        if (empty($rs)) {
             $user->conf->xt_factory_error("No PC member matches “" . htmlspecialchars($m[1]) . "”.");
+        }
         return $rs;
     }
 }
