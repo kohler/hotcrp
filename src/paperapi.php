975f3a2a9cd449318fc8ed9a34ccf5a4c8b2211d
Eddie Kohler <ekohler@gmail.com>
2020-03-13 08:50:20 -0400

    Braces.

--- src/paperapi.php
+++ src/paperapi.php
@@ -117,15 +117,18 @@ class PaperApi {
                 $votemap[$m[1][$i]] = $m[2][$i];
         $user->ksort_cid_array($votemap);
         $result = [];
-        foreach ($votemap as $k => $v)
-            if ($is_approval)
+        foreach ($votemap as $k => $v) {
+            if ($is_approval) {
                 $result[] = $user->reviewer_html_for($k);
-            else
+            } else {
                 $result[] = $user->reviewer_html_for($k) . " ($v)";
-        if (empty($result))
+            }
+        }
+        if (empty($result)) {
             json_exit(["ok" => true, "result" => ""]);
-        else
+        } else {
             json_exit(["ok" => true, "result" => '<span class="nw">' . join(',</span> <span class="nw">', $result) . '</span>']);
+        }
     }
 
     static function get_user(Contact $user, Qrequest $qreq) {
@@ -135,12 +138,13 @@ class PaperApi {
             if ($x === ""
                 || strcasecmp($x, "me") == 0
                 || ($user->contactId > 0 && $x == $user->contactId)
-                || strcasecmp($x, $user->email) == 0)
+                || strcasecmp($x, $user->email) == 0) {
                 $u = $user;
-            else if (ctype_digit($x))
+            } else if (ctype_digit($x)) {
                 $u = $user->conf->cached_user_by_id($x);
-            else
+            } else {
                 $u = $user->conf->cached_user_by_email($x);
+            }
             if (!$u) {
                 error_log("PaperApi::get_user: rejecting user {$x}, requested by {$user->email}");
                 json_exit(403, $user->isPC ? "No such user." : "Permission error.");
@@ -162,8 +166,9 @@ class PaperApi {
     static function follow_api(Contact $user, $qreq, $prow) {
         $reviewer = self::get_reviewer($user, $qreq, $prow);
         $following = friendly_boolean($qreq->following);
-        if ($following === null)
+        if ($following === null) {
             return ["ok" => false, "error" => "Bad 'following'."];
+        }
         $bits = Contact::WATCH_REVIEW_EXPLICIT | ($following ? Contact::WATCH_REVIEW : 0);
         $user->conf->qe("insert into PaperWatch set paperId=?, contactId=?, watch=? on duplicate key update watch=(watch&~?)|?",
             $prow->paperId, $reviewer->contactId, $bits,
@@ -175,17 +180,19 @@ class PaperApi {
         $result = [];
         if ($user->can_view_pc()) {
             $pcmap = $user->conf->pc_completion_map();
-            foreach ($user->conf->pc_members_and_admins() as $pc)
+            foreach ($user->conf->pc_members_and_admins() as $pc) {
                 if (!$pc->is_disabled()
                     && (!$prow || $pc->can_view_new_comment_ignore_conflict($prow))) {
                     $primary = true;
-                    foreach ($pc->completion_items() as $k => $level)
+                    foreach ($pc->completion_items() as $k => $level) {
                         if (get($pcmap, $k) === $pc) {
                             $skey = $primary ? "s" : "sm1";
                             $result[$k] = [$skey => $k, "d" => $pc->name()];
                             $primary = false;
                         }
+                    }
                 }
+            }
         }
         ksort($result);
         return ["ok" => true, "mentioncompletion" => array_values($result)];
@@ -197,8 +204,9 @@ class PaperApi {
         $need_id = false;
         if (isset($qreq->r)) {
             $rrow = $prow->full_review_of_textual_id($qreq->r);
-            if ($rrow === false)
+            if ($rrow === false) {
                 return new JsonResult(400, "Bad request.");
+            }
             $rrows = $rrow ? [$rrow] : [];
         } else if (isset($qreq->u)) {
             $need_id = true;
@@ -206,8 +214,9 @@ class PaperApi {
             $rrows = $prow->full_reviews_of_user($u);
             if (!$rrows
                 && $user->contactId !== $u->contactId
-                && !$user->can_view_review_identity($prow, null))
+                && !$user->can_view_review_identity($prow, null)) {
                 return new JsonResult(403, "Permission error.");
+            }
         } else {
             $prow->ensure_full_reviews();
             $rrows = $prow->viewable_submitted_reviews_by_display($user);
@@ -220,56 +229,65 @@ class PaperApi {
                 $vrrows[] = $rf->unparse_review_json($user, $prow, $rrow);
             }
         }
-        if (!$vrrows && $rrows)
+        if (!$vrrows && $rrows) {
             return new JsonResult(403, "Permission error.");
-        else
+        } else {
             return new JsonResult(["ok" => true, "reviews" => $vrrows]);
+        }
     }
 
     static function reviewrating_api(Contact $user, Qrequest $qreq, PaperInfo $prow) {
         if (!$qreq->r
-            || ($rrow = $prow->full_review_of_textual_id($qreq->r)) === false)
+            || ($rrow = $prow->full_review_of_textual_id($qreq->r)) === false) {
             return new JsonResult(400, "Bad request.");
-        else if (!$user->can_view_review($prow, $rrow))
+        } else if (!$user->can_view_review($prow, $rrow)) {
             return new JsonResult(403, "Permission error.");
-        else if (!$rrow)
+        } else if (!$rrow) {
             return new JsonResult(404, "No such review.");
+        }
         $editable = $user->can_rate_review($prow, $rrow);
         if ($qreq->method() !== "GET") {
             if (!isset($qreq->user_rating)
-                || ($rating = ReviewInfo::parse_rating($qreq->user_rating)) === false)
+                || ($rating = ReviewInfo::parse_rating($qreq->user_rating)) === false) {
                 return new JsonResult(400, "Bad request.");
-            else if (!$editable)
+            } else if (!$editable) {
                 return new JsonResult(403, "Permission error.");
-            if ($rating === null)
+            }
+            if ($rating === null) {
                 $user->conf->qe("delete from ReviewRating where paperId=? and reviewId=? and contactId=?", $prow->paperId, $rrow->reviewId, $user->contactId);
-            else
+            } else {
                 $user->conf->qe("insert into ReviewRating set paperId=?, reviewId=?, contactId=?, rating=? on duplicate key update rating=values(rating)", $prow->paperId, $rrow->reviewId, $user->contactId, $rating);
+            }
             $rrow = $prow->fresh_review_of_id($rrow->reviewId);
         }
         $rating = $rrow->rating_of_user($user);
         $jr = new JsonResult(["ok" => true, "user_rating" => $rating]);
-        if ($editable)
+        if ($editable) {
             $jr->content["editable"] = true;
-        if ($user->can_view_review_ratings($prow, $rrow))
+        }
+        if ($user->can_view_review_ratings($prow, $rrow)) {
             $jr->content["ratings"] = array_values($rrow->ratings());
+        }
         return $jr;
     }
 
     static function reviewround_api(Contact $user, $qreq, $prow) {
         if (!$qreq->r
-            || ($rrow = $prow->full_review_of_textual_id($qreq->r)) === false)
+            || ($rrow = $prow->full_review_of_textual_id($qreq->r)) === false) {
             return new JsonResult(400, "Bad request.");
-        else if (!$user->can_administer($prow))
+        } else if (!$user->can_administer($prow)) {
             return new JsonResult(403, "Permission error.");
-        else if (!$rrow)
+        } else if (!$rrow) {
             return new JsonResult(404, "No such review.");
-        $rname = trim((string) $qreq->round);
-        $round = $user->conf->sanitize_round_name($rname);
-        if ($round === false)
-            return ["ok" => false, "error" => Conf::round_name_error($rname)];
-        $rnum = (int) $user->conf->round_number($round, true);
-        $user->conf->qe("update PaperReview set reviewRound=? where paperId=? and reviewId=?", $rnum, $prow->paperId, $rrow->reviewId);
-        return ["ok" => true];
+        } else {
+            $rname = trim((string) $qreq->round);
+            $round = $user->conf->sanitize_round_name($rname);
+            if ($round === false) {
+                return ["ok" => false, "error" => Conf::round_name_error($rname)];
+            }
+            $rnum = (int) $user->conf->round_number($round, true);
+            $user->conf->qe("update PaperReview set reviewRound=? where paperId=? and reviewId=?", $rnum, $prow->paperId, $rrow->reviewId);
+            return ["ok" => true];
+        }
     }
 }
