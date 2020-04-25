kohler/hotcrp

commit 5ab21ca2bd9205ef351f79fd781cdaacfe9681f5

HTML/CSS coding.
* Use h2/h3 elements for header-like things.
* Use some semantic HTML elements.

scripts/script.js
@@ -3637,7 +3637,7 @@ return function (id, name, elt) {
             observer && observer.unobserve($$(id));
             $(pslcard).find("a[href='#" + id + "']").remove();
         } else {
-            var $psli = $('<div class="pslitem ui js-click-child"><a href="#' + id + '" class="x hover-child">' + name + '</a></div>');
+            var $psli = $('<li class="pslitem ui js-click-child"><a href="#' + id + '" class="x hover-child">' + name + '</a></li>');
             $psli.appendTo(pslcard);
             elt = elt || $$(id);
             linkmap && linkmap.set(elt, $psli[0]);
@@ -3714,7 +3714,7 @@ tooltip.add_builder("rf-description", function (info) {
                 d += "<div class=\"od\">Choices are:</div>";
                 for (si = 0, vo = fieldj.score_info.value_order();
                      si < vo.length; ++si)
-                    d += "<div class=\"od\"><span class=\"rev_num " + fieldj.score_info.className(vo[si]) + "\">" + fieldj.score_info.unparse(vo[si]) + ".</span>&nbsp;" + escape_entities(fieldj.options[vo[si] - 1]) + "</div>";
+                    d += "<div class=\"od\"><strong class=\"rev_num " + fieldj.score_info.className(vo[si]) + "\">" + fieldj.score_info.unparse(vo[si]) + ".</strong>&nbsp;" + escape_entities(fieldj.options[vo[si] - 1]) + "</div>";
             }
             info = $.extend({content: d, dir: "l"}, info);
         }
@@ -3734,44 +3734,45 @@ function render_review_body(rrow) {
         else
             return !!rrow[f.uid];
     });
-    var t = "", i, f, x, nextf, last_display = 0, display;
+    var t = "", i, f, k, x, nextf, last_display = 0, display;
     for (i = 0; i != view_order.length; ++i) {
         f = view_order[i];
         nextf = view_order[i + 1];
         if (last_display != 1 && f.options && nextf && nextf.options) {
             display = 1;
-            t += '<div class="rvg">';
         } else {
             display = last_display == 1 ? 2 : 0;
         }
 
         t += '<div class="rv rv' + "glr".charAt(display) + '" data-rf="' + f.uid +
-            '"><div class="revvt"><div class="revfn">' + f.name_html;
+            '"><div class="revvt"><h3 class="revfn">' + f.name_html;
         x = f.visibility;
         if (x == "audec" && hotcrp_status && hotcrp_status.myperm
-            && hotcrp_status.myperm.some_author_can_view_decision)
+            && hotcrp_status.myperm.some_author_can_view_decision) {
             x = "au";
+        }
         if (x != "au") {
             t += '<div class="revvis">(' +
                 (({secret: "secret", admin: "shown only to chairs",
                    pc: "hidden from authors", audec: "hidden from authors until decision"})[x] || x) +
                 ')</div>';
         }
-        t += '</div></div><div class="revv revv' + "glr".charAt(display);
+        t += '</h3></div>';
+        k = "revv revv" + "glr".charAt(display);
 
         if (!f.options) {
             x = render_text(rrow.format, rrow[f.uid], f.uid);
-            t += ' revtext format' + (x.format || 0) + '">' + x.content;
+            t += '<div class="' + k + ' revtext format' + (x.format || 0) + '">'
+                + x.content + '</div>';
         } else if (rrow[f.uid] && (x = f.score_info.parse(rrow[f.uid]))) {
-            t += '"><table><tr><td class="nw">' + f.score_info.unparse_revnum(x) +
-                "&nbsp;</td><td>" + escape_entities(f.options[x - 1]) + "</td></tr></table>";
+            t += '<p class="' + k + ' revscore"><span class="revscorenum">' +
+                f.score_info.unparse_revnum(x) + ' </span><span class="revscoredesc">' +
+                escape_entities(f.options[x - 1]) + '</span></p>';
         } else {
-            t += ' rev_unknown">' + (f.allow_empty ? "No entry" : "Unknown");
+            t += '<p class="' + k + ' rev_unknown">' + (f.allow_empty ? "No entry" : "Unknown") + '</p>';
         }
 
-        t += '</div></div>';
-        if (display == 2)
-            t += '</div>';
+        t += '</div>';
         last_display = display;
     }
     return t;
@@ -3917,13 +3918,13 @@ function add_review(rrow) {
         has_user_rating = false, i, ratekey, selected;
 
     i = rrow.ordinal ? '" data-review-ordinal="' + rrow.ordinal : '';
-    hc.push('<div class="pcard revcard has-fold '
+    hc.push('<article id="r' + rid + '" class="pcard revcard has-fold '
             + (rrow.folded ? "fold20c" : "fold20o")
-            + '" id="r' + rid + '" data-pid="' + rrow.pid
-            + '" data-rid="' + rrow.rid + i + '">', '</div>');
+            + '" data-pid="' + rrow.pid
+            + '" data-rid="' + rrow.rid + i + '">', '</article>');
 
     // HEADER
-    hc.push('<div class="revcard-head">', '</div>');
+    hc.push('<header class="revcard-head">', '</header>');
 
     // review description
     var rdesc = rrow.subreview ? "Subreview" : "Review";
@@ -3936,9 +3937,9 @@ function add_review(rrow) {
 
     // edit/text links
     if (rrow.folded) {
-        hc.push('<h3><a class="ui js-foldup nn" href="" data-fold-target="20"><span class="expander"><span class="in0 fx20"><svg class="licon" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="M1 1L8 15L15 1z" /></svg></span><span class="in1 fn20"><svg class="licon" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="M1 1L15 8L1 15z" /></svg></span></span>', '</a></h3>');
+        hc.push('<h2><a class="ui js-foldup nn" href="" data-fold-target="20"><span class="expander"><span class="in0 fx20"><svg class="licon" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="M1 1L8 15L15 1z" /></svg></span><span class="in1 fn20"><svg class="licon" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="M1 1L15 8L1 15z" /></svg></span></span>', '</a></h2>');
     } else {
-        hc.push('<h3><a class="nn" href="' + hoturl_html("review", rlink) + '">', '</a></h3>');
+        hc.push('<h2><a class="nn" href="' + hoturl_html("review", rlink) + '">', '</a></h2>');
     }
     hc.push('<span class="revcard-header-name">' + rdesc + '</span>');
     if (rrow.editable && rrow.folded) {
@@ -3949,7 +3950,7 @@ function add_review(rrow) {
     hc.pop();
 
     // author info
-    var revname, revtime;
+    var revname = "", revtime;
     if (rrow.review_token) {
         revname = 'Review token ' + rrow.review_token;
     } else if (rrow.reviewer) {
@@ -3958,8 +3959,6 @@ function add_review(rrow) {
             revname = '[' + revname + ']';
         if (rrow.reviewer_email)
             revname = '<span title="' + rrow.reviewer_email + '">' + revname + '</span>';
-    } else {
-        revname = "";
     }
     if (rrow.rtype) {
         revname += (revname ? " " : "") + '<span class="rto rt' + rrow.rtype +
@@ -3971,14 +3970,14 @@ function add_review(rrow) {
             revname += ' <span class="revround" title="Review round">' + escape_entities(rrow.round) + '</span>';
     }
     if (rrow.modified_at) {
-        revtime = rrow.modified_at_text;
+        revtime = '<time class="revtime" datetime="' + (new Date(rrow.modified_at * 1000)).toISOString() + '">' + rrow.modified_at_text + '</time>';
     }
     if (revname || revtime) {
         hc.push('<div class="revthead">');
         if (revname)
-            hc.push('<div class="revname">' + revname + '</div>');
+            hc.push('<address class="revname" itemprop="author">' + revname + '</address>');
         if (revtime)
-            hc.push('<div class="revtime">' + revtime + '</div>');
+            hc.push(revtime);
         hc.push('</div>');
     }
 
@@ -3987,7 +3986,7 @@ function add_review(rrow) {
     hc.push_pop('<hr class="c">');
 
     // body
-    hc.push('<div class="revcard-body fx20">', '</div>');
+    hc.push('<div class="revcard-body revcard-render fx20">', '</div>');
     hc.push_pop(render_review_body(rrow));
 
     // ratings
@@ -4082,10 +4081,10 @@ function comment_identity_time(cj) {
                + cj.ordinal + '</span></a></div>');
     }
     if (cj.author && cj.author_hidden) {
-        t.push('<div class="cmtname fold9c"><span class="fx9' +
+        t.push('<address class="cmtname fold9c" itemprop="author"><span class="fx9' +
                (cj.author_email ? '" title="' + cj.author_email : '') +
                '">' + cj.author + ' </span><a class="ui qq js-foldup" href="" data-fold-target="9" title="Toggle author"><span class="fn9"><span class="expander"><svg class="licon" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="M1 1L15 8L1 15z" /></svg></span>' +
-               (cj.author_pseudonym || "<i>Hidden</i>") + '</span><span class="fx9">(deblinded)</span></a></div>');
+               (cj.author_pseudonym || "<i>Hidden</i>") + '</span><span class="fx9">(deblinded)</span></a></address>');
     } else if (cj.author) {
         x = cj.author;
         if (cj.blind && cj.visibility === "au") {
@@ -4094,15 +4093,15 @@ function comment_identity_time(cj) {
         if (cj.author_pseudonym) {
             x = cj.author_pseudonym + ' ' + x;
         }
-        t.push('<div class="cmtname' +
+        t.push('<address class="cmtname' +
                (cj.author_email ? '" title="' + cj.author_email : "") +
-               '">' + x + '</div>');
+               '" itemprop="author">' + x + '</address>');
     } else if (cj.author_pseudonym
                && (!cj.response || cj.author_pseudonym !== "Author")) {
-        t.push('<div class="cmtname">' + cj.author_pseudonym + '</div>');
+        t.push('<address class="cmtname" itemprop="author">' + cj.author_pseudonym + '</address>');
     }
     if (cj.modified_at) {
-        t.push('<div class="cmttime">' + cj.modified_at_text + '</div>');
+        t.push('<time class="cmttime" datetime="' + (new Date(cj.modified_at * 1000)).toISOString() + '">' + cj.modified_at_text + '</time>');
     }
     if (!cj.response && cj.tags) {
         x = [];
@@ -4559,16 +4558,16 @@ function render_cmt($c, cj, editing, msg) {
     // header
     t = cj.is_new ? '>' : ' id="cid' + cj.cid + '">';
     if (cj.editable) {
-        hc.push('<div class="cmtt ui js-click-child"' + t, '</div>');
+        hc.push('<header class="cmtt ui js-click-child"' + t, '</header>');
     } else {
-        hc.push('<div class="cmtt"' + t, '</div>');
+        hc.push('<header class="cmtt"' + t, '</header>');
     }
     if (cj.is_new && !cj.response) {
         hc.push('<div class="cmtnumid"><div class="cmtnum">New Comment</div></div>');
     } else if (cj.editable && !editing && cj.response) {
-        var $h3 = $(chead).find("h3");
-        if (!$h3.find("a").length) {
-            $h3.html('<a href="" class="nn ui cmteditor">' + $h3.html() + ' <span class="t-editor">✎</span></a>');
+        var $h2 = $(chead).find("h2");
+        if (!$h2.find("a").length) {
+            $h2.html('<a href="" class="nn ui cmteditor">' + $h2.html() + ' <span class="t-editor">✎</span></a>');
         }
     }
     t = comment_identity_time(cj);
@@ -4676,7 +4675,7 @@ function add(cj, editing) {
         var $c = $(".pcontainer").children().last();
         if (cj.is_new && !editing) {
             if (!$c.hasClass("cmtcard") || $c[0].id !== "ccactions") {
-                $c = $('<div class="pcard cmtcard" id="ccactions"><div class="cmtcard-body"><div class="aab aabig"></div></div></div>').appendTo(".pcontainer");
+                $c = $('<div id="ccactions" class="pcard cmtcard"><div class="cmtcard-body"><div class="aab aabig"></div></div></div>').appendTo(".pcontainer");
             }
             if (!$c.find("a[href='#" + cid + "']").length) {
                 t = '<div class="aabut"><a href="#' + cid + '" class="btn ui js-edit-comment">Add ';
@@ -4693,23 +4692,25 @@ function add(cj, editing) {
             $c = $c.prev();
         }
 
-        var iddiv = '<div id="' + cid + '" class="cmtid' + (cj.editable ? " editable" : "");
+        var idattr = ' id="' + cid + '" class="cmtid' + (cj.editable ? " editable" : "");
         if (!$c.hasClass("cmtcard")
             || cj.response
             || $c.hasClass("response")) {
-            var t;
+            var t, tx;
             if (cj.response) {
-                t = iddiv + ' response pcard cmtcard">';
+                t = '<article' + idattr + ' response pcard cmtcard">';
                 if (cj.text !== false) {
                     cdesc = (cj.response == "1" ? "" : cj.response + " ") + "Response";
-                    t += '<div class="cmtcard-head"><h3><span class="cmtcard-header-name">' +
-                        cdesc + '</span></h3></div>';
+                    t += '<header class="cmtcard-head"><h2><span class="cmtcard-header-name">' +
+                        cdesc + '</span></h2></header>';
                 }
+                tx = '</article>';
             } else {
-                t = '<div class="pcard cmtcard" id="cc' + cid + '">';
+                t = '<div id="cc' + cid + '" class="pcard cmtcard">';
                 cdesc = "Comment";
+                tx = '</div>';
             }
-            $c = $(t + '<div class="cmtcard-body"></div></div>').insertAfter($c);
+            $c = $(t + '<div class="cmtcard-body"></div>' + tx).insertAfter($c);
             if (cdesc) {
                 add_pslitem(cj.response ? cid : "cc" + cid, cdesc);
             }
@@ -4721,7 +4722,7 @@ function add(cj, editing) {
         if (cj.response) {
             j = $('<div class="cmtg"></div>');
         } else {
-            j = $(iddiv + ' cmtg"></div>');
+            j = $('<article' + idattr + ' cmtg"></article>');
         }
         j.appendTo($c.find(".cmtcard-body"));
     }
@@ -5352,8 +5353,8 @@ function suggest() {
         if (!hintdiv) {
             hintdiv = make_bubble({dir: "nw", color: "suggest"});
             hintdiv.self().on("mousedown", function (evt) { evt.preventDefault(); })
-                .on("click", "div.suggestion", click)
-                .on("mousemove", "div.suggestion", hover);
+                .on("click", ".suggestion", click)
+                .on("mousemove", ".suggestion", hover);
         }
 
         var i, clist = cinfo.list, same_list = false;
@@ -8940,8 +8941,8 @@ function make_info(n, c, sv) {
         },
         unparse_revnum: function (val) {
             if (val >= 1 && val <= n)
-                return '<span class="rev_num sv ' + sv + fm9(val) + '">' +
-                    unparse(val) + '.</span>';
+                return '<strong class="rev_num sv ' + sv + fm9(val) + '">' +
+                    unparse(val) + '.</strong>';
             else
                 return '(???)';
         },

stylesheets/mobile.css
@@ -154,11 +154,8 @@ table.pltable {
 }
 .rvl, .rvr {
 	width: 100%;
+	padding-left: 0;
+	padding-right: 0;
 	float: none;
-}
-.rvr {
-	margin-top: 8px;
-	border-top: 1px solid #ffecef;
-	padding: 8px 0 0;
 }
 }

stylesheets/style.css
@@ -63,6 +63,9 @@ ul.x > li {
 ul.x > li:last-child {
 	margin-bottom: 0;
 }
+address {
+	font-style: inherit;
+}
 
 
 /* page background */
@@ -164,11 +167,11 @@ h1.paptitle {
 }
 #header-page.header-page-submission {
 	float: none;
-	padding: 14px 0 0;
+	padding: 0;
 	position: relative;
+	top: 14px;
+	margin-bottom: 14px;
 	z-index: 1;
-}
-#header-page-submission-inner {
 	background: white;
 	border-top: 1px solid #f33360; /*#b2a584;*/
 	border-bottom: 1px solid #f33360; /*#b2a584;*/
@@ -362,40 +365,40 @@ label.checki {
 	margin-bottom: 0 !important;
 }
 .mb-3 {
-	margin-bottom: 1em;
-	margin-bottom: 1rem;
+	margin-bottom: 1em !important;
+	margin-bottom: 1rem !important;
 }
 .mb-5 {
-	margin-bottom: 2.25em;
-	margin-bottom: 2.25rem;
+	margin-bottom: 2.25em !important;
+	margin-bottom: 2.25rem !important;
 }
 .mt-2 {
-	margin-top: 0.5em;
-	margin-top: 0.5rem;
+	margin-top: 0.5em !important;
+	margin-top: 0.5rem !important;
 }
 .mt-3 {
-	margin-top: 1em;
-	margin-top: 1rem;
+	margin-top: 1em !important;
+	margin-top: 1rem !important;
 }
 .mt-4 {
-	margin-top: 1.5em;
-	margin-top: 1.5rem;
+	margin-top: 1.5em !important;
+	margin-top: 1.5rem !important;
 }
 .mt-5 {
-	margin-top: 2.25em;
-	margin-top: 2.25rem;
+	margin-top: 2.25em !important;
+	margin-top: 2.25rem !important;
 }
 .mt-7 {
 	margin-top: 5em !important;
 	margin-top: 5rem !important;
 }
 .ml-2 {
-	margin-left: 0.5em;
-	margin-left: 0.5rem;
+	margin-left: 0.5em !important;
+	margin-left: 0.5rem !important;
 }
 .ml-3 {
-	margin-left: 1em;
-	margin-left: 1rem;
+	margin-left: 1em !important;
+	margin-left: 1rem !important;
 }
 
 .mb-0-last-child > *:last-child {
@@ -427,7 +430,8 @@ td.g {
 	clear: both;
 }
 .clearfix:after, .pst:after, .pavt:after, .papet:after,
-.revvt:after, .revet:after, .rvg:after, .cmteditinfo:after, .aa:after, .aab:after,
+.revvt:after, .revet:after, .rvl:before, .rvg:before,
+.cmteditinfo:after, .aa:after, .aab:after,
 .entryi:after, .popup-actions:after, .modal-content:after, .f-horizontal:after {
 	clear: both;
 	content: "";
@@ -1473,6 +1477,7 @@ td.padls, th.padls, div.padls {
 .has-error > label,
 .has-error > .settings-itemheading,
 label.has-error,
+h2.has-error,
 h3.has-error,
 .is-error {
 	color: #ee0000;
@@ -1495,6 +1500,7 @@ select.has-error {
 .has-warning > label,
 .has-warning > .settings-itemheading,
 label.has-warning,
+h2.has-warning,
 h3.has-warning,
 .is-warning {
 	color: #c07700;
@@ -1900,7 +1906,7 @@ td.rl {
 tr.reviewers-highlight {
 	background: #f0f0ff;
 }
-th.revscore, th.rl {
+th.rlscore, th.rl {
 	font-size: smaller;
 	padding: 0 0.25em;
 	color: #666666;
@@ -1915,11 +1921,11 @@ span.revround {
 	font-weight: normal;
 	font-variant-numeric: tabular-nums;
 }
-td.revscore {
+td.rlscore {
 	padding-right: 0;
 	text-align: center;
 }
-.rldraft > td.revscore {
+.rldraft > td.rlscore {
 	opacity: 0.5;
 }
 .revlink {
@@ -2565,7 +2571,7 @@ a.badge:active {
 
 /* paper mode tabs */
 .papmode {
-	float: left;
+	display: inline-block;
 	text-align: center;
 	font-size: smaller;
 	padding: 4.5px 5px 4px 5px;
@@ -2600,6 +2606,11 @@ a.badge:active {
 	padding: 12px 0 0 24px;
 	z-index: 5;
 }
+.submission-modes > ul {
+	list-style-type: none;
+	margin: 0;
+	padding: 0;
+}
 
 /* paper strip */
 .pcard-left {
@@ -2630,6 +2641,8 @@ a.badge:active {
 	position: sticky;
 	top: 84px;
 	padding: 20px 16px 20px 24px;
+	margin: 0;
+	list-style-type: none;
 }
 .pslcard-home {
 	position: absolute;
@@ -2941,6 +2954,11 @@ a.badge:active {
 .papfn, .pavfn, .psfn, .revfn {
 	font-weight: bold;
 }
+h3.papfn, h3.pavfn, h3.psfn, h3.revfn {
+	font-size: inherit;
+	display: inline;
+	margin: 0;
+}
 .pavfnsp:after {
 	content: " ";
 }
@@ -2948,6 +2966,7 @@ a.badge:active {
 	float: left;
 }
 .papet {
+	font-size: inherit;
 	color: #313168;
 	margin-bottom: 0.25rem;
 }
@@ -3009,10 +3028,9 @@ a.badge:active {
 	background: #ffffff;
 	padding: 16px 0;
 }
-.papcard-sep {
-	margin: 48px -24px 16px -24px;
-	height: 5px;
-	background-color: #fad5d5;
+.papcard h2, .notecard h2 {
+	font-size: 1.17em;
+	color: #d41743;
 }
 .pedcard-head {
 	padding: 3px 16px 4px;
@@ -3020,8 +3038,9 @@ a.badge:active {
 
 /* paper notes */
 .notecard {
+	border-top: 5px solid #fad5d5;
 	margin-top: 16px;
-	margin-bottom: 48px;
+	margin-bottom: 24px;
 	position: relative;
 }
 
@@ -3035,11 +3054,13 @@ a.badge:active {
 	margin: 16px 7em;
 }
 .revcard-head {
-	margin-top: 12px;
+	margin-top: 32px;
+	margin-top: 2rem;
 }
-.revcard-head h3 {
+.revcard-head h2 {
 	display: inline-block;
 	margin-right: 1.5em;
+	font-size: 1.17em;
 	color: #d41743;
 }
 .revcard-header-name {
@@ -3108,6 +3129,15 @@ a:hover .t-editor,
 	float: none;
 	padding: 0 1em 5px 0;
 }
+.revscore {
+	display: flex;
+	align-items: baseline;
+	margin-bottom: 0;
+}
+.revscorenum {
+	display: block;
+	white-space: pre;
+}
 .papev, .revev {
 	margin: 5px 0;
 }
@@ -3121,13 +3151,22 @@ a:hover .t-editor,
 .paphint > p, .revhint > p, .cmthint > p {
 	margin-bottom: 0.5em;
 }
+.rvg, .rvl, .rvr {
+	margin-bottom: 1.25rem;
+}
 .rvl, .rvr {
 	width: 50%;
 	max-width: 360px;
 	float: left;
 }
-.revvl {
-	padding-right: 1.5em;
+.rvl {
+	padding-right: 0.75rem;
+}
+.rvr {
+	padding-left: 0.75rem;
+}
+.revcard-render {
+	margin-bottom: -1.25rem;
 }
 .papeg, .papegroup {
 	margin-bottom: 2.5rem;
@@ -3155,12 +3194,6 @@ a:hover .t-editor,
 	background: #eae8f2;
 	background: linear-gradient(to bottom, rgba(234,232,242,0), #eae8ee 4px, #eae8ee 51%, transparent 51%),
 		linear-gradient(to top, rgba(234,232,242,0), #eae8ee 4px, #eae8ee 51%, transparent 51%);
-}
-.rvg {
-	margin-bottom: 1.25rem;
-}
-.rvg:last-child {
-	margin-bottom: 0;
 }
 .eauname, .eauaff, .eauemail {
 	margin: 2px 0.5em 2px 0;
@@ -3255,12 +3288,14 @@ a:hover .t-editor,
 	padding-top: 16px;
 }
 .cmtcard-head {
-	padding-top: 12px;
+	padding-top: 32px;
+	padding-top: 2rem;
 	padding-bottom: 4px;
 }
-.cmtcard h3 {
+.cmtcard-head h2 {
 	display: inline-block;
 	margin-right: 1.5em;
+	font-size: 1.17em;
 	color: #c77634;
 }
 .cmtcard-header-name {

assign.php
@@ -218,7 +218,9 @@ $paperTable->paptabBegin();
 // reviewer information
 $t = review_table($Me, $prow, $paperTable->all_reviews(), null, "assign");
 if ($t !== "") {
-    echo '<hr class="papcard-sep"><h3>Reviews</h3>', $t;
+    echo '<div class="pcard revcard">',
+        '<div class="revcard-head"><h2>Reviews</h2></div>',
+        '<div class="revpcard-body">', $t, '</div></div>';
 }
 
 
@@ -257,7 +259,9 @@ usort($requests, function ($a, $b) {
 });
 
 if ($requests) {
-    echo '<hr class="papcard-sep"><h3>Review requests</h3><div class="ctable-wide">';
+    echo '<div class="pcard revcard">',
+        '<div class="revcard-head"><h2>Review requests</h2></div>',
+        '<div class="revcard-body"><div class="ctable-wide">';
 }
 foreach ($requests as $req) {
     echo '<div class="ctelt"><div class="ctelti has-fold';
@@ -436,7 +440,7 @@ foreach ($requests as $req) {
     echo '</div></div>';
 }
 if ($requests) {
-    echo '</div>';
+    echo '</div></div></div>';
 }
 
 
@@ -456,9 +460,10 @@ if ($Me->can_administer($prow)) {
     }
 
     // PC conflicts row
-    echo '<hr class="papcard-sep"><h3>PC assignments</h3>',
-        Ht::form(hoturl_post("assign", "p=$prow->paperId"), array("id" => "ass", "class" => "need-unload-protection")),
-        '<p>';
+    echo '<div class="pcard revcard">',
+        '<div class="revcard-head"><h2>PC assignments</h2></div>',
+        '<div class="revcard-body">',
+        Ht::form(hoturl_post("assign", "p=$prow->paperId"), array("id" => "ass", "class" => "need-unload-protection"));
     Ht::stash_script('hiliter_children("#ass")');
 
     if ($Conf->has_topics()) {
@@ -556,11 +561,9 @@ if ($Me->can_administer($prow)) {
         '<div class="aabut">', Ht::submit("update", "Save assignments", ["class" => "btn-primary"]), '</div>',
         '<div class="aabut">', Ht::submit("cancel", "Cancel"), '</div>',
         '<div id="assresult" class="aabut"></div>',
-        '</div></form>';
+        '</div></form></div></div>';
 }
 
-
-echo "</div></div>\n";
 
 // add external reviewers
 $req = "Request an external review";
@@ -570,7 +573,7 @@ if (!$Me->allow_administer($prow) && $Conf->setting("extrev_chairreq")) {
 echo '<div class="pcard revcard">',
     Ht::form(hoturl_post("assign", "p=$prow->paperId"), ["novalidate" => true]),
     '<div class="revcard-head">',
-    "<h3>", $req, "</h3></div><div class=\"revcard-body\">";
+    "<h2>", $req, "</h2></div><div class=\"revcard-body\">";
 
 echo '<p class="papertext">', $Conf->_i("external-review-request-description", null);
 if ($Me->allow_administer($prow)) {
@@ -627,6 +630,6 @@ echo '<div class="aab aabr">',
     '<div class="aabut"><a class="ui x js-request-review-preview-email" href="">Preview request email</a></div>',
     "</div>\n\n";
 
-echo "</div></div></form></div></div>\n";
+echo "</div></div></form></div></article>\n";
 
 $Conf->footer();

paper.php
@@ -530,5 +530,5 @@ if ($paperTable->mode === "edit") {
     }
 }
 
-echo "</div>\n";
+echo "</article>\n";
 $Conf->footer();

review.php
@@ -327,5 +327,5 @@ if (!$viewAny
     }
 }
 
-echo "</div>\n";
+echo "</article>\n";
 $Conf->footer();

commit 9287799983a3f48f76ba1403953b275e52f9f650

Fix review rendering.
Broken in 5ab21ca2bd9205ef351f79fd781cdaacfe9681f5.
Fix using display: flex.

scripts/script.js
@@ -3775,7 +3775,7 @@ function render_review_body(rrow) {
                 f.score_info.unparse_revnum(x) + ' </span><span class="revscoredesc">' +
                 escape_entities(f.options[x - 1]) + '</span></p>';
         } else {
-            t += '<p class="' + k + ' rev_unknown">' + (f.allow_empty ? "No entry" : "Unknown") + '</p>';
+            t += '<p class="' + k + ' revscore">' + (f.allow_empty ? "No entry" : "Unknown") + '</p>';
         }
 
         t += '</div>';

stylesheets/mobile.css
@@ -156,6 +156,5 @@ table.pltable {
 	width: 100%;
 	padding-left: 0;
 	padding-right: 0;
-	float: none;
 }
 }

stylesheets/style.css
@@ -443,7 +443,7 @@ td.g {
 	clear: both;
 }
 .clearfix:after, .pst:after, .pavt:after, .papet:after,
-.revvt:after, .revet:after, .rvl:before, .rvg:before,
+.revvt:after, .revet:after,
 .cmteditinfo:after, .aa:after, .aab:after,
 .entryi:after, .popup-actions:after, .modal-content:after, .f-horizontal:after {
 	clear: both;
@@ -1640,7 +1640,7 @@ div.formatdescription {
 }
 
 .revrating {
-	margin-top: 12px;
+	margin-bottom: 1.25rem;
 	font-size: 0.9rem;
 	color: #df4c4c;
 }
@@ -3116,6 +3116,8 @@ h3.papfn, h3.pavfn, h3.psfn, h3.revfn {
 .revcard-head {
 	margin-top: 32px;
 	margin-top: 2rem;
+	margin-bottom: 12px;
+	margin-bottom: 0.75rem;
 }
 .revcard-head h2 {
 	display: inline-block;
@@ -3126,11 +3128,6 @@ h3.papfn, h3.pavfn, h3.psfn, h3.revfn {
 .revcard-header-name {
 	display: inline-block;
 	border-bottom: 5px solid #df4c4c;
-}
-.revcard-body {
-	background: #ffffff;
-	padding: 12px 0;
-	border-radius: 2px;
 }
 .t-editor {
 	display: inline-block;
@@ -3212,22 +3209,22 @@ a:hover .t-editor,
 .field-d > p {
 	margin-bottom: 0.5em;
 }
+.revcard-render {
+	display: flex;
+	flex-wrap: wrap;
+}
 .rvg, .rvl, .rvr {
 	margin-bottom: 1.25rem;
 }
 .rvl, .rvr {
 	width: 50%;
 	max-width: 360px;
-	float: left;
 }
 .rvl {
 	padding-right: 0.75rem;
 }
 .rvr {
 	padding-left: 0.75rem;
-}
-.revcard-render {
-	margin-bottom: -1.25rem;
 }
 .papeg, .papegroup {
 	margin-bottom: 2.5rem;