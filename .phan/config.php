c8b444157da6b8548b40766bc83cf7eab0224274
Eddie Kohler <ekohler@gmail.com>
2020-05-05 10:58:35 -0400

    Add initial configuration for phan, a PHP type checker.

---
+++ .phan/config.php
@@ -0,0 +1,58 @@
+<?php
+
+use Phan\Config;
+
+/**
+ * This configuration will be read and overlaid on top of the
+ * default configuration. Command line arguments will be applied
+ * after this file is read.
+ *
+ * @see src/Phan/Config.php
+ * See Config for all configurable options.
+ *
+ * A Note About Paths
+ * ==================
+ *
+ * Files referenced from this file should be defined as
+ *
+ * ```
+ *   Config::projectPath('relative_path/to/file')
+ * ```
+ *
+ * where the relative path is relative to the root of the
+ * project which is defined as either the working directory
+ * of the phan executable or a path passed in via the CLI
+ * '-d' flag.
+ */
+return [
+    // If true, missing properties will be created when
+    // they are first seen. If false, we'll report an
+    // error message.
+    "allow_missing_properties" => true,
+
+    // Allow null to be cast as any type and for any
+    // type to be cast to null.
+    "null_casts_as_any_type" => true,
+
+    // Backwards Compatibility Checking
+    "backward_compatibility_checks" => false,
+
+    // Run a quick version of checks that takes less
+    // time
+    "quick_mode" => true,
+
+    // Only emit critical issues to start with
+    // (0 is low severity, 5 is normal severity, 10 is critical)
+    "minimum_severity" => 10,
+
+    // A list of directories that should be parsed for class and
+    // method information. After excluding the directories
+    // defined in exclude_analysis_directory_list, the remaining
+    // files will be statically analyzed for errors.
+    //
+    // Thus, both first-party and third-party code being used by
+    // your application should be included in this list.
+    'directory_list' => [
+        "lib", "src"
+    ]
+];
