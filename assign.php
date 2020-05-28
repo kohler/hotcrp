48117de9f701bf41d12b61c4104e91bae8208dfb
Eddie Kohler <ekohler@gmail.com>
2020-05-28 16:51:41 -0400

    Zero-time requests are old.

--- assign.php
+++ assign.php
@@ -247,11 +247,7 @@ usort($requests, function ($a, $b) {
     if ($a[0] !== $b[0]) {
         return $a[0] - $b[0];
     } else if ($a[1] !== $b[1]) {
-        if ($a[1] === 0 || $b[1] === 0) {
-            return $a[1] === 0 ? 1 : -1;
-        } else {
-            return $a[1] < $b[1] ? -1 : 1;
-        }
+        return $a[1] < $b[1] ? -1 : 1;
     } else {
         return $a[2] - $b[2];
     }
