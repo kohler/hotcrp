ef04bbdf6c79ee13e3d72bc7dd6cfc42e39f3204
Eddie Kohler <ekohler@gmail.com>
2020-08-10 22:24:52 -0400

    PaperSaver simplified.

--- src/papersaver.php
+++ src/papersaver.php
@@ -3,103 +3,38 @@
 // Copyright (c) 2008-2020 Eddie Kohler; see LICENSE.
 
 class PaperSaver {
-    static private $list = [];
-
-    static function register($prio, PaperSaver $saver) {
-        self::$list[] = [$prio, count(self::$list), $saver];
-        usort(self::$list, function ($a, $b) {
-            if ($a[0] != $b[0]) {
-                return $a[0] - $b[0];
-            } else {
-                return $a[1] - $b[1];
-            }
-        });
-    }
     static function apply_all(Qrequest $qreq, PaperInfo $prow = null, Contact $user, $action) {
         $ps = new PaperStatus($user->conf);
         $pj = (object) $ps->paper_json($prow);
         if (!isset($pj->pid)) {
             $pj->pid = -1;
         }
-        foreach (self::$list as $fn) {
-            $fn[2]->apply($pj, $qreq, $prow, $user, $action);
-        }
-        return $pj;
-    }
-
-    function apply($pj, Qrequest $qreq, PaperInfo $prow = null, Contact $user, $action) {
-    }
-
-    /** @param Qrequest $qreq */
-    static function replace_contacts($pj, $qreq) {
-        $pj->contacts = [];
-        for ($n = 1; isset($qreq["contacts:email_$n"]); ++$n) {
-            $email = trim($qreq["contacts:email_$n"]);
-            if (strcasecmp($email, "Email") === 0) {
-                $email = "";
-            }
-            $name = simplify_whitespace((string) $qreq["contacts:name_$n"]);
-            if (strcasecmp($name, "Name") === 0) {
-                $name = "";
-            }
-            if ($qreq["contacts:active_$n"] && $email !== "") {
-                $pj->contacts[] = (object) [
-                    "email" => $email,
-                    "name" => $name === "" ? null : $name,
-                    "is_new" => !!$qreq["contacts:isnew_$n"], "index" => $n
-                ];
-            }
-        }
-    }
-}
-
-class Default_PaperSaver extends PaperSaver {
-    function apply($pj, Qrequest $qreq, PaperInfo $prow = null, Contact $user, $action) {
-        $admin = $prow ? $user->can_administer($prow) : $user->privChair;
-
-        // Contacts
-        if ($qreq->has_contacts
-            || $action === "updatecontacts") {
-            PaperSaver::replace_contacts($pj, $qreq);
-        }
-        if (!$prow) {
-            if (!isset($pj->contacts)) {
-                $pj->contacts = [];
-            }
-            $has_me = !!array_filter($pj->contacts, function ($c) use ($user) {
-                return strcasecmp($c->email, $user->email) === 0;
-            });
-            if (!$has_me) {
-                $pj->contacts[] = Author::make_keyed($user);
-            }
-        }
-        if ($action === "updatecontacts") {
-            return;
-        }
 
         // Status
         unset($pj->status);
+        $updatecontacts = $action === "updatecontacts";
         if ($action === "submit") {
             $pj->submitted = true;
             $pj->draft = false;
         } else if ($action === "final") {
             $pj->final_submitted = $pj->submitted = true;
             $pj->draft = false;
-        } else {
+        } else if (!$updatecontacts) {
             $pj->submitted = false;
             $pj->draft = true;
         }
 
         // Fields
-        $nnprow = $prow ? : PaperInfo::make_new($user);
+        $nnprow = $prow ?? PaperInfo::make_new($user);
         foreach ($user->conf->options()->form_fields($nnprow) as $o) {
             if (($qreq["has_{$o->formid}"] || isset($qreq[$o->formid]))
-                && (!$o->final || $action === "final")) {
+                && (!$o->final || $action === "final")
+                && (!$updatecontacts || $o->id === PaperOption::CONTACTSID)) {
                 // XXX test_editable
                 $pj->{$o->json_key()} = $o->parse_web($nnprow, $qreq);
             }
         }
+
+        return $pj;
     }
 }
-
-PaperSaver::register(0, new Default_PaperSaver);
