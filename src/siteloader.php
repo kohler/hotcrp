609373f7f4076c16993f8b100275b34c029cbe56
Eddie Kohler <ekohler@gmail.com>
2020-09-21 10:24:14 -0400

    Mark settings that should not be transferred.

--- src/siteloader.php
+++ src/siteloader.php
@@ -33,6 +33,7 @@ class SiteLoader {
         "SearchTerm" => "src/papersearch.php",
         "SearchWord" => "src/papersearch.php",
         "SettingParser" => "src/settingvalues.php",
+        "Si" => "src/settingvalues.php",
         "StreamS3Result" => "lib/s3result.php",
         "TagAnno" => "lib/tagger.php",
         "TagInfo" => "lib/tagger.php",
