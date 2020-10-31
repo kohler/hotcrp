1556e2019445d92e8e01de6fd77967c8ed6b5888
Eddie Kohler <ekohler@gmail.com>
2020-10-31 16:15:47 -0400

    JS errors: Do not record errors in extensions.

--- src/api/api_error.php
+++ src/api/api_error.php
@@ -4,11 +4,14 @@
 
 class Error_API {
     static function jserror(Contact $user, Qrequest $qreq) {
-        if (isset($_SERVER["HTTP_USER_AGENT"])
-            && preg_match('/MSIE [78]|MetaSr/', $_SERVER["HTTP_USER_AGENT"])) {
+        $errormsg = trim((string) $qreq->error);
+        if ($errormsg === ""
+            || (isset($_SERVER["HTTP_USER_AGENT"])
+                && preg_match('/MSIE [78]|MetaSr/', $_SERVER["HTTP_USER_AGENT"]))
+            || preg_match('/(?:moz|safari|chrome)-extension/', $errormsg . ($qreq->stack ?? ""))) {
             return new JsonResult(true);
         }
-        $url = (string) $qreq->url;
+        $url = $qreq->url ?? "";
         if (preg_match(',[/=]((?:script|jquery)[^/&;]*[.]js),', $url, $m)) {
             $url = $m[1];
         }
@@ -21,30 +24,31 @@ class Error_API {
         if ($url !== "") {
             $url .= ": ";
         }
-        $errormsg = trim((string) $qreq->error);
-        if ($errormsg) {
-            $suffix = "";
-            if ($user->email)
-                $suffix .= ", user " . $user->email;
-            if (isset($_SERVER["REMOTE_ADDR"]))
-                $suffix .= ", host " . $_SERVER["REMOTE_ADDR"];
-            error_log("JS error: $url$errormsg$suffix");
-            if (($stacktext = $qreq->stack)) {
-                $stack = array();
-                foreach (explode("\n", $stacktext) as $line) {
-                    $line = trim($line);
-                    if ($line === "" || $line === $errormsg || "Uncaught $line" === $errormsg)
-                        continue;
-                    if (preg_match('/\Aat (\S+) \((\S+)\)/', $line, $m))
-                        $line = $m[1] . "@" . $m[2];
-                    else if (substr($line, 0, 1) === "@")
-                        $line = substr($line, 1);
-                    else if (substr($line, 0, 3) === "at ")
-                        $line = substr($line, 3);
-                    $stack[] = $line;
+        $suffix = "";
+        if ($user->email) {
+            $suffix .= ", user " . $user->email;
+        }
+        if (isset($_SERVER["REMOTE_ADDR"])) {
+            $suffix .= ", host " . $_SERVER["REMOTE_ADDR"];
+        }
+        error_log("JS error: $url$errormsg$suffix");
+        if (($stacktext = $qreq->stack)) {
+            $stack = array();
+            foreach (explode("\n", $stacktext) as $line) {
+                $line = trim($line);
+                if ($line === "" || $line === $errormsg || "Uncaught $line" === $errormsg) {
+                    continue;
                 }
-                error_log("JS error: {$url}via " . join(" ", $stack));
+                if (preg_match('/\Aat (\S+) \((\S+)\)/', $line, $m)) {
+                    $line = $m[1] . "@" . $m[2];
+                } else if (substr($line, 0, 1) === "@") {
+                    $line = substr($line, 1);
+                } else if (substr($line, 0, 3) === "at ") {
+                    $line = substr($line, 3);
+                }
+                $stack[] = $line;
             }
+            error_log("JS error: {$url}via " . join(" ", $stack));
         }
         return new JsonResult(true);
     }
