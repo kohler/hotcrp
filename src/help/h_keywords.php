e07eb4218c87e1172b8ede21f74ffe96a5593f2f
Eddie Kohler <ekohler@gmail.com>
2021-03-03 16:26:48 -0500

    Help tweak.

--- src/help/h_keywords.php
+++ src/help/h_keywords.php
@@ -134,7 +134,8 @@ class Keywords_HelpTopic {
             echo $hth->search_trow("retoken:J88ADNAB", "has a review with token J88ADNAB");
         }
         if ($hth->conf->setting("rev_ratings") != REV_RATINGS_NONE) {
-            echo $hth->search_trow("rate:good", "review was rated positively (“rate:bad” and “rate:bias” also work; can combine with “re:”)");
+            echo $hth->search_trow("rate:good", "has a positively-rated review (“rate:bad” and “rate:bias” also work)");
+            echo $hth->search_trow("rate:good:me", "has a positively-rated review by you");
         }
 
         echo $hth->tgroup("Comments");
