ca2c0838b44c3394d2e6f9596f08b5e13e413081
Eddie Kohler <ekohler@gmail.com>
2022-01-05 12:24:03 -0500

    Update d3.

--- devel/d3/rollup.config.js
+++ devel/d3/rollup.config.js
@@ -1,19 +1,28 @@
 import ascii from "rollup-plugin-ascii";
-import node from "rollup-plugin-node-resolve";
+import json from "@rollup/plugin-json";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import {terser} from "rollup-plugin-terser";
 
 export default [{
-  input: "index.js",
-  plugins: [
-    node(),
-    ascii(),
-    terser()
-  ],
-  output: {
-    extend: true,
-    file: "d3-hotcrp.min.js",
-    format: "umd",
-    indent: false,
-    name: "d3",
-  }
+    input: "index.js",
+    plugins: [
+        nodeResolve(),
+        json(),
+        ascii(),
+        terser({
+            mangle: {
+                reserved: [
+                    "InternMap",
+                    "InternSet"
+                ]
+            }
+        })
+    ],
+    output: {
+        file: "d3-hotcrp.min.js",
+        name: "d3",
+        format: "umd",
+        indent: false,
+        extend: true
+    }
 }];
