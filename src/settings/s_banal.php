b861f7cbc32eb1565b268b4b67e3030dc2c92d8e
Eddie Kohler <ekohler@gmail.com>
2022-03-12 16:12:29 -0500

    Banal parsing: support word limit

--- src/settings/s_banal.php
+++ src/settings/s_banal.php
@@ -27,6 +27,9 @@ class Banal_SettingRenderer {
         echo '<div class="entryi fx2"><label></label><div class="entry settings-banal-unlimitedref">';
         $sv->print_checkbox("sub_banal_unlimitedref_{$suffix}", "Unlimited reference pages", ["disabled" => !$uropen || !$editable, "label_class" => $uropen ? null : "dim"]);
         echo '</div></div>';
+        if ($sv->conf->opt("allowBanalWordlimit")) {
+            $sv->print_entry_group("sub_banal_wordlimit_{$suffix}", "Word limit", ["horizontal" => true, "readonly" => !$editable]);
+        }
         $sv->print_entry_group("sub_banal_bodyfontsize_{$suffix}", "Body font size", ["horizontal" => true, "control_after" => "&nbsp;pt", "readonly" => !$editable]);
         $sv->print_entry_group("sub_banal_bodylineheight_{$suffix}", "Line height", ["horizontal" => true, "control_after" => "&nbsp;pt", "readonly" => !$editable]);
         echo "</div></div></div>\n";
@@ -80,6 +83,12 @@ class Banal_SettingParser extends SettingParser {
         }
     }
 
+    /** @param string $s
+     * @return bool */
+    static function is_any_str($s) {
+        return $s === "" || strcasecmp($s, "any") === 0 || strcasecmp($s, "N/A") === 0;
+    }
+
     /** @param string $suffix
      * @param SettingValues $sv
      * @return bool */
@@ -101,119 +110,144 @@ class Banal_SettingParser extends SettingParser {
         $problem = false;
         $cfs = new FormatSpec($sv->oldv("sub_banal_data_{$suffix}"));
         $old_unparse = $cfs->unparse_banal();
-        $cfs->papersize = [];
-        if (($s = trim($sv->reqstr("sub_banal_papersize_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "any") !== 0
-            && strcasecmp($s, "N/A") !== 0) {
-            $ses = preg_split('/\s*,\s*|\s+OR\s+/i', $s);
-            foreach ($ses as $ss) {
-                if ($ss !== "" && ($d = FormatSpec::parse_dimen2($ss))) {
-                    $cfs->papersize[] = $d;
-                } else if ($ss !== "") {
-                    $sv->error_at("sub_banal_papersize_{$suffix}", "<0>Invalid paper size");
-                    $problem = true;
-                    $sout = null;
-                    break;
-                }
-            }
-        }
-
-        $cfs->pagelimit = null;
-        if (($s = trim($sv->reqstr("sub_banal_pagelimit_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "N/A") !== 0) {
-            if (($sx = cvtint($s, -1)) > 0) {
-                $cfs->pagelimit = [0, $sx];
-            } else if (preg_match('/\A(\d+)\s*(?:-|–)\s*(\d+)\z/', $s, $m)
-                       && $m[1] > 0 && $m[2] > 0 && $m[1] <= $m[2]) {
-                $cfs->pagelimit = [+$m[1], +$m[2]];
-            } else {
-                $sv->error_at("sub_banal_pagelimit_{$suffix}", "<5>Requires a whole number greater than 0, or a page range such as <code>2-4</code>");
-                $problem = true;
-            }
-        }
-
-        $cfs->unlimitedref = null;
-        if ($cfs->pagelimit
-            && trim($sv->reqstr("sub_banal_unlimitedref_{$suffix}") ?? "") !== "") {
-            $cfs->unlimitedref = true;
-        }
-
-        $cfs->columns = 0;
-        if (($s = trim($sv->reqstr("sub_banal_columns_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "any") !== 0
-            && strcasecmp($s, "N/A") !== 0) {
-            if (($sx = cvtint($s, -1)) >= 0)
-                $cfs->columns = $sx;
-            else {
-                $sv->error_at("sub_banal_columns_{$suffix}", "<0>Requires a whole number");
-                $problem = true;
-            }
-        }
-
-        $cfs->textblock = null;
-        if (($s = trim($sv->reqstr("sub_banal_textblock_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "any") !== 0
-            && strcasecmp($s, "N/A") !== 0) {
-            // change margin specifications into text block measurements
-            if (preg_match('/^(.*\S)\s+mar(gins?)?/i', $s, $m)) {
-                $s = $m[1];
-                if (!$cfs->papersize || count($cfs->papersize) !== 1) {
-                    $sv->error_at("sub_banal_papersize_{$suffix}", "<0>You must specify a paper size as well as margins");
-                    $sv->error_at("sub_banal_textblock_{$suffix}");
-                    $problem = true;
-                } else {
-                    $ps = $cfs->papersize[0];
-                    if (strpos($s, "x") === false) {
-                        $s = preg_replace('/\s+(?=[\d.])/', 'x', trim($s));
-                        $css = 1;
-                    } else {
-                        $css = 0;
-                    }
-                    if (!($m = FormatSpec::parse_dimen($s))
-                        || (is_array($m) && count($m) > 4)) {
-                        $sv->error_at("sub_banal_textblock_{$suffix}", "<0>Invalid margin definition");
+        if ($sv->has_req("sub_banal_papersize_{$suffix}")) {
+            $cfs->papersize = [];
+            $s = trim($sv->reqstr("sub_banal_papersize_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                $ses = preg_split('/\s*,\s*|\s+OR\s+/i', $s);
+                foreach ($ses as $ss) {
+                    if ($ss !== "" && ($d = FormatSpec::parse_dimen2($ss))) {
+                        $cfs->papersize[] = $d;
+                    } else if ($ss !== "") {
+                        $sv->error_at("sub_banal_papersize_{$suffix}", "<0>Invalid paper size");
                         $problem = true;
-                        $s = "";
-                    } else if (!is_array($m)) {
-                        $s = [$ps[0] - 2 * $m, $ps[1] - 2 * $m];
-                    } else if (count($m) == 2) {
-                        $s = [$ps[0] - 2 * $m[$css], $ps[1] - 2 * $m[1 - $css]];
-                    } else if (count($m) == 3) {
-                        $s = [$ps[0] - $m[$css] - $m[2 - $css], $ps[1] - $m[1 - $css] - $m[1 + $css]];
-                    } else {
-                        $s = [$ps[0] - $m[$css] - $m[2 + $css], $ps[1] - $m[1 - $css] - $m[3 - $css]];
+                        $sout = null;
+                        break;
                     }
                 }
-                $s = (is_array($s) ? FormatSpec::unparse_dimen($s) : "");
-            }
-            // check text block measurements
-            if ($s && ($s = FormatSpec::parse_dimen2($s))) {
-                $cfs->textblock = $s;
-            } else {
-                $sv->error_at("sub_banal_textblock_{$suffix}", "<0>Invalid text block definition");
-                $problem = true;
             }
         }
 
-        $cfs->bodyfontsize = null;
-        if (($s = trim($sv->reqstr("sub_banal_bodyfontsize_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "any") !== 0
-            && strcasecmp($s, "N/A") !== 0) {
-            $cfs->bodyfontsize = FormatSpec::parse_range($s);
-            if (!$cfs->bodyfontsize) {
-                $sv->error_at("sub_banal_bodyfontsize_{$suffix}", "<0>Requires a number greater than 0");
-                $problem = true;
+        if ($sv->has_req("sub_banal_pagelimit_{$suffix}")) {
+            $cfs->pagelimit = null;
+            $s = trim($sv->reqstr("sub_banal_pagelimit_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                if (($sx = cvtint($s, -1)) > 0) {
+                    $cfs->pagelimit = [0, $sx];
+                } else if (preg_match('/\A(\d+)\s*(?:-|–)\s*(\d+)\z/', $s, $m)
+                           && $m[1] > 0 && $m[2] > 0 && $m[1] <= $m[2]) {
+                    $cfs->pagelimit = [+$m[1], +$m[2]];
+                } else {
+                    $sv->error_at("sub_banal_pagelimit_{$suffix}", "<5>Requires a whole number greater than 0, or a page range such as <code>2-4</code>");
+                    $problem = true;
+                }
             }
         }
 
-        $cfs->bodylineheight = null;
-        if (($s = trim($sv->reqstr("sub_banal_bodylineheight_{$suffix}") ?? "")) !== ""
-            && strcasecmp($s, "any") !== 0
-            && strcasecmp($s, "N/A") !== 0) {
-            $cfs->bodylineheight = FormatSpec::parse_range($s);
-            if (!$cfs->bodylineheight) {
-                $sv->error_at("sub_banal_bodylineheight_{$suffix}", "<0>Requires a number greater than 0");
-                $problem = true;
+        if ($sv->has_req("sub_banal_unlimitedref_{$suffix}")) {
+            $cfs->unlimitedref = null;
+            if ($cfs->pagelimit
+                && trim($sv->reqstr("sub_banal_unlimitedref_{$suffix}")) !== "") {
+                $cfs->unlimitedref = true;
+            }
+        }
+
+        if ($sv->has_req("sub_banal_wordlimit_{$suffix}")) {
+            $cfs->wordlimit = null;
+            $s = trim($sv->reqstr("sub_banal_wordlimit_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                if (($sx = cvtint($s, -1)) >= 0) {
+                    $cfs->wordlimit = [0, $sx];
+                } else if (preg_match('/\A(\d+)\s*(?:-|–)\s*(\d+)\z/', $s, $m)
+                           && $m[1] > 0 && $m[2] > 0 && $m[1] <= $m[2]) {
+                    $cfs->wordlimit = [+$m[1], +$m[2]];
+                } else {
+                    $sv->error_at("sub_banal_wordlimit_{$suffix}", "<5>Requires a whole number or a range such as <code>2-4</code>");
+                    $problem = true;
+                }
+            }
+        }
+
+        if ($sv->has_req("sub_banal_columns_{$suffix}")) {
+            $cfs->columns = 0;
+            $s = trim($sv->reqstr("sub_banal_columns_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                if (($sx = cvtint($s, -1)) >= 0) {
+                    $cfs->columns = $sx;
+                } else {
+                    $sv->error_at("sub_banal_columns_{$suffix}", "<0>Requires a whole number");
+                    $problem = true;
+                }
+            }
+        }
+
+        if ($sv->has_req("sub_banal_textblock_{$suffix}")) {
+            $cfs->textblock = null;
+            $s = trim($sv->reqstr("sub_banal_textblock_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                // change margin specifications into text block measurements
+                if (preg_match('/^(.*\S)\s+mar(gins?)?/i', $s, $m)) {
+                    $s = $m[1];
+                    if (!$cfs->papersize || count($cfs->papersize) !== 1) {
+                        $sv->error_at("sub_banal_papersize_{$suffix}", "<0>You must specify a paper size as well as margins");
+                        $sv->error_at("sub_banal_textblock_{$suffix}");
+                        $problem = true;
+                    } else {
+                        $ps = $cfs->papersize[0];
+                        if (strpos($s, "x") === false) {
+                            $s = preg_replace('/\s+(?=[\d.])/', 'x', trim($s));
+                            $css = 1;
+                        } else {
+                            $css = 0;
+                        }
+                        if (!($m = FormatSpec::parse_dimen($s))
+                            || (is_array($m) && count($m) > 4)) {
+                            $sv->error_at("sub_banal_textblock_{$suffix}", "<0>Invalid margin definition");
+                            $problem = true;
+                            $s = "";
+                        } else if (!is_array($m)) {
+                            $s = [$ps[0] - 2 * $m, $ps[1] - 2 * $m];
+                        } else if (count($m) == 2) {
+                            $s = [$ps[0] - 2 * $m[$css], $ps[1] - 2 * $m[1 - $css]];
+                        } else if (count($m) == 3) {
+                            $s = [$ps[0] - $m[$css] - $m[2 - $css], $ps[1] - $m[1 - $css] - $m[1 + $css]];
+                        } else {
+                            $s = [$ps[0] - $m[$css] - $m[2 + $css], $ps[1] - $m[1 - $css] - $m[3 - $css]];
+                        }
+                    }
+                    $s = (is_array($s) ? FormatSpec::unparse_dimen($s) : "");
+                }
+                // check text block measurements
+                if ($s && ($s = FormatSpec::parse_dimen2($s))) {
+                    $cfs->textblock = $s;
+                } else {
+                    $sv->error_at("sub_banal_textblock_{$suffix}", "<0>Invalid text block definition");
+                    $problem = true;
+                }
+            }
+        }
+
+        if ($sv->has_req("sub_banal_bodyfontsize_{$suffix}")) {
+            $cfs->bodyfontsize = null;
+            $s = trim($sv->reqstr("sub_banal_bodyfontsize_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                $cfs->bodyfontsize = FormatSpec::parse_range($s);
+                if (!$cfs->bodyfontsize) {
+                    $sv->error_at("sub_banal_bodyfontsize_{$suffix}", "<0>Requires a number greater than 0");
+                    $problem = true;
+                }
+            }
+        }
+
+        if ($sv->has_req("sub_banal_bodylineheight_{$suffix}")) {
+            $cfs->bodylineheight = null;
+            $s = trim($sv->reqstr("sub_banal_bodylineheight_{$suffix}"));
+            if (!self::is_any_str($s)) {
+                $cfs->bodylineheight = FormatSpec::parse_range($s);
+                if (!$cfs->bodylineheight) {
+                    $sv->error_at("sub_banal_bodylineheight_{$suffix}", "<0>Requires a number greater than 0");
+                    $problem = true;
+                }
             }
         }
 
