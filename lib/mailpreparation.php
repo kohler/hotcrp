b2c6d01a4fbd82f67b029e7c768a404ba077b57f
Eddie Kohler <ekohler@gmail.com>
2022-03-19 21:35:38 -0400

    Wrap displayed mails.

--- lib/mailpreparation.php
+++ lib/mailpreparation.php
@@ -168,7 +168,7 @@ class MailPreparation implements JsonSerializable {
             unset($headers["mime-version"], $headers["content-type"], $headers["content-transfer-encoding"]);
             $text = join("", $headers) . $eol . $this->body;
             if (PHP_SAPI != "cli") {
-                $this->conf->feedback_msg(new MessageItem(null, "<pre>" . htmlspecialchars($text) . "</pre>", 0));
+                $this->conf->feedback_msg(new MessageItem(null, "<pre class=\"pw\">" . htmlspecialchars($text) . "</pre>", 0));
             } else if (!$this->conf->opt("disablePrintEmail")) {
                 fwrite(STDERR, "========================================\n" . str_replace("\r\n", "\n", $text) .  "========================================\n");
             }
