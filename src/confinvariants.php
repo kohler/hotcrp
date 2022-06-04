3f2c9edca3f5cfd1277ebd5d0ecc9555b137c5fb
Eddie Kohler <ekohler@gmail.com>
2022-06-04 17:08:30 -0400

    Add conference invariant: responses have the right tags.

--- src/confinvariants.php
+++ src/confinvariants.php
@@ -254,6 +254,12 @@ class ConfInvariants {
         if ($any) {
             $this->invariant_error("non-response #{0}/{1} has non-zero round");
         }
+        foreach ($this->conf->response_rounds() as $rrd) {
+            $any = $this->invariantq("select paperId, commentId from PaperComment where (commentType&" . CommentInfo::CT_RESPONSE . ")!=0 and commentRound={$rrd->id} and commentTags not like '% ?ls#%'", [$rrd->tag_name()]);
+            if ($any) {
+                $this->invariant_error("response #{0}/{1} in round ID {$rrd->id} lacks tag '" . $rrd->tag_name() . "'");
+            }
+        }
 
         // non-draft comments are displayed
         $any = $this->invariantq("select paperId, commentId from PaperComment where timeDisplayed=0 and (commentType&" . CommentInfo::CT_DRAFT . ")=0 limit 1");
