0ba65a3f4646a395d46f7e4dbab97907426b31b1
Eddie Kohler <ekohler@gmail.com>
2022-06-20 12:01:56 -0400

    Whoops.

---
+++ src/search/st_badge.php
@@ -0,0 +1,46 @@
+<?php
+// search/st_badge.php -- HotCRP helper class for searching for papers
+// Copyright (c) 2006-2022 Eddie Kohler; see LICENSE.
+
+class Badge_SearchTerm extends SearchTerm {
+    /** @var Contact */
+    private $user;
+    /** @var string */
+    private $word;
+
+    /** @param string $word */
+    function __construct(Contact $user, $word) {
+        parent::__construct("badge");
+        $this->user = $user;
+        $this->word = $word;
+    }
+    function sqlexpr(SearchQueryInfo $sqi) {
+        return 'exists (select * from PaperTag where paperId=Paper.paperId)';
+    }
+    function test(PaperInfo $row, $rrow) {
+        $tags = $row->viewable_tags($this->user);
+        foreach ($row->conf->tags()->badges($tags) as $tb) {
+            if ($this->word === "any" || $this->word === $tb[1])
+                return true;
+        }
+        return false;
+    }
+    function debug_json() {
+        return ["type" => $this->type, "style" => $this->word];
+    }
+    function about_reviews() {
+        return self::ABOUT_NO;
+    }
+
+    static function parse($word, SearchWord $sword, PaperSearch $srch) {
+        $word = strtolower($word);
+        if ($word === "any" || $word === "none") {
+            return (new Badge_SearchTerm($srch->user, "any"))->negate_if($word === "none");
+        } else if (($ks = $srch->conf->tags()->known_badge($word))) {
+            return new Badge_SearchTerm($srch->user, $ks->style);
+        } else {
+            $srch->lwarning($sword, "<0>Unknown badge color ‘{$word}’");
+            return new False_SearchTerm;
+        }
+    }
+}
