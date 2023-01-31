3059b532d50ee69281035960e5371b323c8c2476
Eddie Kohler <ekohler@gmail.com>
2023-01-30 22:58:28 -0500

    `"Peggy" Donellan"` is a first and last name, not name + email.

--- lib/text.php
+++ lib/text.php
@@ -187,13 +187,13 @@ class Text {
             if ($name === "") {
                 /* do nothing */;
             } else if ($name[strlen($name) - 1] === ">"
-                       && preg_match('{\A\"?(.*?)\"?\s*<([^<>]+)>\z}', $name, $m)) {
-                list($name, $email) = [$m[1], $m[2]];
-            } else if ($name[0] === "\""
-                       && preg_match('{\A\s*\"(.*)\"\s+(\S+)\z}', $name, $m)) {
-                list($name, $email) = [$m[1], $m[2]];
+                       && preg_match('/\A(?:\"(.*?)\"|(.*?))\s*<([^<>]+)>\z/', $name, $m)) {
+                list($name, $email) = [$m[1] . $m[2], $m[3]];
             } else if (strpos($name, "@") === false) {
                 /* skip */;
+            } else if ($name[0] === "\""
+                       && preg_match('{\A\s*\"(.*)\"\s+(\S+@\S+)\z}', $name, $m)) {
+                list($name, $email) = [$m[1], $m[2]];
             } else if (!preg_match('{\A(.*?)\s+(\S+)\z}', $name, $m)) {
                 return ["", "", trim($name)];
             } else if (strpos($m[2], "@") !== false) {
