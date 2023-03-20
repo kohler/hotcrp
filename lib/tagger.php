kohler/hotcrp

commit 586b7e74ebd94556b8797933fcc52d54e2094f74

Add more dynamic styles: text-rgb-RRGGBB, weight-WEIGHT.

lib/tagger.php
@@ -301,7 +301,16 @@ class TagStyle {
             } else {
                 return $s;
             }
-        } else if (str_starts_with($s, "font-")) {
+        } else if (str_starts_with($s, "text-rgb-")
+                   && (strlen($s) === 12 || strlen($s) === 15)
+                   && ctype_xdigit(substr($s, 9))) {
+            if (strlen($s) === 12) {
+                return "text-rgb-{$s[9]}{$s[9]}{$s[10]}{$s[10]}{$s[11]}{$s[11]}";
+            } else {
+                return $s;
+            }
+        } else if (str_starts_with($s, "font-")
+                   || str_starts_with($s, "weight-")) {
             return $s;
         } else {
             return null;
@@ -341,7 +350,7 @@ class TagStyle {
         if (($dstyle = self::dynamic_style($this->style)) !== null) {
             $this->style = $dstyle;
             $sclass |= self::DYNAMIC;
-            if ($dstyle[0] === "f") { // `font-`
+            if ($dstyle[0] !== "r") { // not `rgb-`
                 $sclass = ($sclass & ~self::BG) | self::TEXT;
             }
         }
@@ -510,7 +519,9 @@ class TagMap implements IteratorAggregate {
             && (($ltag[0] === ":" && $this->check_emoji_code($ltag))
                 || isset($this->style_lmap[$ltag])
                 || (str_starts_with($ltag, "rgb-") && ctype_xdigit(substr($ltag, 4)))
-                || str_starts_with($ltag, "font-"))) {
+                || (str_starts_with($ltag, "text-rgb-") && ctype_xdigit(substr($ltag, 9)))
+                || str_starts_with($ltag, "font-")
+                || str_starts_with($ltag, "weight-"))) {
             $t = $this->add($tag);
         }
         if ($this->has_pattern
@@ -719,7 +730,7 @@ class TagMap implements IteratorAggregate {
     private function color_regex() {
         if (!$this->color_re) {
             $rex = [
-                "{(?:\\A| )(?:(?:\\d*~|~~|)(font-[^\s#]+|rgb-[0-9a-f]{3}(?:|[0-9a-f]{3})|",
+                "{(?:\\A| )(?:(?:\\d*~|~~|)(font-[^\s#]+|weight-(?:[a-z]+|\d+)|(?:text-)rgb-[0-9a-f]{3}(?:|[0-9a-f]{3})|",
                 join("|", array_keys($this->style_lmap))
             ];
             $any = false;
@@ -796,7 +807,7 @@ class TagMap implements IteratorAggregate {
             $arg = json_encode_browser($key);
             if (str_starts_with($key, "badge-"))
                 $arg .= ",\"badge\"";
-            Ht::stash_script("hotcrp.make_pattern_fill({$arg})");
+            Ht::stash_script("hotcrp.ensure_pattern({$arg})");
             self::$multicolor_map[$key] = true;
         }
     }

scripts/graph.js
@@ -390,7 +390,7 @@ function pid_renderer(ps, cc) {
             }
         }
         if (cx) {
-            make_pattern_fill(cx);
+            ensure_pattern(cx);
             p = '<span class="'.concat(cx, '">#', p, '</span>');
         } else
             p = '#' + p;
@@ -853,7 +853,7 @@ function scatter_create(svg, data, klass) {
     sel.enter()
         .append("path")
         .attr("class", function (d) { return pathklass + (d[3] ? " " + d[3] : "") })
-        .style("fill", function (d) { return make_pattern_fill(d[3], "gdot"); })
+        .style("fill", function (d) { return ensure_pattern(d[3], "gdot"); })
       .merge(sel)
         .attr("d", scatter_annulus)
         .attr("transform", scatter_transform);
@@ -1095,7 +1095,7 @@ function graph_bars(selector, args) {
             .attr("class", function (d) {
                 return d[3] ? "gbar " + d[3] : "gbar";
             })
-            .style("fill", function (d) { return make_pattern_fill(d[3], "gdot"); }));
+            .style("fill", function (d) { return ensure_pattern(d[3], "gdot"); }));
 
     make_axes(svg, xAxis, yAxis, args);
 
@@ -1278,7 +1278,7 @@ function graph_boxplot(selector, args) {
     place_box(svg.selectAll(".gbox.box").data(nonoutliers)
             .enter().append("path")
             .attr("class", function (d) { return "gbox box " + d.c; })
-            .style("fill", function (d) { return make_pattern_fill(d.c, "gdot"); }));
+            .style("fill", function (d) { return ensure_pattern(d.c, "gdot"); }));
 
     place_median(svg.selectAll(".gbox.median").data(nonoutliers)
             .enter().append("line")
@@ -1562,7 +1562,7 @@ function named_integer_ticks(map) {
                         .attr("x", b.x - 3).attr("y", b.y)
                         .attr("width", b.width + 6).attr("height", b.height + 1)
                         .attr("class", "glab " + c)
-                        .style("fill", make_pattern_fill(c, "glab"));
+                        .style("fill", ensure_pattern(c, "glab"));
                 }
             });
         }

scripts/script.js
@@ -5463,7 +5463,7 @@ function render_comment(cj, editing) {
         t.push("cmt" + cj.visibility + "vis");
     }
     if (cj.color_classes) {
-        make_pattern_fill(cj.color_classes);
+        ensure_pattern(cj.color_classes);
         t.push("cmtcolor " + cj.color_classes);
     }
     if (t.length && !editing) {
@@ -8282,9 +8282,7 @@ plinfo.update_tag_decoration = function ($title, html) {
     $title.find(".tagdecoration").remove();
     if (html) {
         $title.append(html);
-        $title.find(".badge").each(function () {
-            make_pattern_fill(this.className);
-        });
+        $title.find(".badge").each(ensure_pattern_here);
     }
 };
 
@@ -8303,13 +8301,13 @@ $(window).on("hotcrptags", function (evt, rv) {
         pr.setAttribute("data-tags-conflicted", rv.tags_conflicted);
     }
     if (rv.color_classes) {
-        make_pattern_fill(rv.color_classes);
+        ensure_pattern(rv.color_classes);
     }
     if ("color_classes_conflicted" in rv) {
         pr.setAttribute("data-color-classes", rv.color_classes);
         pr.setAttribute("data-color-classes-conflicted", rv.color_classes_conflicted);
         $ptr.addClass("colorconflict");
-        make_pattern_fill(rv.color_classes_conflicted);
+        ensure_pattern(rv.color_classes_conflicted);
     } else {
         $ptr.removeClass("colorconflict");
     }
@@ -8391,7 +8389,7 @@ return plinfo;
 
 
 /* pattern fill functions */
-window.make_pattern_fill = (function () {
+window.ensure_pattern = (function () {
 var fmap = {},
     knownmap = {
         "tag-white": true, "tag-red": true, "tag-orange": true, "tag-yellow": true,
@@ -8466,10 +8464,21 @@ function class_color(k) {
         ensure_stylesheet().insertRule(".".concat(k, " { background-color: rgb(", c.r, ", ", c.g, ", ", c.b, "); }"), 0);
         return c;
     }
+    if (k.startsWith("tag-text-rgb-")
+        && (m = k.match(/^tag-text-rgb-([0-9a-f]{2})([0-9a-f]{2})([0-9a-f]{2})$/))) {
+        colormap[k] = c = make_color(k, parseInt(m[1], 16), parseInt(m[2], 16), parseInt(m[3], 16), 1.0);
+        ensure_stylesheet().insertRule(".".concat(k, " { color: rgb(", c.r, ", ", c.g, ", ", c.b, "); }"), 0);
+        return c;
+    }
     if (k.startsWith("tag-font-")) {
         m = k.substring(9).replace(/_/g, " ");
         c = /^(?:serif|sans-serif|monospace|cursive|fantasy|system-ui|ui-(?:serif|sans-serif|monospace|rounded)|math|emoji|fangsong)$/.test(m) ? "" : "\"";
         ensure_stylesheet().insertRule(".".concat(k, ".taghh, .", k, " .taghl { font-family: ", c, m, c, "; }"), 0);
+        colormap[k] = null;
+        return null;
+    }
+    if (k.startsWith("tag-weight-")) {
+        ensure_stylesheet().insertRule(".".concat(k, ".taghh, .", k, " .taghl { font-weight: ", k.substring(11), "; }"), 0);
         colormap[k] = null;
         return null;
     }
@@ -8646,6 +8655,13 @@ return function (classes, type) {
     return fmap[index];
 };
 })();
+
+function ensure_pattern_here() {
+    if (this.className !== ""
+        && (this.className.indexOf("tag-") >= 0
+            || this.className.indexOf("badge-") >= 0))
+        ensure_pattern(this.className);
+}
 
 
 /* form value transfer */
@@ -9008,7 +9024,7 @@ function prepare_paper_select() {
                 var $p = $(self).find(".js-psedit-result").first();
                 $p.html(data.result || ctl.options[ctl.selectedIndex].innerHTML);
                 if (data.color_classes) {
-                    make_pattern_fill(data.color_classes);
+                    ensure_pattern(data.color_classes);
                     $p.html('<span class="taghh ' + data.color_classes + '">' + $p.html() + '</span>');
                 }
             }
@@ -9474,7 +9490,7 @@ if (siteinfo.paperid) {
     $(window).on("hotcrptags", function (event, data) {
         if (data.pid != siteinfo.paperid)
             return;
-        data.color_classes && make_pattern_fill(data.color_classes, "", true);
+        data.color_classes && ensure_pattern(data.color_classes, "", true);
         $(".has-tag-classes").each(function () {
             var t = $.trim(this.className.replace(/(?: |^)(?:tagbg|dark|tag-\S+)(?= |$)/g, " "));
             if (data.color_classes)
@@ -11038,6 +11054,7 @@ window.hotcrp = {
     check_version: check_version,
     demand_load: demand_load,
     edit_comment: papercomment.edit,
+    ensure_pattern: ensure_pattern,
     escape_html: escape_html,
     focus_within: focus_within,
     fold: fold,
@@ -11049,7 +11066,6 @@ window.hotcrp = {
     init_deadlines: hotcrp_deadlines.init,
     load_editable_paper: edit_paper_ui.load,
     load_editable_review: edit_paper_ui.load_review,
-    make_pattern_fill: make_pattern_fill,
     onload: hotcrp_load,
     paper_edit_conditions: edit_paper_ui.edit_condition,
     prepare_editable_paper: edit_paper_ui.prepare,

commit ea539409bfefeaa6878e9af2dcfa9635d691fa9f

Recover inherent coloring of tag like `rgb-RRGGBB`.
Problem introduced in 586b7e74eb.

lib/tagger.php
@@ -730,7 +730,7 @@ class TagMap implements IteratorAggregate {
     private function color_regex() {
         if (!$this->color_re) {
             $rex = [
-                "{(?:\\A| )(?:(?:\\d*~|~~|)(font-[^\s#]+|weight-(?:[a-z]+|\d+)|(?:text-)rgb-[0-9a-f]{3}(?:|[0-9a-f]{3})|",
+                "{(?:\\A| )(?:(?:\\d*~|~~|)(font-[^\s#]+|weight-(?:[a-z]+|\d+)|(?:text-|)rgb-[0-9a-f]{3}(?:|[0-9a-f]{3})|",
                 join("|", array_keys($this->style_lmap))
             ];
             $any = false;
@@ -805,8 +805,9 @@ class TagMap implements IteratorAggregate {
         $key = is_array($classes) ? join(" ", $classes) : $classes;
         if (!isset(self::$multicolor_map[$key])) {
             $arg = json_encode_browser($key);
-            if (str_starts_with($key, "badge-"))
+            if (str_starts_with($key, "badge-")) {
                 $arg .= ",\"badge\"";
+            }
             Ht::stash_script("hotcrp.ensure_pattern({$arg})");
             self::$multicolor_map[$key] = true;
         }