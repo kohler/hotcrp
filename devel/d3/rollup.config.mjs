1b2b1f04c8739254ef86d70f51342aaaebf3a8ae
Eddie Kohler <ekohler@gmail.com>
2023-05-25 08:17:30 -0400

    Update d3-hotcrp bundle.

--- devel/d3/rollup.config.mjs
+++ devel/d3/rollup.config.mjs
@@ -1,14 +1,12 @@
-import ascii from "rollup-plugin-ascii";
 import json from "@rollup/plugin-json";
 import nodeResolve from "@rollup/plugin-node-resolve";
-import {terser} from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 export default [{
     input: "index.js",
     plugins: [
         nodeResolve(),
         json(),
-        ascii(),
         terser({
             mangle: {
                 reserved: [
