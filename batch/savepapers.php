f840be453cdbc5a33d5b8411f23b7d5cd416835f
Eddie Kohler <ekohler@gmail.com>
2024-01-21 18:48:25 +0000

    Improve error reporting.

--- batch/savepapers.php
+++ batch/savepapers.php
@@ -252,7 +252,7 @@ class SavePapers_Batch {
             fwrite(STDERR, "{$action}\n");
         }
         // XXX does not change decision
-        if (!$this->silent) {
+        if (!$this->silent || !$pid) {
             foreach ($this->ps->decorated_message_list() as $mi) {
                 fwrite(STDERR, $prefix . $mi->message_as(0) . "\n");
             }
@@ -330,7 +330,7 @@ class SavePapers_Batch {
             $j = $jl[$index];
             $jl[$index] = null;
             $this->run_one($index, $j);
-            if ($this->nerrors && !$this->ignore_errors) {
+            if ($this->nerrors > 0 && !$this->ignore_errors) {
                 break;
             }
             gc_collect_cycles();
