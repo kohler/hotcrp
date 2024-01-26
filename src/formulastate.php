988b3fb50368417d29e197662896e4c44baf996b
Eddie Kohler <ekohler@gmail.com>
2024-01-26 11:07:00 -0500

    A bit more OO

--- src/formulastate.php
+++ src/formulastate.php
@@ -2,7 +2,7 @@
 // formulastate.php -- HotCRP helper class for formula expressions
 // Copyright (c) 2009-2024 Eddie Kohler; see LICENSE.
 
-class FormulaStateValue {
+abstract class FormulaValue {
     /** @var mixed */
     public $v;
     /** @var int */
@@ -11,8 +11,13 @@ class FormulaStateValue {
     public $vt;
     /** @var string */
     public $name;
-    /** @var callable(FormulaState):mixed */
-    public $evaluator;
+
+    /** @param string $name
+     * @param 0|1 $vt */
+    function __construct($name, $vt) {
+        $this->name = $name;
+        $this->vt = $vt;
+    }
 
     /** @param int $ver
      * @param FormulaState $fst
@@ -21,9 +26,116 @@ class FormulaStateValue {
         $ver = $fst->ver[$this->vt];
         if ($this->ver !== $ver) {
             $this->ver = $ver;
-            $this->v = call_user_func($this->evaluator, $fst);
+            $this->v = $this->evaluate($fst);
         }
         return $this->v;
+    }
+
+    /** @param FormulaState $fst */
+    abstract function evaluate($fst);
+}
+
+class Null_FormulaValue extends FormulaValue {
+    function __construct() {
+        parent::__construct("null", 0);
+    }
+    function evaluate($fst) {
+        return null;
+    }
+}
+
+class Tags_FormulaValue extends FormulaValue {
+    function __construct() {
+        parent::__construct("tags", 0);
+    }
+    function evaluate($fst) {
+        return $fst->prow->searchable_tags($fst->user);
+    }
+}
+
+class SingleTag_FormulaValue extends FormulaValue {
+    /** @var string */
+    private $tm;
+    /** @var bool */
+    private $isvalue;
+    /** @param string $tag
+     * @param bool $isvalue */
+    function __construct($tag, $isvalue) {
+        parent::__construct("single_tag {$tag}")
+        $this->tm = " {$tag}#";
+        $this->isvalue = $isvalue;
+    }
+    function evaluate($fst) {
+        return Tag_Fexpr::tag_value($fst->prow->searchable_tags($fst->user), $this->tm, $this->isvalue);
+    }
+}
+
+class MaxReviewVSB_FormulaValue extends FormulaValue {
+    function __construct() {
+        parent::__construct("max_review_vsb", 0);
+    }
+    function evaluate($fst) {
+        $vsb = VIEWSCORE_EMPTY;
+        foreach ($fst->prow->viewable_reviews_as_display($fst->user) as $rrow) {
+            $vsb = max($vsb, $fst->user->view_score_bound($fst->prow, $fst->rrow));
+        }
+        return $vsb;
+    }
+}
+
+class ReviewVSB_FormulaValue extends FormulaValue {
+    function __construct() {
+        parent::__construct("review_vsb", 1);
+    }
+    function evaluate($fst) {
+        return $fst->user->view_score_bound($fst->prow, $fst->rrow);
+    }
+}
+
+class ReviewFieldRoot_FormulaValue extends FormulaValue {
+    /** @var ReviewField */
+    protected $rf;
+    /** @var int */
+    protected $order;
+    /** @var bool */
+    protected $always_exists;
+    function __construct(ReviewField $rf) {
+        parent::__construct("rf.{$rf->short_id}", 1);
+        $this->rf = $rf;
+        $this->order = $rf->order;
+        $this->always_exists = $rf->always_exists();
+    }
+    function evaluate($fst) {
+        if (!$fst->rrow->reviewSubmitted) {
+            return null;
+        } else if ($this->always_exists) {
+            return $fst->rrow->fields[$this->order];
+        } else {
+            return $fst->rrow->fval($this->rf);
+        }
+    }
+}
+
+class ReviewField_FormulaValue extends ReviewFieldRoot_FormulaValue {
+    /** @var FormulaValue */
+    private $max_review_vsb;
+    /** @var FormulaValue */
+    private $review_vsb;
+    function __construct(ReviewField $rf, FormulaValue $max_review_vsb, FormulaValue $review_vsb) {
+        parent::__construct($rf);
+        $this->max_review_vsb = $max_review_vsb;
+        $this->review_vsb = $review_vsb;
+    }
+    function evaluate($fst) {
+        if (!$fst->rrow->reviewSubmitted
+            || ($this->rf->view_score <= $this->max_review_vsb->value($fst)
+                && $this->rf->view_score <= $this->review_vsb->value($fst))) {
+            return null;
+        } else if ($this->always_exists) {
+            return $fst->rrow->fields[$this->order];
+        } else {
+            return $fst->rrow->fval($this->rf);
+        }
     }
 }
 
@@ -40,8 +152,8 @@ class FormulaState {
     public $rrow;
     /** @var list<int> */
     public $ver = [0, 0];
-    /** @var list<FormulaStateValue> */
-    private $fsv = [];
+    /** @var list<FormulaValue> */
+    private $fv = [];
     /** @var ?list */
     private $ldomain;
     /** @var int */
@@ -55,93 +167,70 @@ class FormulaState {
     }
 
     /** @param string $name
-     * @return ?FormulaStateValue */
+     * @return ?FormulaValue */
     function find($name) {
-        foreach ($this->fsv as $fsv) {
-            if ($fsv->name === $name)
-                return $fsv;
+        foreach ($this->fv as $fv) {
+            if ($fv->name === $name)
+                return $fv;
         }
         return null;
     }
 
     /** @param string $name
-     * @param callable(FormulaState):mixed $evaluator
-     * @param 0|1 $vt
-     * @return FormulaStateValue */
-    function ensure($name, $evaluator, $vt) {
-        if (!($fsv = $this->find($name))) {
-            $fsv = new FormulaStateValue;
-            $fsv->name = $name;
-            $fsv->evaluator = $evaluator;
-            $fsv->vt = $vt;
-            $this->fsv[] = $fsv;
+     * @param class-string<FormulaValue> $value_class
+     * @return FormulaValue */
+    function ensure($name, $value_class) {
+        if (!($fv = $this->find($name))) {
+            $fv = new $value_class;
+            assert($fv->name === $name);
+            $this->fv[] = $fv;
         }
-        return $fsv;
+        return $fv;
     }
 
-    /** @return FormulaStateValue */
+    /** @param FormulaValue $fv
+     * @return $this */
+    function add($fv) {
+        $this->fv[] = $fv;
+        return $this;
+    }
+
+    /** @return FormulaValue */
     function ensure_null() {
-        return $this->ensure("null", "FormulaState::null_evaluator", 0);
+        return $this->ensure("null", "Null_FormulaValue");
     }
 
-    /** @return FormulaStateValue */
+    /** @return FormulaValue */
     function ensure_tags() {
-        return $this->ensure("tags", "FormulaState::tags_evaluator", 0);
+        return $this->ensure("tags", "Tags_FormulaValue");
     }
 
-    /** @return FormulaStateValue */
+    /** @return FormulaValue */
     function ensure_max_rrow_view_score_bound() {
-        return $this->ensure("max_rrow_vsb", "FormulaState::max_rrow_vsb_evaluator", 0);
+        return $this->ensure("max_review_vsb", "MaxReviewVSB_FormulaValue");
     }
 
-    /** @return FormulaStateValue */
+    /** @return FormulaValue */
     function ensure_rrow_view_score_bound() {
-        return $this->ensure("rrow_vsb", "FormulaState::rrow_vsb_evaluator", 1);
+        return $this->ensure("review_vsb", "ReviewVSB_FormulaValue");
     }
 
-    /** @return FormulaStateValue */
+    /** @return FormulaValue */
     function ensure_rf(ReviewField $rf) {
         if ($rf->view_score <= $this->user->permissive_view_score_bound()) {
             return $this->ensure_null();
-        } else if (($fsv = $this->find("rf.{$rf->short_id}"))) {
-            return $fsv;
         }
-        if ($this->user->is_root_user()) {
-            if ($rf->always_exists()) {
-                $fn = function ($fst) use ($rf) {
-                    return $fst->rrow->reviewSubmitted ? $fst->rrow->fields[$rf->order] : null;
-                };
+        if (!($fv = $this->find("rf.{$rf->short_id}"))) {
+            if ($this->user->is_root_user()) {
+                $fv = new ReviewFieldRoot_FormulaValue($rf);
             } else {
-                $fn = function ($fst) use ($rf) {
-                    return $fst->rrow->reviewSubmitted ? $fst->rrow->fval($rf) : null;
-                };
+                $mb = $this->ensure_max_rrow_view_score_bound();
+                $b = $this->ensure_rrow_view_score_bound();
+                $fv = new ReviewField_FormulaValue($rf, $mb, $b);
             }
-        } else {
-            $mb = $this->ensure_max_rrow_view_score_bound();
-            $b = $this->ensure_rrow_view_score_bound();
-            if ($rf->always_exists()) {
-                $fn = function ($fst) use ($mb, $b, $rf) {
-                    if ($fst->rrow->reviewSubmitted
-                        && ($rf->view_score > $mb->value($fst)
-                            || $rf->view_score > $b->value($fst))) {
-                        return $fst->rrow->fields[$rf->order];
-                    } else {
-                        return null;
-                    }
-                };
-            } else {
-                $fn = function ($fst) use ($mb, $b, $rf) {
-                    if ($fst->rrow->reviewSubmitted
-                        && ($rf->view_score > $mv->value($fst)
-                            || $rf->view_score > $b->value($fst))) {
-                        return $fst->rrow->fval($rf);
-                    } else {
-                        return null;
-                    }
-                };
-            }
+            $this->fv[] = $fv;
         }
-        return $this->ensure("rf.{$rf->short_id}", $fn, 1);
+        return $fv;
     }
 
     function reset(?PaperInfo $prow) {
@@ -186,30 +275,5 @@ class FormulaState {
             $this->ldomain = null;
         }
         ++$this->ver[1];
-    }
-
-
-    /** @param FormulaState $fst */
-    static function null_evaluator($fst) {
-        return null;
-    }
-
-    /** @param FormulaState $fst */
-    static function tags_evaluator($fst) {
-        return $fst->prow->searchable_tags($fst->user);
-    }
-
-    /** @param FormulaState $fst */
-    static function max_rrow_vsb_evaluator($fst) {
-        $vsb = VIEWSCORE_EMPTY;
-        foreach ($fst->prow->viewable_reviews_as_display($fst->user) as $rrow) {
-            $vsb = max($vsb, $fst->user->view_score_bound($fst->prow, $fst->rrow));
-        }
-        return $vsb;
-    }
-
-    /** @param FormulaState $fst */
-    static function rrow_vsb_evaluator($fst) {
-        return $fst->user->view_score_bound($fst->prow, $fst->rrow);
     }
 }
