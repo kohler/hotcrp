8a73b16d553dae0334ca17c7ff54d96507215e5b
Eddie Kohler <ekohler@gmail.com>
2024-04-17 10:36:09 -0400

    Derp

--- src/confinvariants.php
+++ src/confinvariants.php
@@ -417,7 +417,7 @@ class ConfInvariants {
 
         // load users
         $primary = [];
-        $result = $this->conf->qe("select " . $this->conf->user_query_fields() . " from ContactInfo");
+        $result = $this->conf->qe("select " . $this->conf->user_query_fields() . ", unaccentedName from ContactInfo");
         while (($u = $result->fetch_object())) {
             $u->contactId = intval($u->contactId);
             $u->primaryContactId = intval($u->primaryContactId);
