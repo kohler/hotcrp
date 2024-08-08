kohler/hotcrp

commit b6f8fc1dd6dfdac64bb748b82d529fdec7039e70

Nits.

src/contact.php
@@ -4534,7 +4534,7 @@ class Contact implements JsonSerializable {
         $rights = $this->rights($prow);
         if (!$this->can_view_review($prow, $rrow)
             || (!$rights->allow_pc && !$rights->allow_review)
-            || ($rs != REV_RATINGS_PC && $rs != REV_RATINGS_PC_EXTERNAL)) {
+            || ($rs !== REV_RATINGS_PC && $rs !== REV_RATINGS_PC_EXTERNAL)) {
             return false;
         }
         if (!$rrow
@@ -4549,10 +4549,10 @@ class Contact implements JsonSerializable {
         // See also PaperSearch::unusable_ratings.
         $nsubraters = 0;
         foreach ($prow->all_reviews() as $rrow) {
-            if ($rrow->reviewNeedsSubmit == 0
-                && $rrow->contactId != $this->contactId
-                && ($rs == REV_RATINGS_PC_EXTERNAL
-                    || ($rs == REV_RATINGS_PC && $rrow->reviewType > REVIEW_EXTERNAL)))
+            if ($rrow->reviewNeedsSubmit === 0
+                && $rrow->contactId !== $this->contactId
+                && ($rs === REV_RATINGS_PC_EXTERNAL
+                    || ($rs === REV_RATINGS_PC && $rrow->reviewType > REVIEW_EXTERNAL)))
                 ++$nsubraters;
         }
         return $nsubraters >= 2;
@@ -4561,7 +4561,8 @@ class Contact implements JsonSerializable {
     /** @return bool */
     function can_view_some_review_ratings() {
         $rs = $this->conf->setting("rev_ratings");
-        return $this->is_reviewer() && ($rs == REV_RATINGS_PC || $rs == REV_RATINGS_PC_EXTERNAL);
+        return ($rs === REV_RATINGS_PC || $rs === REV_RATINGS_PC_EXTERNAL)
+            && $this->is_reviewer();
     }
 
     /** @param ?ReviewInfo $rrow

lib/abbreviationmatcher.php
@@ -167,11 +167,6 @@ class AbbreviationMatcher {
     }
     /** @param string $s
      * @return bool */
-    static function is_camel_word_old($s) {
-        return preg_match('/\A[-_.A-Za-z0-9]*(?:[A-Za-z](?=[-_.A-Z0-9])|[0-9](?=[-_.A-Za-z]))[-_.A-Za-z0-9]*\*?\z/', $s);
-    }
-    /** @param string $s
-     * @return bool */
     static function is_camel_word($s) {
         return preg_match('/\A[_.A-Za-z0-9~?!\'*]*(?:[A-Za-z][_.A-Z0-9]|[0-9][_.A-Za-z])[_.A-Za-z0-9~?!\'*]*\z/', $s);
     }

lib/icons.php
@@ -82,16 +82,16 @@ class Icons {
     /** @param 0|1|2|3 $direction
      * @return string */
     static function ui_linkarrow($direction) {
-        $t = '<svg class="licon-s" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="';
-        if ($direction == 0)
-            $t .= 'M2 11L8 1L14 11';
-        else if ($direction == 1)
-            $t .= 'M5 3L15 9L5 15';
-        else if ($direction == 2)
-            $t .= 'M2 1L8 11L14 1';
-        else if ($direction == 3)
-            $t .= 'M11 3L1 9L11 15';
-        return $t . '" /></svg>';
+        if ($direction === 0) {
+            $t = 'M2 11L8 1L14 11';
+        } else if ($direction === 1) {
+            $t = 'M5 3L15 9L5 15';
+        } else if ($direction === 2) {
+            $t = 'M2 1L8 11L14 1';
+        } else { // $direction === 3
+            $t = 'M11 3L1 9L11 15';
+        }
+        return '<svg class="licon-s" width="0.75em" height="0.75em" viewBox="0 0 16 16" preserveAspectRatio="none"><path d="' . $t . '" /></svg>';
     }
     /** @param 0|2 $direction
      * @return string */

lib/mincostmaxflow.php
@@ -141,9 +141,9 @@ class MinCostMaxFlow_Edge {
      * @return bool */
     function is_distance_admissible_from($v) {
         if ($v === $this->src) {
-            return $this->flow < $this->cap && $v->distance == $this->dst->distance + 1;
+            return $this->flow < $this->cap && $v->distance === $this->dst->distance + 1;
         } else {
-            return $this->flow > $this->mincap && $v->distance == $this->src->distance + 1;
+            return $this->flow > $this->mincap && $v->distance === $this->src->distance + 1;
         }
     }
     /** @param MinCostMaxFlow_Node $v
@@ -459,7 +459,7 @@ class MinCostMaxFlow {
      * @param MinCostMaxFlow_Node $src */
     private function pushrelabel_push_from($e, $src) {
         $amt = min($src->excess, $e->residual_cap_from($src));
-        $amt = ($src == $e->dst ? -$amt : $amt);
+        $amt = ($src === $e->dst ? -$amt : $amt);
         //fwrite(STDERR, "push {$amt} {$e->src->name}@{$e->src->distance} -> {$e->dst->name}@{$e->dst->distance}\n");
         $e->update_flow($amt);
     }
@@ -533,7 +533,7 @@ class MinCostMaxFlow {
         while ($l) {
             // check progress
             ++$n;
-            if ($n % 32768 == 0) {
+            if ($n % 32768 === 0) {
                 foreach ($this->progressf as $progressf) {
                     call_user_func($progressf, $this, self::PMAXFLOW);
                 }
@@ -548,7 +548,7 @@ class MinCostMaxFlow {
                 }
                 // global relabeling heuristic is quite useful
                 ++$this->nrelabel;
-                if ($this->nrelabel % count($this->v) == 0) {
+                if ($this->nrelabel % count($this->v) === 0) {
                     $this->pushrelabel_make_distance();
                 }
                 // if relabeled, put back on front

lib/navigation.php
@@ -387,7 +387,7 @@ class NavigationState {
         if ($this->path !== "") {
             $p = explode("/", substr($this->path, 1));
             if ($n + 1 < count($p)
-                || ($n + 1 == count($p) && $p[$n] !== "")) {
+                || ($n + 1 === count($p) && $p[$n] !== "")) {
                 return $decoded ? urldecode($p[$n]) : $p[$n];
             }
         }

lib/unicodehelper.php
@@ -243,9 +243,9 @@ class UnicodeHelper {
     static function utf8_to_html_entities($str, $flag = ENT_NOQUOTES) {
         if ($flag & ENT_IGNORE) {
             $start = "";
-        } else if (($flag & ENT_QUOTES) == ENT_QUOTES) {
+        } else if (($flag & ENT_QUOTES) === ENT_QUOTES) {
             $start = "&<>\"'";
-        } else if (($flag & ENT_COMPAT) == ENT_COMPAT) {
+        } else if (($flag & ENT_COMPAT) === ENT_COMPAT) {
             $start = "&<>\"";
         } else {
             $start = "&<>";
@@ -280,9 +280,9 @@ class UnicodeHelper {
     static function utf8_to_xml_numeric_entities($str, $flag = ENT_NOQUOTES) {
         if ($flag & ENT_IGNORE) {
             $start = "";
-        } else if (($flag & ENT_QUOTES) == ENT_QUOTES) {
+        } else if (($flag & ENT_QUOTES) === ENT_QUOTES) {
             $start = "&<>\"'";
-        } else if (($flag & ENT_COMPAT) == ENT_COMPAT) {
+        } else if (($flag & ENT_COMPAT) === ENT_COMPAT) {
             $start = "&<>\"";
         } else {
             $start = "&<>";

src/pages/p_offline.php
@@ -81,19 +81,20 @@ class Offline_Page {
         }
     }
 
+    /** @return bool */
     function handle_request() {
-        if ($this->qreq->download || $this->qreq->downloadForm /* XXX */) {
+        if ($this->qreq->download) {
             $this->handle_download();
             return true;
-        }
-        if (($this->qreq->upload || $this->qreq->uploadForm /* XXX */)
-            && $this->qreq->valid_post()) {
+        } else if ($this->qreq->upload
+                   && $this->qreq->valid_post()) {
             return $this->handle_upload();
-        }
-        if (($this->qreq->setvote || $this->qreq->setrank)
-            && $this->qreq->valid_post()
-            && $this->user->is_reviewer()) {
+        } else if (($this->qreq->setvote || $this->qreq->setrank)
+                   && $this->qreq->valid_post()
+                   && $this->user->is_reviewer()) {
             return $this->handle_tag_indexes();
+        } else {
+            return false;
         }
     }
 

src/pages/p_review.php
@@ -252,7 +252,7 @@ class Review_Page {
             if ($this->rrow->reviewToken !== 0) {
                 $this->conf->update_rev_tokens_setting(-1);
             }
-            if ($this->rrow->reviewType == REVIEW_META) {
+            if ($this->rrow->reviewType === REVIEW_META) {
                 $this->conf->update_metareviews_setting(-1);
             }
 
@@ -400,7 +400,7 @@ class Review_Page {
             $pp->handle_adopt();
         } else if ($qreq->upload && $qreq->valid_post()) {
             $pp->handle_upload_form();
-        } else if ($qreq->download || $qreq->downloadForm /* XXX */) {
+        } else if ($qreq->download) {
             $pp->handle_download_form();
         } else if ($qreq->text) {
             $pp->handle_download_text();

commit d3412df2d16c161eaa0fb19aa572ce0e526f54cd

Recover review rating display and search (#354).
Was broken when b6f8fc1dd6dfdac64bb748b82d529fdec7039e70 mistakenly
introduced strict equality; correct behavior required that null
be treated the same as 0.

src/conference.php
@@ -356,7 +356,7 @@ class Conf {
 
     function load_settings() {
         $this->__load_settings();
-        if ($this->sversion < 295) {
+        if ($this->sversion < 296) {
             $old_nerrors = Dbl::$nerrors;
             while ((new UpdateSchema($this))->run()) {
                 usleep(50000);
@@ -3421,6 +3421,12 @@ class Conf {
                 || ($this->settings["viewrevid_ext"] ?? 0) >= 0);
     }
 
+    /** @return 0|-1|1 */
+    function review_ratings() {
+        $rs = $this->settings["rev_ratings"] ?? 0;
+        return $rs > 1 ? -1 : $rs;
+    }
+
     /** @return bool */
     function has_any_submitted() {
         return !($this->settings["no_papersub"] ?? false);
@@ -3917,7 +3923,7 @@ class Conf {
 
     /** @return string */
     function rating_signature_query() {
-        if ($this->setting("rev_ratings") !== REV_RATINGS_NONE) {
+        if ($this->review_ratings() >= 0) {
             return "coalesce((select group_concat(contactId, ' ', rating) from ReviewRating force index (primary) where paperId=PaperReview.paperId and reviewId=PaperReview.reviewId),'')";
         } else {
             return "''";

src/contact.php
@@ -4564,11 +4564,11 @@ class Contact implements JsonSerializable {
 
     /** @return bool */
     function can_view_review_ratings(PaperInfo $prow, ?ReviewInfo $rrow = null, $override_self = false) {
-        $rs = $this->conf->setting("rev_ratings");
+        $rs = $this->conf->review_ratings();
         $rights = $this->rights($prow);
-        if (!$this->can_view_review($prow, $rrow)
-            || (!$rights->allow_pc() && !$rights->allow_review())
-            || ($rs !== REV_RATINGS_PC && $rs !== REV_RATINGS_PC_EXTERNAL)) {
+        if ($rs < 0
+            || !$this->can_view_review($prow, $rrow)
+            || (!$rights->allow_pc() && !$rights->allow_review())) {
             return false;
         }
         if (!$rrow
@@ -4585,8 +4585,7 @@ class Contact implements JsonSerializable {
         foreach ($prow->all_reviews() as $rrow) {
             if ($rrow->reviewNeedsSubmit === 0
                 && $rrow->contactId !== $this->contactId
-                && ($rs === REV_RATINGS_PC_EXTERNAL
-                    || ($rs === REV_RATINGS_PC && $rrow->reviewType > REVIEW_EXTERNAL)))
+                && ($rs > 0 || $rrow->reviewType > REVIEW_EXTERNAL))
                 ++$nsubraters;
         }
         return $nsubraters >= 2;
@@ -4594,9 +4593,7 @@ class Contact implements JsonSerializable {
 
     /** @return bool */
     function can_view_some_review_ratings() {
-        $rs = $this->conf->setting("rev_ratings");
-        return ($rs === REV_RATINGS_PC || $rs === REV_RATINGS_PC_EXTERNAL)
-            && $this->is_reviewer();
+        return $this->conf->review_ratings() >= 0 && $this->is_reviewer();
     }
 
     /** @param ?ReviewInfo $rrow

src/contactlist.php
@@ -243,7 +243,7 @@ class ContactList {
             return true;
         case self::FIELD_REVIEW_RATINGS:
             if (!$this->user->isPC
-                || $this->conf->setting("rev_ratings") == REV_RATINGS_NONE) {
+                || $this->conf->review_ratings() < 0) {
                 return false;
             }
             $this->qopt["revratings"] = $this->qopt["reviews"] = true;

src/help/h_keywords.php
@@ -119,11 +119,11 @@ class Keywords_HelpTopic {
         echo $hth->search_trow("re:me", "you are a reviewer");
         echo $hth->search_trow("re:fdabek", "“fdabek” in reviewer name/email");
         if ($retag) {
-            echo $hth->search_trow("re:#$retag", "has a reviewer tagged “#" . $retag . "”");
+            echo $hth->search_trow("re:#{$retag}", "has a reviewer tagged “#{$retag}”");
         }
         echo $hth->search_trow("re:4", "four reviewers (assigned and/or completed)");
         if ($retag) {
-            echo $hth->search_trow("re:#$retag>1", "at least two reviewers (assigned and/or completed) tagged “#" . $retag . "”");
+            echo $hth->search_trow("re:#{$retag}>1", "at least two reviewers (assigned and/or completed) tagged “#{$retag}”");
         }
         echo $hth->search_trow("re:complete<3", "less than three completed reviews<br><div class=\"hint\">Use “cre:<3” for short.</div>");
         echo $hth->search_trow("re:incomplete>0", "at least one incomplete review");
@@ -144,7 +144,7 @@ class Keywords_HelpTopic {
         if ($hth->conf->setting("rev_tokens")) {
             echo $hth->search_trow("retoken:J88ADNAB", "has a review with token J88ADNAB");
         }
-        if ($hth->conf->setting("rev_ratings") != REV_RATINGS_NONE) {
+        if ($hth->conf->review_ratings() >= 0) {
             echo $hth->search_trow("rate:good", "has a positively-rated review (“rate:bad”, “rate:biased”, etc. also work)");
             echo $hth->search_trow("rate:good:me", "has a positively-rated review by you");
         }

src/help/h_revrate.php
@@ -5,8 +5,9 @@
 class RevRate_HelpTopic {
     static function print(HelpRenderer $hth) {
         $what = "PC members";
-        if ($hth->conf->setting("rev_ratings") == REV_RATINGS_PC_EXTERNAL)
+        if ($hth->conf->review_ratings() > 0) {
             $what = "PC members and external reviewers";
+        }
         echo "<p>{$what} can anonymously rate one another’s
 reviews. We hope this feedback will help reviewers improve the quality of
 their reviews.</p>
@@ -40,12 +41,13 @@ $hth->search_link("search for “rate:good”", "rate:good"), ".
 You may also search for reviews with specific ratings; for instance, ",
 $hth->search_link("search for “rate:short”", "rate:short"), ".</p>";
 
-        if ($hth->conf->setting("rev_ratings") == REV_RATINGS_PC)
+        if ($hth->conf->review_ratings() === 0) {
             $what = "only PC members";
-        else if ($hth->conf->setting("rev_ratings") == REV_RATINGS_PC_EXTERNAL)
+        } else if ($hth->conf->review_ratings() > 0) {
             $what = "PC members and external reviewers";
-        else
+        } else {
             $what = "no one";
+        }
         echo $hth->subhead("Settings");
         echo "<p>Chairs set how ratings work on the ",
             $hth->setting_link("review settings page", "review_rating"), ".",

src/init.php
@@ -19,10 +19,6 @@ const CONFLICT_PCMASK = 31;
 const CONFLICT_AUTHOR = 32;
 const CONFLICT_CONTACTAUTHOR = 64;
 
-const REV_RATINGS_PC = 0;
-const REV_RATINGS_PC_EXTERNAL = 1;
-const REV_RATINGS_NONE = 2;
-
 const DTYPE_SUBMISSION = 0;
 const DTYPE_FINAL = -1;
 const DTYPE_COMMENT = -2;
@@ -278,12 +274,9 @@ function initialize_request($kwarg = null) {
         exit();
     }
 
-    // GET and HEAD: mark as already expired to discourage caching, but allow
-    // the browser to cache for history buttons
-    // POST is not cacheable at all
-    if ($qreq->method() !== "POST") {
-        header("Cache-Control: max-age=0,must-revalidate,private");
-    }
+    // mark as already expired to discourage caching, but allow the browser
+    // to cache for history buttons
+    header("Cache-Control: max-age=0,must-revalidate,private");
 
     // set up Content-Security-Policy if appropriate
     $conf->prepare_security_headers();

src/pages/p_home.php
@@ -425,8 +425,7 @@ class Home_Page {
             $sep = $xsep;
         }
 
-        if ($has_rinfo
-            && $conf->setting("rev_ratings") != REV_RATINGS_NONE) {
+        if ($has_rinfo && $conf->review_ratings() >= 0) {
             $badratings = PaperSearch::unusable_ratings($user);
             $qx = (count($badratings) ? " and not (PaperReview.reviewId in (" . join(",", $badratings) . "))" : "");
             $result = $conf->qe_raw("select sum((rating&" . ReviewInfo::RATING_GOODMASK . ")!=0), sum((rating&" . ReviewInfo::RATING_BADMASK . ")!=0) from PaperReview join ReviewRating using (reviewId) where PaperReview.contactId={$user->contactId} $qx");

src/papersearch.php
@@ -924,8 +924,7 @@ class PaperSearch extends MessageSet {
         // This query should return those reviewIds whose ratings
         // are not visible to the current querier:
         // reviews by `$user` on papers with <=2 reviews and <=2 ratings
-        $rateset = $user->conf->setting("rev_rating");
-        if ($rateset == REV_RATINGS_PC) {
+        if ($user->conf->review_ratings() === 0) {
             $npr_constraint = "reviewType>" . REVIEW_EXTERNAL;
         } else {
             $npr_constraint = "true";

src/reviewinfo.php
@@ -908,7 +908,7 @@ class ReviewInfo implements JsonSerializable {
 
     function ensure_ratings() {
         if ($this->ratingSignature === null) {
-            if ($this->conf->setting("rev_ratings") === REV_RATINGS_NONE) {
+            if ($this->conf->review_ratings() < 0) {
                 $this->ratingSignature = "";
             } else if ($this->prow) {
                 $this->prow->ensure_review_ratings($this);

src/schema.sql
@@ -627,7 +627,7 @@ CREATE TABLE `TopicInterest` (
 -- Initial settings
 -- (each setting must be on its own line for createdb.php/createdb.sh)
 insert into Settings (name, value, data) values
-  ('allowPaperOption', 295, null),   -- schema version
+  ('allowPaperOption', 296, null),   -- schema version
   ('setupPhase', 1, null),           -- initial user is chair
   ('no_papersub', 1, null),          -- no submissions yet
   ('sub_pcconf', 1, null),           -- collect PC conflicts, not collaborators

src/settings/s_review.php
@@ -313,9 +313,9 @@ class Review_SettingParser extends SettingParser {
 
     static function print_ratings(SettingValues $sv) {
         $sv->print_radio_table("review_rating", [
-                REV_RATINGS_NONE => "No",
-                REV_RATINGS_PC => "Yes, PC members can rate reviews",
-                REV_RATINGS_PC_EXTERNAL => "Yes, PC members and external reviewers can rate reviews"
+                -1 => "No",
+                0 => "Yes, PC members can rate reviews",
+                1 => "Yes, PC members and external reviewers can rate reviews"
             ], 'Should HotCRP collect ratings of reviews?   <a class="hint" href="' . $sv->conf->hoturl("help", "t=revrate") . '">Learn more</a>');
     }
 

src/updateschema.php
@@ -3034,6 +3034,10 @@ set ordinal=(t.maxOrdinal+1) where commentId={$row[1]}");
             && $this->v295_unfuck_mentions()) {
             $conf->update_schema_version(295);
         }
+        if ($conf->sversion === 295
+            && $conf->ql_ok("update Settings set value=-1 where name='rev_ratings' and value=2")) {
+            $conf->update_schema_version(296);
+        }
 
         $conf->ql_ok("delete from Settings where name='__schema_lock'");
         Conf::$main = $old_conf_g;