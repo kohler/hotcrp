kohler/hotcrp

commit e87a60d59763d6905b4b6f30234df168d2671675

Add Contact::CFLAG_UNCONFIRMED.
An unconfirmed contact has never logged in. Contacts all start out
unconfirmed. Act-as does not confirm a contact, only login does.

src/contact.php
@@ -218,6 +218,7 @@ class Contact implements JsonSerializable {
     const CFLAG_ROLEDISABLED = 0x4;
     const CFLAG_DELETED = 0x8;
     const CFLAG_GDISABLED = 0x10;
+    const CFLAG_UNCONFIRMED = 0x20;
 
     const CFLAG_DISABLEMENT = 0x1F;
     const CFLAG_DBMASK = ~0xC;
@@ -275,6 +276,7 @@ class Contact implements JsonSerializable {
     static function make(Conf $conf) {
         $u = new Contact($conf);
         $u->contactXid = self::$next_xid--;
+        $u->cflags = self::CFLAG_UNCONFIRMED;
         $u->set_roles_properties();
         return $u;
     }
@@ -285,6 +287,7 @@ class Contact implements JsonSerializable {
         $u = new Contact($conf);
         $u->contactXid = self::$next_xid--;
         $u->email = $email ?? "";
+        $u->cflags = self::CFLAG_UNCONFIRMED;
         $u->set_roles_properties();
         return $u;
     }
@@ -293,7 +296,7 @@ class Contact implements JsonSerializable {
     static function make_placeholder(Conf $conf) {
         $u = new Contact($conf);
         $u->contactXid = self::$next_xid--;
-        $u->cflags = self::CFLAG_PLACEHOLDER;
+        $u->cflags = self::CFLAG_PLACEHOLDER | self::CFLAG_UNCONFIRMED;
         /** @phan-suppress-next-line PhanDeprecatedProperty */
         $u->disabled = self::CFLAG_PLACEHOLDER;
         $u->set_roles_properties();
@@ -319,6 +322,7 @@ class Contact implements JsonSerializable {
         $u->contactXid = self::$next_xid--;
         $u->email = $email ?? "";
         $u->cdb_confid = $conf->cdb_confid();
+        $u->cflags = self::CFLAG_UNCONFIRMED;
         $u->set_roles_properties();
         return $u;
     }
@@ -339,7 +343,7 @@ class Contact implements JsonSerializable {
         }
         $u->affiliation = simplify_whitespace($args["affiliation"] ?? "");
         $u->orcid = trim($args["orcid"] ?? "");
-        $u->cflags = $args["disablement"] ?? 0;
+        $u->cflags = ($args["disablement"] ?? 0) | self::CFLAG_UNCONFIRMED;
         /** @phan-suppress-next-line PhanDeprecatedProperty */
         $u->disabled = $u->cflags & self::CFLAG_DISABLEMENT & self::CFLAG_DBMASK;
         $u->set_roles_properties();
@@ -784,8 +788,12 @@ class Contact implements JsonSerializable {
 
         // maybe auto-create a user
         if (($this->_activated & 2) === 0 && $this->email) {
-            if ($this->activate_placeholder_prop(true)) {
+            if ($this->activate_placeholder_prop(($this->_activated & 7) === 1)) {
                 $this->save_prop();
+                if (($cdbu = $this->cdb_user())
+                    && $cdbu->activate_placeholder_prop(($this->_activated & 7) === 1)) {
+                    $cdbu->save_prop();
+                }
             }
             $trueuser_aucheck = $qreq->csession("trueuser_author_check") ?? 0;
             if (!$this->has_account_here()
@@ -979,6 +987,9 @@ class Contact implements JsonSerializable {
             $cdbux->set_prop("cflags", $cdbux->cflags | Contact::CFLAG_PLACEHOLDER);
             $cdbux->set_prop("disabled", $cdbux->cflags & Contact::CFLAG_DISABLEMENT & Contact::CFLAG_DBMASK);
         }
+        if (($this->cflags & self::CFLAG_UNCONFIRMED) === 0) {
+            $cdbux->set_prop("cflags", $cdbux->cflags & ~Contact::CFLAG_UNCONFIRMED);
+        }
         if (!empty($cdbux->_mod_undo)) {
             assert($cdbux->cdb_confid !== 0);
             $cdbux->save_prop();
@@ -1047,6 +1058,11 @@ class Contact implements JsonSerializable {
     /** @return int */
     function disabled_flags() {
         return $this->cflags & self::CFLAG_DISABLEMENT;
+    }
+
+    /** @return bool */
+    function is_unconfirmed() {
+        return ($this->cflags & self::CFLAG_UNCONFIRMED) !== 0;
     }
 
     /** @return int */
@@ -1884,6 +1900,10 @@ class Contact implements JsonSerializable {
             $this->set_prop("disabled", $this->cflags & self::CFLAG_DISABLEMENT & self::CFLAG_DBMASK);
             $changed = true;
         }
+        if (($this->cflags & self::CFLAG_UNCONFIRMED) !== 0 && $confirm) {
+            $this->set_prop("cflags", $this->cflags & ~self::CFLAG_UNCONFIRMED);
+            $changed = true;
+        }
         return $changed;
     }
 
@@ -2043,6 +2063,12 @@ class Contact implements JsonSerializable {
             && $this->cdb_confid === 0) {
             $this->set_prop("cflags", $this->cflags | self::CFLAG_GDISABLED);
             $this->set_prop("disabled", $this->cflags & self::CFLAG_DISABLEMENT & self::CFLAG_DBMASK);
+        }
+
+        // unconfirmed import is special
+        // source is confirmed: this is confirmed
+        if (($src->cflags & self::CFLAG_UNCONFIRMED) === 0) {
+            $this->set_prop("cflags", $this->cflags & ~self::CFLAG_UNCONFIRMED);
         }
     }
 

src/conference.php
@@ -348,7 +348,7 @@ class Conf {
 
     function load_settings() {
         $this->__load_settings();
-        if ($this->sversion < 285) {
+        if ($this->sversion < 286) {
             $old_nerrors = Dbl::$nerrors;
             while ((new UpdateSchema($this))->run()) {
                 usleep(50000);

src/schema.sql
@@ -622,7 +622,7 @@ CREATE TABLE `TopicInterest` (
 -- Initial settings
 -- (each setting must be on its own line for createdb.sh)
 insert into Settings (name, value, data) values
-  ('allowPaperOption', 285, null),   -- schema version
+  ('allowPaperOption', 286, null),   -- schema version
   ('setupPhase', 1, null),           -- initial user is chair
   ('no_papersub', 1, null),          -- no submissions yet
   ('sub_pcconf', 1, null),           -- collect PC conflicts, not collaborators

src/updateschema.php
@@ -2936,6 +2936,10 @@ set ordinal=(t.maxOrdinal+1) where commentId={$row[1]}");
             && $conf->ql_ok("update ContactInfo set cflags=disabled")) {
             $conf->update_schema_version(285);
         }
+        if ($conf->sversion === 285
+            && $conf->ql_ok("update ContactInfo set cflags=34 where cflags=2")) {
+            $conf->update_schema_version(286);
+        }
 
         $conf->ql_ok("delete from Settings where name='__schema_lock'");
         Conf::$main = $old_conf_g;

test/t_login.php
@@ -1,6 +1,6 @@
 <?php
 // t_login.php -- HotCRP tests
-// Copyright (c) 2006-2022 Eddie Kohler; see LICENSE.
+// Copyright (c) 2006-2023 Eddie Kohler; see LICENSE.
 
 class Login_Tester {
     /** @var Conf
@@ -94,6 +94,7 @@ class Login_Tester {
             $user->ensure_account_here();
             xassert_neqq($user->contactId, 0);
             xassert_eqq($user->contactDbId, 0);
+            xassert(!$user->is_unconfirmed());
         }
     }
 
@@ -103,6 +104,8 @@ class Login_Tester {
             "email" => $email,
             "disablement" => Contact::CFLAG_PLACEHOLDER
         ])->store();
+        $user = $this->conf->user_by_email($email);
+        xassert($user->is_unconfirmed());
 
         $this->conf->invalidate_caches(["users" => true, "cdb" => true]);
 
@@ -124,6 +127,7 @@ class Login_Tester {
         $u = $this->conf->checked_user_by_email($email);
         xassert(!!$u);
         xassert_eqq($u->disabled_flags(), Contact::CFLAG_PLACEHOLDER);
+        xassert($u->is_unconfirmed());
         if ($this->cdb) {
             $u = $this->conf->checked_cdb_user_by_email($email);
             xassert(!!$u);
@@ -149,14 +153,36 @@ class Login_Tester {
         xassert(!!$result);
         xassert(user($email)->check_password("newuserpassword!"));
 
-        // user is no longer a placeholder
+        // user is no longer a placeholder, but unconfirmed
         $u = $this->conf->checked_user_by_email($email);
         xassert(!!$u);
         xassert_eqq($u->disabled_flags(), 0);
+        xassert($u->is_unconfirmed());
         if ($this->cdb) {
             $u = $this->conf->checked_cdb_user_by_email($email);
             xassert(!!$u);
             xassert_eqq($u->disabled_flags(), 0);
+            xassert($u->is_unconfirmed());
+        }
+
+        // logging in confirms user
+        $user = Contact::make($this->conf);
+        $qreq = TestRunner::make_qreq($user, "signin?email={$email}&password=newuserpassword!", "POST");
+        $result = null;
+        try {
+            $cs = $this->conf->page_components($user, $qreq);
+            $signinp = $cs->callable("Signin_Page");
+            $signinp->signin_request($user, $qreq, $cs);
+        } catch (Redirection $redir) {
+            $result = $redir;
+        }
+        xassert(!!$result);
+        xassert_str_contains($result->url, "postlogin");
+        $u = $this->conf->fresh_user_by_email($email);
+        xassert(!$u->is_unconfirmed());
+        if ($this->cdb) {
+            $u = $this->conf->checked_cdb_user_by_email($email);
+            xassert(!$u->is_unconfirmed());
         }
     }
 

commit 81d55d6b4dffebf85c612b3d7cf34a16ddf32ccc

Do not prematurely create users when a CDB user examines a site.
Yucky: users were being created because of activate_placeholder_prop.
The problem was introduced in e87a60d59763d6905b4b6f30234df168d2671675
with the unconfirmed flag. Activating a placeholder should turn off
the unconfirmed flag; but a user with no local account should count
as confirmed (because they were confirmed globally).

I hope that there are no problems in the future with unconfirmed
users.

src/contact.php
@@ -281,12 +281,13 @@ class Contact implements JsonSerializable {
     }
 
     /** @param ?string $email
+     * @param bool $confirmed
      * @return Contact */
-    static function make_email(Conf $conf, $email) {
+    static function make_email(Conf $conf, $email, $confirmed = false) {
         $u = new Contact($conf);
         $u->contactXid = self::$next_xid--;
         $u->email = $email ?? "";
-        $u->cflags = self::CF_UNCONFIRMED;
+        $u->cflags = $confirmed ? 0 : self::CF_UNCONFIRMED;
         $u->set_roles_properties();
         return $u;
     }

src/init.php
@@ -390,7 +390,8 @@ function initialize_request($kwarg = null) {
 
     // look up and activate user
     $muser = ($conf->fresh_user_by_email($uemail)
-              ?? Contact::make_email($conf, $uemail))->activate($qreq, true, $uindex);
+              ?? Contact::make_email($conf, $uemail, true))
+        ->activate($qreq, true, $uindex);
     Contact::set_main_user($muser);
     $qreq->set_user($muser);
 