2c087526be3e79efcb2131b7e3a4b18eb949551b
Eddie Kohler <ekohler@gmail.com>
2025-08-22 21:59:34 -0400

    Dewarn

--- src/paperoption.php
+++ src/paperoption.php
@@ -1813,7 +1813,7 @@ class Text_PaperOption extends PaperOption {
 class Unknown_PaperOption extends PaperOption {
     function __construct(Conf $conf, $args) {
         $args->type = "none";
-        $args->configurable = false;
         parent::__construct($conf, $args, "hidden");
+        $this->configurable = false;
     }
 }
