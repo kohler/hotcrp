dc8e2bc64522883aaf33c16b31f86fa744a8b0ba
Eddie Kohler <ekohler@gmail.com>
2025-09-27 17:19:41 -0400

    Ding

--- src/useractions.php
+++ src/useractions.php
@@ -177,7 +177,7 @@ class UserActions extends MessageSet {
         $user->set_prop("passwordUseTime", 0);
         $user->set_prop("lastLogin", 0);
         $user->set_prop("defaultWatch", 2);
-        $user->clear_data();
+        $user->clear_data_prop();
         $user->save_prop();
 
         // unlink from primary
