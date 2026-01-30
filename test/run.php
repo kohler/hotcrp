57440781bc36ed8b06a141c6395421dc23e9f13b
Eddie Kohler <ekohler@gmail.com>
2026-01-30 11:33:04 -0500

    Test infrastructure: Add test/run.php script to run specific tests
    * Limit tests to only call public methods.
    * Support tester `finalize` method, called before test class is done.

---
+++ test/run.php
@@ -0,0 +1,7 @@
+<?php
+// run.php -- HotCRP API tests
+// Copyright (c) 2024 Eddie Kohler; see LICENSE.
+
+declare(strict_types=1);
+require_once(__DIR__ . '/setup.php');
+TestRunner::run("reset_db");
