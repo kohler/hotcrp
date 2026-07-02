6ebfff36008b1d69f0acf4a89d2b25b9f35a2ab3
Eddie Kohler <ekohler@gmail.com>
2026-07-02 07:44:47 -0400

    Subprocess: Support start() + step() as well as run()

--- lib/subprocess.php
+++ lib/subprocess.php
@@ -18,11 +18,21 @@ class Subprocess {
     /** @var string */
     public $stderr = "";
     /** @var float */
+    public $start_time;
+    /** @var float */
     public $runtime = 0.0;
+    /** @var int */
+    private $_run_status = 0;
     /** @var bool */
     public $ok = false;
     /** @var ?list<callable> */
     private $_progress_functions;
+    /** @var ?resource */
+    private $_proc;
+    /** @var ?list<resource> */
+    private $_pipes;
+    /** @var int */
+    private $_stdin_pos = 0;
 
 
     /** @param list<string> $command
@@ -88,79 +98,110 @@ class Subprocess {
 
 
     /** @return $this */
-    function run() {
-        if ($this->status !== null) {
+    function start() {
+        if ($this->_run_status !== 0) {
             throw new Error("restarting subprocess");
         }
 
-        $stdinpos = 0;
-        $stdinlen = strlen($this->stdin);
         $descriptors = [
-            $stdinpos === $stdinlen ? ["file", "/dev/null", "r"] : ["pipe", "rb"],
+            $this->_stdin_pos === strlen($this->stdin) ? ["file", "/dev/null", "r"] : ["pipe", "rb"],
             ["pipe", "wb"],
             ["pipe", "wb"]
         ];
-        $start_time = microtime(true);
-        $proc = proc_open(self::args_to_command($this->command), $descriptors, $pipes, $this->cwd, $this->env);
-        if (!$proc) {
+        $this->start_time = microtime(true);
+        $this->_proc = proc_open(self::args_to_command($this->command),
+            $descriptors, $this->_pipes, $this->cwd, $this->env);
+        if (!$this->_proc) {
             $this->status = -1;
+            $this->_run_status = 4;
             return $this;
         }
 
-        if ($stdinpos !== $stdinlen) {
-            stream_set_blocking($pipes[0], false);
+        if ($this->_stdin_pos !== strlen($this->stdin)) {
+            stream_set_blocking($this->_pipes[0], false);
+        } else {
+            $this->_pipes[0] = null;
         }
-        stream_set_blocking($pipes[1], false);
-        stream_set_blocking($pipes[2], false);
-        $nloop = 0;
+        stream_set_blocking($this->_pipes[1], false);
+        stream_set_blocking($this->_pipes[2], false);
+        $this->_run_status = 1;
+        return $this;
+    }
 
-        while (!feof($pipes[1]) || !feof($pipes[2])) {
-            ++$nloop;
-            if ($nloop > 1 && !empty($this->_progress_functions)) {
-                $this->runtime = microtime(true) - $start_time;
-                foreach ($this->_progress_functions as $f) {
-                    $f($this);
-                }
-            }
-
-            if ($stdinpos !== $stdinlen) {
-                $nw = fwrite($pipes[0], substr($this->stdin, $stdinpos));
-                if ($nw !== false) {
-                    $stdinpos += $nw;
-                }
-                if ($stdinpos === $stdinlen) {
-                    fclose($pipes[0]);
-                }
-            }
-            $x = fread($pipes[1], 32768);
-            if ($x !== false) {
-                $this->stdout .= $x;
-            }
-            $y = fread($pipes[2], 32768);
-            if ($y !== false) {
-                $this->stderr .= $y;
-            }
-            if ($x === false
-                || $y === false) {
-                break;
-            }
-
-            $r = [$pipes[1], $pipes[2]];
-            $w = $stdinpos === $stdinlen ? [] : [$pipes[0]];
-            $e = [];
-            stream_select($r, $w, $e, 5);
+    private function _run_step() {
+        if (feof($this->_pipes[1]) && feof($this->_pipes[2])) {
+            $this->_run_status = 3;
+            return;
         }
 
-        if ($stdinpos !== $stdinlen) {
-            fclose($pipes[0]);
+        if ($this->_run_status === 1) {
+            $this->_run_status = 2;
+        } else if (!empty($this->_progress_functions)) {
+            $this->runtime = microtime(true) - $this->start_time;
+            foreach ($this->_progress_functions as $f) {
+                $f($this);
+            }
         }
-        fclose($pipes[1]);
-        fclose($pipes[2]);
-        $st = proc_get_status($proc);
-        $close = proc_close($proc);
-        $this->status = $st["running"] ? $close : $st["exitcode"] /* = -1 on signal */;
-        $this->ok = $this->status === 0;
-        $this->runtime = microtime(true) - $start_time;
+
+        if ($this->_pipes[0]) {
+            $nw = fwrite($this->_pipes[0], substr($this->stdin, $this->_stdin_pos));
+            if ($nw !== false) {
+                $this->_stdin_pos += $nw;
+            }
+            if ($this->_stdin_pos === strlen($this->stdin)) {
+                fclose($this->_pipes[0]);
+                $this->_pipes[0] = null;
+            }
+        }
+        $x = fread($this->_pipes[1], 32768);
+        if ($x !== false) {
+            $this->stdout .= $x;
+        }
+        $y = fread($this->_pipes[2], 32768);
+        if ($y !== false) {
+            $this->stderr .= $y;
+        }
+        if ($x === false || $y === false) {
+            $this->_run_status = 3;
+            return;
+        }
+
+        $r = [$this->_pipes[1], $this->_pipes[2]];
+        $w = $this->_pipes[0] ? [$this->_pipes[0]] : [];
+        $e = [];
+        stream_select($r, $w, $e, 5);
+    }
+
+    /** @return $this */
+    function step() {
+        if ($this->_run_status === 0) {
+            $this->start();
+        }
+        if ($this->_run_status === 1 || $this->_run_status === 2) {
+            $this->_run_step();
+        }
+        if ($this->_run_status === 3) {
+            if ($this->_pipes[0]) {
+                fclose($this->_pipes[0]);
+            }
+            fclose($this->_pipes[1]);
+            fclose($this->_pipes[2]);
+            $st = proc_get_status($this->_proc);
+            $close = proc_close($this->_proc);
+            $this->status = $st["running"] ? $close : $st["exitcode"] /* = -1 on signal */;
+            $this->ok = $this->status === 0;
+            $this->runtime = microtime(true) - $this->start_time;
+            $this->_proc = $this->_pipes = null;
+            $this->_run_status = 4;
+        }
+        return $this;
+    }
+
+    /** @return $this */
+    function run() {
+        while ($this->_run_status !== 4) {
+            $this->step();
+        }
         return $this;
     }
 }
