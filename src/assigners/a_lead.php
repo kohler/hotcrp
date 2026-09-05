5edb17eb4190be1493a5486107332a11c6674512
Eddie Kohler <ekohler@gmail.com>
2026-09-05 14:45:37 -0400

    Manager assignment obeys token scope

--- src/assigners/a_lead.php
+++ src/assigners/a_lead.php
@@ -61,6 +61,10 @@ class Lead_AssignmentParser extends AssignmentParser {
         if ($this->key !== "manager") {
             return $state->user->can_manage($prow);
         } else if ($state->user->privChair) {
+            if (!$state->user->scope_allows(TokenScope::S_SUB_ADMIN, $prow)) {
+                $state->paper_error($prow->failure_reason(["scope" => TokenScope::S_SUB_ADMIN]));
+                return false;
+            }
             return true;
         }
         $state->paper_error("<0>Only chairs and sysadmins can change paper administrators");
